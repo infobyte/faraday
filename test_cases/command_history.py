2b278dad21c7db370039add13c935c6a727a6d75
danito <fdaniel@infobytesec.com>
2014-03-20 12:59:38 -0300

    Managers mangling
    Created Command Manager object for interaction with persistence objects
    Moved CouchdbManager and FS Manager to managers.all module for
    consistency
    
    next steps: deacouple from model.api.devlog using python logging
    capabilities

--- test_cases/command_history.py
+++ test_cases/command_history.py
@@ -18,13 +18,18 @@ import re
 from model.commands_history import CommandRunInformation
 from time import time
 
+from config.configuration import getInstanceConfiguration
+CONF = getInstanceConfiguration()
+
+from managers.all import CommandManager, CouchdbManager
 
 class CommandHistoryTestSuite(unittest.TestCase):
 
     def setUp(self):
+        self.couch_host = "http://192.168.33.10:5984" 
+        CONF.setCouchUri(self.couch_host)
         # self.plugin_repo_path = os.path.join(os.getcwd(), "plugins", "repo")
         # self.plugin_manager = managers.PluginManager(self.plugin_repo_path)
-        pass
 
         # class WorkspaceStub():
         #     def __init__(self):
@@ -33,6 +38,27 @@ class CommandHistoryTestSuite(unittest.TestCase):
         #     self.plugin_manager.createController(WorkspaceStub())
 
     def test_valid_command_creation(self):
+        information = self.getDefaultCommandInfo()
+
+        command_info = CommandRunInformation(**information)
+        self.assertIsNotNone(command_info, "Command wrongly created")
+
+        self.assertEquals(command_info.command, information['command'], "Field %s not instantiated" % information['command'])
+
+        self.assertEquals(command_info.parameters, information['parameters'],
+                "Field %s not instantiated" % information['parameters'])
+
+        self.assertEquals(command_info.itime, information['itime'], "Field %s not instantiated" % information['itime'])
+
+        self.assertEquals(command_info.duration, information['duration'], "Field %s not instantiated" % information['duration'])
+
+        self.assertEquals(command_info.workspace, information['workspace'], "Field %s not instantiated" % information['workspace'])
+
+    def test_create_command_manager(self):
+        cm = CommandManager()
+        self.assertIsNotNone(cm, "Command Manager noti nstantiated")
+
+    def getDefaultCommandInfo(self):
         information = { 'command' : 'nmap',
                         'parameters' : '-Sv',
                         'itime' : time(),
@@ -40,24 +66,7 @@ class CommandHistoryTestSuite(unittest.TestCase):
                         'workspace' : 'default'
                         }
 
-
-        command_info = CommandRunInformation(**information)
-        self.assertIsNotNone(command_info, "Command wrongly created")
-
-        self.assertEquals(command_info.command, information['command'], "Field
-                %s not instantiated" % information['command'])
-
-        self.assertEquals(command_info.parameters, information['parameters'],
-                "Field %s not instantiated" % information['parameters'])
-
-        self.assertEquals(command_info.itime, information['itime'], "Field %s
-                not instantiated" % information['itime'])
-
-        self.assertEquals(command_info.duration, information['duration'],
-                "Field %s not instantiated" % information['duration'])
-
-        self.assertEquals(command_info.workspace, information['workspace'],
-                "Field %s not instantiated" % information['workspace'])
+        return information
 
 
 if __name__ == '__main__':

8015cfce57f64b25f2f9bb420dde99eb438b1cc2
umpacu <rgerman@infobytesec.com>
2014-04-06 17:47:14 -0300

    Preserve original command on user history
    If a user types "nmal localhost" and we change that command because of a
    plugin, we want to keep the original command on user history.
    That way Faraday is more transparent to the user

--- zsh/faraday.zsh
+++ zsh/faraday.zsh
@@ -9,13 +9,23 @@ echo ">>> WELCOME TO FARADAY"
 PS1="%{${fg_bold[red]}%}[faraday]%{${reset_color}%} $PS1"
 
 setopt multios
+setopt histignorespace
 
 plugin_controller_client=$ZDOTDIR/plugin_controller_client.py
+old_cmd=
 
 add-output() {
+    old_cmd=$BUFFER
     new_cmd=`python2 $plugin_controller_client send_cmd $BUFFER`
-    BUFFER="$new_cmd"
+    BUFFER=" $new_cmd"
     zle .$WIDGET "$@"
+}
+
+function zshaddhistory() {
+    emulate -L zsh
+    print -sr -- "$old_cmd"
+    fc -p
+    return 1
 }
 
 zle -N accept-line add-output

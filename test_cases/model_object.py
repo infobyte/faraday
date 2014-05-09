03a8e4cf794ae5afd6ee5755a6598cc12e977252
danito <fdaniel@infobytesec.com>
2014-05-09 18:06:06 -0300

    Adding ModelObject CRUD Testcases
    For add host and removal

--- test_cases/model_object.py
+++ test_cases/model_object.py
@@ -42,7 +42,7 @@ def create_service(self, host, interface, service_name = "coquito"):
                                 interface.getID(), service)
     return service
 
-class ModelObjectRetrieval(unittest.TestCase):
+class ModelObjectCRUD(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -54,13 +54,27 @@ class ModelObjectRetrieval(unittest.TestCase):
                                     mock(plcore.PluginController))
         self.temp_workspace = self.wm.createWorkspace(
                                         new_random_workspace_name(),
-                                        workspaceClass=WorkspaceOnCouch) 
+                                        workspaceClass=WorkspaceOnCouch)
 
         self.wm.setActiveWorkspace(self.temp_workspace)
         WorkspacePersister.stopThreads()
 
     def tearDown(self):
         self.wm.removeWorkspace(self.temp_workspace.name)
+
+    def testAddHost(self):
+        """ This test case creates a host within the Model Controller context
+        then checks it's vality"""
+        # When
+        hostname = 'host'
+        _ = create_host(self, host_name=hostname, os='windows')
+
+        # #Then
+        added_host = self.model_controller.getHost(hostname)
+
+        self.assertEquals(added_host.getName(), hostname,
+                'Saved object name is not correctly saved')
+
 
     def testAddVulnToHost(self):
         """ This test case creates a host within the Model Controller context
@@ -74,15 +88,9 @@ class ModelObjectRetrieval(unittest.TestCase):
 
         added_host = self.model_controller.getHost(h.getName())
         vulns = added_host.getVulns()
+        #Then
         self.assertIn(vuln, vulns, 'Vuln not added')
 
-        self.temp_workspace.load()
-
-        # Then
-        added_host = self.model_controller.getHost(h.getName())
-        vulns = added_host.getVulns()
-        self.assertIn(vuln.getID(), [v.getID() for v in vulns],
-                            'Vuln not reloaded')
 
 
     def testAddVulnToInterface(self):
@@ -102,18 +110,8 @@ class ModelObjectRetrieval(unittest.TestCase):
         added_host = self.model_controller.getHost(host.getName())
         added_interface = added_host.getInterface(interface.getID())
         vulns = added_interface.getVulns()
-        self.assertIn(vuln, vulns, 'Vuln not added')
-
-        self.temp_workspace.load()
-
         # Then
-        added_host = self.model_controller.getHost(host.getName())
-        added_interface = added_host.getInterface(interface.getID())
-        vulns = added_interface.getVulns()
-        self.assertIn(vuln.getID(), [v.getID() for v in vulns],
-                            'Vuln not reloaded')
-
-
+        self.assertIn(vuln, vulns, 'Vuln not added')
 
     def testAddVulnToService(self):
         """ This test case creates a host within the Model Controller context
@@ -134,18 +132,9 @@ class ModelObjectRetrieval(unittest.TestCase):
         added_interface = added_host.getInterface(interface.getID())
         added_service = added_interface.getService(service.getID())
         vulns = added_service.getVulns()
+        # Then
         self.assertIn(vuln, vulns, 'Vuln not added')
 
-        self.temp_workspace.load()
-
-        # Then
-        added_host = self.model_controller.getHost(host.getName())
-        added_interface = added_host.getInterface(interface.getID())
-        added_service = added_interface.getService(service.getID())
-        vulns = added_service.getVulns()
-
-        self.assertIn(vuln.getID(), [v.getID() for v in vulns],
-                            'Vuln not reloaded')
 
     def testAddVulnWebToHost(self):
         """ This test case creates a host within the Model Controller context
@@ -159,14 +148,8 @@ class ModelObjectRetrieval(unittest.TestCase):
 
         added_host = self.model_controller.getHost(h.getName())
         vulns = added_host.getVulns()
-        self.assertIn(vuln, vulns, 'Vuln not added')
-
-        self.temp_workspace.load()
         # Then
-        added_host = self.model_controller.getHost(h.getName())
-        vulns = added_host.getVulns()
-        self.assertIn(vuln.getID(), [v.getID() for v in vulns],
-                'Vuln not reloaded')
+        self.assertIn(vuln, vulns, 'Vuln not added')
 
 
     def testAddVulnWebToInterface(self):
@@ -217,43 +200,28 @@ class ModelObjectRetrieval(unittest.TestCase):
         added_interface = added_host.getInterface(interface.getID())
         added_service = added_interface.getService(service.getID())
         vulns = added_service.getVulns()
+        # Then
         self.assertIn(vuln, vulns, 'Vuln not added')
 
-        self.temp_workspace.load()
-
-        # Then
-        added_host = self.model_controller.getHost(host.getName())
-        added_interface = added_host.getInterface(interface.getID())
-        added_service = added_interface.getService(service.getID())
-        vulns = added_service.getVulns()
-        self.assertIn(vuln.getID(), [v.getID() for v in vulns],
-                'Vuln not reloaded')
 
     def testAddNoteToHost(self):
         """ This test case creates a host within the Model Controller context
-        then adds a VULN"""
+        then adds a Note"""
 
         # When
         h = create_host(self)
         note = ModelObjectNote(name='NoteTest', text='TestDescription')
         self.model_controller.addNoteToHostSYNC(h.getID(), note)
 
+        # Then
         added_host = self.model_controller.getHost(h.getName())
         notes = added_host.getNotes()
         self.assertIn(note, notes, 'Note not added')
 
-        self.temp_workspace.load()
-
-        # Then
-        added_host = self.model_controller.getHost(h.getName())
-        notes = added_host.getNotes()
-        self.assertIn(note.getID(), [n.getID() for n in notes],
-                        'Note not reloaded')
-
 
     def testAddNoteToInterface(self):
         """ This test case creates a host within the Model Controller context
-        adds an interface to it then adds a VULN"""
+        adds an interface to it then adds a Note"""
 
         # When
         host = create_host(self)
@@ -267,22 +235,13 @@ class ModelObjectRetrieval(unittest.TestCase):
         added_host = self.model_controller.getHost(host.getName())
         added_interface = added_host.getInterface(interface.getID())
         notes = added_interface.getNotes()
-        self.assertIn(note, notes, 'Note not added')
-
-        self.temp_workspace.load()
-
         # Then
-        added_host = self.model_controller.getHost(host.getName())
-        added_interface = added_host.getInterface(interface.getID())
-        notes = added_interface.getNotes()
-        self.assertIn(note.getID(), [n.getID() for n in notes],
-                        'Note not reloaded')
-
+        self.assertIn(note, notes, 'Note not added')
 
 
     def testAddNoteToService(self):
         """ This test case creates a host within the Model Controller context
-        adds an interface to it then adds service then a VULN"""
+        adds an interface to it then adds service then a Note"""
 
         # When
         host = create_host(self)
@@ -298,18 +257,84 @@ class ModelObjectRetrieval(unittest.TestCase):
         added_interface = added_host.getInterface(interface.getID())
         added_service = added_interface.getService(service.getID())
         notes = added_service.getNotes()
+        # Then
         self.assertIn(note, notes, 'Note not added')
 
-        self.temp_workspace.load()
+    def testDeleteHost(self):
+        """ Creates a Host to test it's removal from the controllers list """
 
-        # Then
-        added_host = self.model_controller.getHost(host.getName())
-        added_interface = added_host.getInterface(interface.getID())
-        added_service = added_interface.getService(service.getID())
-        notes = added_service.getNotes()
+        host1 = create_host(self, "coquito")
+        hosts_ids = [h.getID() for h in self.model_controller.getAllHosts()]
 
-        self.assertIn(note.getID(), [n.getID() for n in notes],
-                            'Note not reloaded')
+        self.assertIn(host1.getID(), hosts_ids,
+                                "Host not in controller")
+
+        self.model_controller.delHostSYNC(host1.name)
+
+        hosts_ids = [h.getID() for h in self.model_controller.getAllHosts()]
+        self.assertNotIn(host1.getID(), hosts_ids,
+                                "Host not deleted")
+
+    def testDeleteInterface(self):
+        """ Creates a Host and an Interface, then deletes the interface
+        to test it's removal from the controllers list """
+
+        host1 = create_host(self, "coquito")
+        interface1 = create_interface(self, host1, iname="pepito")
+
+        hosts_ids = [h.getID() for h in self.model_controller.getAllHosts()]
+        self.assertIn(host1.getID(), hosts_ids,
+                                "Host not in controller")
+
+        host1 = self.model_controller.getHost(host1.getID())
+
+        interfaces_ids = [i.getID() for i in host1.getAllInterfaces()]
+        self.assertIn(interface1.getID(), interfaces_ids,
+                                "Interface not in host!")
+
+        self.model_controller.delInterfaceSYNC(host1.getID(), "pepito")
+
+        
+        interfaces_ids = [i.getID() for i in
+                self.model_controller.getHost(host1.getID()).getAllInterfaces()]
+
+        self.assertNotIn(interface1.getID(), interfaces_ids,
+                                "Interface not in host!")
+
+
+    def testDeleteService(self):
+        """ Creates a Host an Interface and a Service, then deletes the Service
+        to test it's removal from the controllers list """
+
+        host1 = create_host(self, "coquito")
+        interface1 = create_interface(self, host1, iname="pepito")
+        service1 = create_service(self, host1, interface1)
+
+        hosts_ids = [h.getID() for h in self.model_controller.getAllHosts()]
+        self.assertIn(host1.getID(), hosts_ids,
+                                "Host not in controller")
+
+        host1 = self.model_controller.getHost(host1.getID())
+        interfaces_ids = [i.getID() for i in host1.getAllInterfaces()]
+        self.assertIn(interface1.getID(), interfaces_ids,
+                                "Interface not in host!")
+
+        services_ids = [s.getID() for s in \
+                            self.model_controller.getHost(host1.getID())
+                            .getInterface(interface1.getID()).getAllServices()]
+
+        self.assertIn(service1.getID(), services_ids,
+                                "Service not in Interface!")
+
+        self.model_controller.delServiceFromInterfaceSYNC(host1.getID(),
+                                    interface1.getID(), service1.getID())
+
+        services_ids = [s.getID() for s in \
+                            self.model_controller.getHost(host1.getID())
+                            .getInterface(interface1.getID()).getAllServices()]
+
+        self.assertNotIn(service1.getID(), services_ids, \
+                        "Service not deleted")
 
 if __name__ == '__main__':
     unittest.main()

3a79c6a9acffcb0c5dc5b24b274442098f5a9d0a
danito <fdaniel@infobytesec.com>
2014-07-18 17:45:58 -0300

    Creation of workspaces interactions with mappers managers

--- test_cases/workspace_manager.py
+++ test_cases/workspace_manager.py
@@ -12,32 +12,52 @@ sys.path.append('.')
 from config.configuration import getInstanceConfiguration
 from managers.model_managers import WorkspaceManager
 from persistence.persistence_managers import DBTYPE
-from mockito import mock, verify, when
+from mockito import mock, verify, when, any
 CONF = getInstanceConfiguration()
 
 class UnitTestWorkspaceManager(unittest.TestCase):
     """ Unit tests for WorkspaceManager """
 
     def testCreateWorkspaceManager(self):
-        workspace_manager = WorkspaceManager(mock())
+        workspace_manager = WorkspaceManager(mock(), mock())
         self.assertIsNotNone(workspace_manager)
 
     def testCreateWorkspaceDBManagerInteract(self):
         dbManager = mock()
-        when(dbManager).dbCreate('test_workspace', DBTYPE.FS).thenReturn(True)
-        workspace_manager = WorkspaceManager(dbManager)
+        dbConnector = mock()
+        when(dbManager).dbCreate('test_workspace', DBTYPE.FS).thenReturn(dbConnector)
+        workspace_manager = WorkspaceManager(dbManager, mock())
         workspace_manager.createWorkspace('test_workspace', 'a test workspace',
                                         DBTYPE.FS)
         verify(dbManager).dbCreate('test_workspace', DBTYPE.FS)
 
     def testCreateWorkspaceReturnsWorkspace(self):
-        workspace_manager = WorkspaceManager(mock())
+        workspace_manager = WorkspaceManager(mock(), mock())
         workspace = workspace_manager.createWorkspace('test_workspace', 
                                         'a test workspace',
                                         DBTYPE.FS)
         self.assertTrue(workspace, 'workspace not instantiated')
         self.assertEquals(workspace.name, 'test_workspace',
                             'Workspace name not set, is it valid?')
+
+    def testCreateWorkspaceCreateMappers(self):
+        dbManager = mock()
+        mappersManager = mock()
+        dbConnector = mock()
+        mappers = mock()
+
+        when(mappersManager).saveObj(any()).thenReturn(True) 
+        when(dbManager).dbCreate('test_workspace', DBTYPE.FS).thenReturn(dbConnector)
+        when(mappersManager).createMappers(dbConnector).thenReturn(True) 
+
+        workspace_manager = WorkspaceManager(dbManager, mappersManager)
+        workspace_manager.createWorkspace('test_workspace', 'a test workspace',
+                                        DBTYPE.FS)
+
+        verify(mappersManager).createMappers(dbConnector)
+        verify(mappersManager).saveObj(any())
+
+
 
 if __name__ == '__main__':
     unittest.main()

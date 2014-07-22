65fb5bb2c565b55b054ad35f2b87887e035b1fc2
danito <fdaniel@infobytesec.com>
2014-07-22 16:30:56 -0300

    Add and open workspace support

---
+++ managers/model_managers.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+'''
+Faraday Penetration Test IDE - Community Version
+Copyright (C) 2013  Infobyte LLC (http://www.infobytesec.com/)
+See the file 'doc/LICENSE' for the license information
+
+'''
+from utils.logs import getLogger
+from model.workspace import Workspace
+
+class WorkspaceManager(object):
+    """Workspace Manager class
+    It's responsabilities goes from:
+        * Workspace creation
+        * Workspace removal
+        * Workspace opening
+        * Active Workspace switching"""
+
+    def __init__(self, dbManager, mappersManager, *args, **kwargs):
+        self.dbManager = dbManager
+        self.mappersManager = mappersManager
+
+    def createWorkspace(self, name, desc, dbtype):
+        workspace = Workspace(name, desc)
+        dbConnector = self.dbManager.dbCreate(name, dbtype)
+        if dbConnector:
+            self.mappersManager.createMappers(dbConnector)
+            self.mappersManager.saveObj(workspace)
+            return workspace
+        return False
+
+    def openWorkspace(self, name):
+        dbConnector = self.dbManager.dbOpen(name)
+        if dbConnector:
+            self.mappersManager.createMappers(dbConnector)
+            workspace = self.mappersManager.findObject(name)
+            return workspace
+        return False
+
--- test_cases/workspace_manager.py
+++ test_cases/workspace_manager.py
@@ -10,6 +10,7 @@ import sys
 sys.path.append('.')
 
 from config.configuration import getInstanceConfiguration
+from model.workspace import Workspace
 from managers.model_managers import WorkspaceManager
 from persistence.persistence_managers import DBTYPE
 from mockito import mock, verify, when, any
@@ -31,16 +32,7 @@ class UnitTestWorkspaceManager(unittest.TestCase):
                                         DBTYPE.FS)
         verify(dbManager).dbCreate('test_workspace', DBTYPE.FS)
 
-    def testCreateWorkspaceReturnsWorkspace(self):
-        workspace_manager = WorkspaceManager(mock(), mock())
-        workspace = workspace_manager.createWorkspace('test_workspace', 
-                                        'a test workspace',
-                                        DBTYPE.FS)
-        self.assertTrue(workspace, 'workspace not instantiated')
-        self.assertEquals(workspace.name, 'test_workspace',
-                            'Workspace name not set, is it valid?')
-
-    def testCreateWorkspaceCreateMappers(self):
+    def testCreateWorkspaceCreateMappersAndWorkspace(self):
         dbManager = mock()
         mappersManager = mock()
         dbConnector = mock()
@@ -51,13 +43,96 @@ class UnitTestWorkspaceManager(unittest.TestCase):
         when(mappersManager).createMappers(dbConnector).thenReturn(True) 
 
         workspace_manager = WorkspaceManager(dbManager, mappersManager)
-        workspace_manager.createWorkspace('test_workspace', 'a test workspace',
+        workspace = workspace_manager.createWorkspace('test_workspace', 'a test workspace',
                                         DBTYPE.FS)
 
         verify(mappersManager).createMappers(dbConnector)
         verify(mappersManager).saveObj(any())
 
+        self.assertTrue(workspace, 'workspace not instantiated')
+        self.assertEquals(workspace.name, 'test_workspace',
+                            'Workspace name not set, is it valid?')
+
+    def testCreateExistingWorkspaceReturnsFalse(self):
+        dbManager = mock()
+        mappersManager = mock()
+        dbConnector = mock()
+        mappers = mock()
+
+        when(mappersManager).saveObj(any()).thenReturn(True) 
+        when(dbManager).dbCreate('test_workspace', DBTYPE.FS).thenReturn(False)
+        when(mappersManager).createMappers(dbConnector).thenReturn(True) 
+
+        workspace_manager = WorkspaceManager(dbManager, mappersManager)
+        workspace = workspace_manager.createWorkspace('test_workspace', 'a test workspace',
+                                        DBTYPE.FS)
+
+        verify(dbManager).dbCreate('test_workspace', DBTYPE.FS)
+        verify(mappersManager, times=0).createMappers(dbConnector)
+        verify(mappersManager, times=0).saveObj(any())
+
+    def testOpenWorkspace(self):
+        dbManager = mock()
+        mappersManager = mock()
+        dbConnector = mock()
+        mappers = mock()
+
+        workspace = Workspace('test_workspace', 'a desc')
+
+        when(dbManager).dbOpen('test_workspace').thenReturn(dbConnector)
+        when(mappersManager).createMappers(dbConnector).thenReturn(True)
+        when(mappersManager).findObject('test_workspace').thenReturn(workspace)
+
+        workspace_manager = WorkspaceManager(dbManager, mappersManager)
+
+        opened_workspace = workspace_manager.openWorkspace('test_workspace')
+
+        verify(dbManager).dbOpen('test_workspace')
+        verify(mappersManager).createMappers(dbConnector)
+        verify(mappersManager).findObject('test_workspace')
+        self.assertEquals(opened_workspace.getName(), 'test_workspace')
+
+    def testOpenWorkspaceNoneExisting(self):
+        dbManager = mock()
+        mappersManager = mock()
+        dbConnector = mock()
+        mappers = mock()
+
+        workspace = Workspace('test_workspace', 'a desc') 
+        when(dbManager).dbOpen('test_workspace').thenReturn(False)
+
+        workspace_manager = WorkspaceManager(dbManager, mappersManager)
+        opened_workspace = workspace_manager.openWorkspace('test_workspace')
+
+        verify(dbManager).dbOpen('test_workspace')
+
+        verify(mappersManager, times=0).createMappers(dbConnector)
+        verify(mappersManager, times=0).findObject('test_workspace')
+        self.assertFalse(opened_workspace, 'Workspace retrieved but non existing')
+
+
+    def testRemoveWorkspace(self):
+        dbManager = mock()
+        mappersManager = mock()
+        dbConnector = mock()
+        mappers = mock()
+
+        workspace = Workspace('test_workspace', 'a desc')
+
+        when(dbManager).dbOpen('test_workspace').thenReturn(dbConnector)
+        when(mappersManager).createMappers(dbConnector).thenReturn(True) 
+        when(mappersManager).findObject('test_workspace').thenReturn(workspace)
+
+        workspace_manager = WorkspaceManager(dbManager, mappersManager)
+
+        opened_workspace = workspace_manager.openWorkspace('test_workspace')
+
+        verify(dbManager).dbOpen('test_workspace')
+        verify(mappersManager).createMappers(dbConnector)
+        verify(mappersManager).findObject('test_workspace')
+        self.assertEquals(opened_workspace.getName(), 'test_workspace')
 
 
 if __name__ == '__main__':
     unittest.main()
+

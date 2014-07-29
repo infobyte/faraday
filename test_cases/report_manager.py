45af358c295ae32c6d5a3bc41a204f4e75acac95
danito <fdaniel@infobytesec.com>
2014-07-29 11:56:52 -0300

    Adding watch and startWatch support

--- test_cases/report_manager.py
+++ test_cases/report_manager.py
@@ -11,11 +11,13 @@ sys.path.append('.')
 
 from config.configuration import getInstanceConfiguration
 from model.workspace import Workspace
-from managers.reports_managers import ReportManager
+from managers.reports_managers import ReportManager, NoReportsWatchException
 
 from persistence.persistence_managers import DBTYPE
 from mockito import mock, verify, when, any
 CONF = getInstanceConfiguration()
+
+from test_cases import common
 
 class UnitTestWorkspaceManager(unittest.TestCase):
     def testCreateReportManager(self):
@@ -24,7 +26,21 @@ class UnitTestWorkspaceManager(unittest.TestCase):
 
         self.assertIsNotNone(report_manager)
 
+    def testWatchReportPath(self):
+        import os.path 
+        import os
+        workspace_name = common.new_random_workspace_name()
+        timer = 10
 
+        report_manager = ReportManager(timer, mock())
+        report_manager.watch(workspace_name)
+
+        self.assertTrue(os.path.exists(os.path.join(CONF.getReportPath(),
+                            workspace_name)), 'Report directory not found')
+        
+    def testStartReportNoPathRunsException(self): 
+        report_manager = ReportManager(0, mock())
+        self.assertRaises(NoReportsWatchException, report_manager.startWatch) 
 
 if __name__ == '__main__':
     unittest.main()

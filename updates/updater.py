c15a31b7b4171c9a9ff535fa72717214a6dba7a0
root <root@localhost.localdomain>
2014-09-16 04:33:21 +0000

    Fixes on updater
    Deminish log verbosity
    wrong varnames updates

--- updates/updater.py
+++ updates/updater.py
@@ -25,7 +25,8 @@ class Updater(object):
         logger.info('Removing old pyc files')
         subprocess.call(['find', '.', '-name', '*.pyc', '-delete'])
         logger.info('Pulling latest Github Master copy')
-        subprocess.call(['git', 'pull'])
+        if query_yes_no('Proceed?', 'yes'):
+            subprocess.call(['git', 'pull'])
         logger.info('Installing missing dependencies in pip')
         pip.main(['install', '-r', CONST_REQUIREMENTS_FILE, '--user'])
         logger.info('Upgrading DBs to latest version')
@@ -37,47 +38,22 @@ class Update(object):
 class DB(Update): 
     def __init__(self):
         pass
-    
-    def run(self):
+
+    def update_db(self, db_name):
+        if 'backup' in db_name:
+            logger.info('Database [%s] is a backup, ignoring' % db_name)
+            
         source_server = CONF.getCouchURI()
-        if not source_server:
-            logger.info("""No DB configuration found.
-                    To upgrade your DB please configure a valid CouchDB URI in:
-                    ~/.faraday/config/user.xml configuration file.""")
-            return
-
-        serv = couchdbkit.Server(source_server)
-
-        logger.info('We are about to upgrade dbs in Server [%s]' % source_server)
-        dbs = filter(lambda x: not x.startswith("_") and 'backup' not in x, serv.all_dbs())
-        logger.info('Dbs to upgrade: %s' % (', '.join(dbs)))
-
-        if not query_yes_no('Proceed?', 'no'):
-            return
-
-        logger.info('Preparing updates on Couchdbs')
-        processed = 0
-        logger.info('About to upgrade %d dbs' % len(dbs))
-        for db_name in dbs:
-            logger.info('Updating db %s' % db_name)
-            try:
-                update_db(db_name)
-                processed = processed + 1
-            except Exception as e:
-                logger.error(e) 
-            logger.info('Updated DB [%s]. %d remaining' % (db_name, len(dbs) - processed)) 
-        logger.info("Update process finish, be kind to review the process.\nBackuped databases won't be accesible") 
-
-    def update_db(db_name):
         # Levanto los servidores 
         db_source = couchdbkit.Database("/".join((source_server, db_name)))
 
         # Primero replico para no cagarla
         logger.info('Creating db backup: %s' % ('%s-backup' % db_name))
         db_source.server.replicate(db_name, '%s-backup' % db_name, create_target = True)
+        import time
+        time.sleep(1)
         db_bkp = couchdbkit.Database("/".join((source_server,
                                             '%s-backup' % db_name)))
-
         if db_source.doc_exist(db_name): 
             logger.info('DB: [%s] Already had suffer migration' % db_name)
             return
@@ -102,7 +78,7 @@ class DB(Update):
                 continue 
             elif doc['type'] == 'Workspace':
                 # Already modified
-                break
+                continue
             else: 
                 # Modify the parent ID
                 parent = doc['parent']
@@ -112,7 +88,10 @@ class DB(Update):
                     l_parent = doc['_id'].split('.')[:-1]
                     parent = '.'.join(l_parent) 
                 doc['parent'] = parent
-                doc['owned'] = eval(doc['owned'])
+                if doc['owned'] == '':
+                    doc['owned'] == False
+                else: 
+                    doc['owned'] = eval(doc['owned'])
 
                 document['doc'] = doc 
                 db_source.save_doc(doc, force_update = True)
@@ -121,3 +100,33 @@ class DB(Update):
 
         logger.info("Transformed %s objects" % str(types))
 
+    
+    def run(self):
+        source_server = CONF.getCouchURI()
+        if not source_server:
+            logger.info("""No DB configuration found.
+                    To upgrade your DB please configure a valid CouchDB URI in:
+                    ~/.faraday/config/user.xml configuration file.""")
+            return
+
+        serv = couchdbkit.Server(source_server)
+
+        logger.info('We are about to upgrade dbs in Server [%s]' % source_server)
+        dbs = filter(lambda x: not x.startswith("_") and 'backup' not in x, serv.all_dbs())
+        logger.info('Dbs to upgrade: %s' % (', '.join(dbs)))
+
+        if not query_yes_no('Proceed?', 'no'):
+            return
+
+        logger.info('Preparing updates on Couchdbs')
+        processed = 0
+        logger.info('About to upgrade %d dbs' % len(dbs))
+        for db_name in dbs:
+            logger.info('Updating db %s' % db_name)
+            try:
+                self.update_db(db_name)
+                processed = processed + 1
+            except Exception as e:
+                logger.error(e) 
+            logger.info('Updated DB [%s]. %d remaining' % (db_name, len(dbs) - processed)) 
+        logger.info("Update process finish, be kind to review the process.\nBackuped databases won't be accesible") 

a7e55ddb34a98432e4794c3b7cdd89ef2f0c8dd6
micabot <mranea@infobytesec.com>
2014-10-07 17:29:25 -0300

    ***BEWARE*** this commit modifies CouchDB's view for vulnerabilities, open Faraday and select WS before using Test #669 - Adds CouchDB vuln removal from UI

--- views/_design/vulns/views/all/map.js
+++ views/_design/vulns/views/all/map.js
@@ -3,6 +3,6 @@
 // See the file 'doc/LICENSE' for the license information
 function(doc) {
       if(doc.type=="Vulnerability" || doc.type=="VulnerabilityWeb"){
-              emit(doc._id,{"date":doc.metadata.create_time, "status":doc.type, "name":doc.name, "desc":doc.desc, "severity": doc.severity, "parent": doc.parent});
+              emit(doc._id,{"date":doc.metadata.create_time, "status":doc.type, "name":doc.name, "desc":doc.desc, "severity": doc.severity, "parent": doc.parent, "rev": doc._rev});
                 }
 }

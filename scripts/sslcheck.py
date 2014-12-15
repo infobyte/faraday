8c0fcff9295e310155341304dc96573ba49f4133
danito <fdaniel@infobytesec.com>
2014-12-15 11:04:29 -0300

    SSL Check license header
    This was missing

--- scripts/sslcheck.py
+++ scripts/sslcheck.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python2	
+'''
+Faraday Penetration Test IDE - Community Version
+Copyright (C) 2013  Infobyte LLC (http://www.infobytesec.com/)
+See the file 'doc/LICENSE' for the license information
+
+'''
 
 #libraries
 import subprocess								

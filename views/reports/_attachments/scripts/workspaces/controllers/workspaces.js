0805b3e9c10b8f067bf60d14412c7cae153294a5
umpacu <rgerman@infobytesec.com>
2014-12-16 14:25:21 -0300

    Sanitizing location.hash

--- views/reports/_attachments/scripts/workspaces/controllers/workspaces.js
+++ views/reports/_attachments/scripts/workspaces/controllers/workspaces.js
@@ -3,4 +3,12 @@ angular.module('faradayApp')
         workspacesFact.get(function(wss) {
             $scope.wss = wss;
         });
+        var hash_tmp = window.location.hash.split("/")[1];
+        switch (hash_tmp){
+            case "status":
+                $scope.hash = "status";
+                break;
+            default:
+                $scope.hash = "";
+        }
     }]);

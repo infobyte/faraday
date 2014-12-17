198ae430c6a9f48238d94ccd043561e3bf961cfc
danito <fdaniel@infobytesec.com>
2014-12-17 14:53:42 -0300

    Workspace insert loads scope
    But it won't be calling the factory yet

--- views/reports/tests/faradayApp/components/workspaces/controllers/workspace_test.js
+++ views/reports/tests/faradayApp/components/workspaces/controllers/workspace_test.js
@@ -19,15 +19,35 @@ describe('workspacesCtrl', function() {
         inject(function(_$rootScope_, _$controller_, _workspacesFact_){
             // The injector unwraps the underscores (_) from around the parameter names when matching
             $scope = _$rootScope_.$new();
-            $controller = _$controller_('workspacesCtrl', { $scope: $scope, workspacesFact: _workspacesFact_});
+            $controller = _$controller_('workspacesCtrl',
+                    { $scope: $scope, workspacesFact: _workspacesFact_});
         });
     });
 
 
-
-    describe('$scope.wss', function() {
+    describe('Workspaces load in $scope.wss', function() {
         it('tests if wss is loaded properly', function() {
             expect($scope.wss).toEqual(['ws1', 'ws2']);
+        });
+    });
+    describe('Workspaces inserts in $scope.wss', function() { 
+        it('tests if wss is updated properly', function() {
+            workspace_name = 'test_workspace';
+            workspace = {
+                "_id": workspace_name,
+                "_rev": "2-bd88abf79cf2b7e8b419cd4387c64bef",
+                "customer": "",
+                "sdate": 1410832741.48194,
+                "name": workspace_name,
+                "fdate": 1410832741.48194,
+                "type": "Workspace",
+                "children": [
+                ],
+                "description": ""
+            };
+            $scope.insert(workspace);
+            // http://jasmine.github.io/1.3/introduction.html#section-Matchers
+            expect($scope.wss).toContain(workspace_name);
         });
     });
 });

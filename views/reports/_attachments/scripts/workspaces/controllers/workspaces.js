87a84d7b0944a5d94a2e3b094589ded8f41a5e8d
danito <fdaniel@infobytesec.com>
2014-12-17 14:53:42 -0300

    Workspace insert loads scope
    But it won't be calling the factory yet

--- views/reports/_attachments/scripts/workspaces/controllers/workspaces.js
+++ views/reports/_attachments/scripts/workspaces/controllers/workspaces.js
@@ -1,6 +1,11 @@
 angular.module('faradayApp')
-    .controller('workspacesCtrl', ['$scope', 'workspacesFact', function($scope, workspacesFact) {
+    .controller('workspacesCtrl', ['$scope', 'workspacesFact',
+            function($scope, workspacesFact) {
         workspacesFact.get(function(wss) {
             $scope.wss = wss;
         });
+        
+        $scope.insert = function(workspace){
+            $scope.wss.push(workspace.name);
+        };
     }]);

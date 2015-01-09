309c091bac40bdd5612f33447b5ff4c091cfbb93
umpacu <rgerman@infobytesec.com>
2015-01-09 18:05:29 -0300

    Fix left bar to change the links based on route

--- views/reports/_attachments/scripts/navigation/controllers/navigationCtrl.js
+++ views/reports/_attachments/scripts/navigation/controllers/navigationCtrl.js
@@ -1,4 +1,9 @@
 angular.module('faradayApp')
-    .controller('navigationCtrl', function($scope) {
+    .controller('navigationCtrl', ['$scope', '$route', '$routeParams',
+        function($scope, $route, $routeParams) {
 
-	});
+            $scope.$on('$routeChangeSuccess', function(){
+                $scope.workspace = $routeParams.wsId;
+            });
+
+	}]);

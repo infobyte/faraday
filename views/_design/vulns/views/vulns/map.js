290a61333567e0ee6095be5015080c565ace9654
micabot <mranea@infobytesec.com>
2015-01-20 10:54:41 -0300

    Adding attachments field to CouchDB views

--- views/_design/vulns/views/vulns/map.js
+++ views/_design/vulns/views/vulns/map.js
@@ -4,19 +4,20 @@
 function(doc) {
     if(doc.type == "Vulnerability"){
         var obj = {
-            "rev":      doc._rev,
-            "data":     doc.data,
-            "date":     doc.metadata.create_time, 
-            "desc":     doc.desc, 
-            "meta":     doc.metadata,
-            "name":     doc.name, 
-            "oid":      doc.obj_id,
-            "owned":    doc.owned,
-            "owner":    doc.owner,
-            "parent":   doc.parent, 
-            "refs":     doc.refs,
-            "severity": doc.severity, 
-            "type":   doc.type 
+            "rev":          doc._rev,
+            "attachments":  doc._attachments,
+            "data":         doc.data,
+            "date":         doc.metadata.create_time, 
+            "desc":         doc.desc, 
+            "meta":         doc.metadata,
+            "name":         doc.name, 
+            "oid":          doc.obj_id,
+            "owned":        doc.owned,
+            "owner":        doc.owner,
+            "parent":       doc.parent, 
+            "refs":         doc.refs,
+            "severity":     doc.severity, 
+            "type":         doc.type 
         };
         emit(doc._id, obj);
     }

296351c1d3b4f6c13917cc8ea489968eaa2efa10
umpacu <rgerman@infobytesec.com>
2015-01-27 12:20:38 -0300

    Adding services count summary

---
+++ views/reports/_attachments/scripts/dashboard/controllers/sumarizedCtrl.js
@@ -0,0 +1,19 @@
+angular.module('faradayApp')
+    .controller('sumarizedCtrl', 
+        ['$scope', '$route', '$routeParams', 'dashboardSrv',
+        function($scope, $route, $routeParams, dashboardSrv) {
+            //current workspace
+            var workspace = $routeParams.wsId;
+            $scope.servicesCount = [];
+
+            if (workspace != undefined){
+                dashboardSrv.getServicesCount(workspace).then(function(res){
+                    res.sort(function(a, b){
+                        return b.value - a.value;
+                    });
+                    $scope.servicesCount = res;
+
+                });
+            }
+
+    }]);

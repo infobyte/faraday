50d7db5521971e2b0f2da9d097731c188cef1d32
micabot <mranea@infobytesec.com>
2015-02-26 18:11:16 -0300

    Persistent cookie for currentWorkspace

--- views/reports/_attachments/scripts/navigation/controllers/navigationCtrl.js
+++ views/reports/_attachments/scripts/navigation/controllers/navigationCtrl.js
@@ -1,12 +1,30 @@
 angular.module('faradayApp')
-    .controller('navigationCtrl', ['$scope', '$route', '$routeParams', '$cookieStore', '$location',
-        function($scope, $route, $routeParams, $cookieStore, $location) {
+    .controller('navigationCtrl', ['$scope', '$route', '$routeParams', '$cookies', '$location',
+        function($scope, $route, $routeParams, $cookies, $location) {
 
-            $scope.$on('$routeChangeSuccess', function(){
-                if($routeParams.wsId != undefined) {
-                    $scope.workspace = $routeParams.wsId;
-                    $cookieStore.put('currentUrl', $location.path());
-                }
-            });
+        $scope.workspace = "";
+
+        $scope.$on('$routeChangeSuccess', function(){
+            if($routeParams.wsId != undefined) {
+                $scope.workspace = $routeParams.wsId;
+                $cookies.currentUrl = $location.path();
+            }
+        });
+
+        $scope.loadCurrentWorkspace = function() {
+            var pos = -1;
+
+            if($cookies.currentUrl != undefined) {
+                pos = $cookies.currentUrl.indexOf('ws/');
+            }
+
+            if($routeParams.wsId != undefined) {
+                $scope.workspace = $routeParams.wsId;
+            } else if(pos >= 0) {
+                $scope.workspace = $cookies.currentUrl.slice(pos+3);
+            }
+        };
+
+        $scope.loadCurrentWorkspace();
 
 	}]);

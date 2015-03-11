0453d36526035ec95406da277df3537fb9ffafb5
micabot <mranea@infobytesec.com>
2015-03-11 19:12:04 -0300

    Added uploadViews method to WS service.

--- views/reports/_attachments/scripts/workspaces/services/workspaces.js
+++ views/reports/_attachments/scripts/workspaces/services/workspaces.js
@@ -37,7 +37,8 @@ angular.module('faradayApp')
 
         workspacesFact.put = function(workspace) {
             return createDatabase(workspace).
-                then(function(resp){ createWorkspaceDoc(resp, workspace);}, errorHandler);
+                then(function(resp) { createWorkspaceDoc(resp, workspace); }, errorHandler).
+                then(function(resp) { uploadViews(workspace); }, errorHandler);
         };
 
         createDatabase = function(workspace){
@@ -45,10 +46,33 @@ angular.module('faradayApp')
         };
 
         createWorkspaceDoc = function(response, workspace){
-            return ($http.put(BASEURL + workspace.name + '/' + workspace.name,
-                        workspace). success(function(data){ 
-                workspace._rev = response.data.rev;
-            }));
+            $http.put(BASEURL + workspace.name + '/' + workspace.name, workspace).
+                success(function(data){ 
+                    workspace._rev = response.data.rev;
+                }).
+                error(function(data) {
+                    errorHandler;
+                });
+        };
+
+        uploadViews = function(workspace) {
+            $http.get(BASEURL + 'reports/_design/reports').
+                success(function(data) {
+                    var views = {},
+                    attachments = data._attachments;
+                    if(Object.keys(attachments).length > 0) {
+                        for(var prop in attachments) {
+                            if(attachments.hasOwnProperty(prop)) {
+                                if(prop.indexOf("views/") > -1) {
+                                    views[prop] = attachments[prop];
+                                }
+                            }
+                        }
+                    }
+                }).
+                error(function(data) {
+                    errorHandler;
+                });
         };
 
         workspacesFact.update = function(workspace, onSuccess) {

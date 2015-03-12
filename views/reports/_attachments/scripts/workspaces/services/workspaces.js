2b229705a2f64f1e1794dd35f4652a8b5cdf17f0
micabot <mranea@infobytesec.com>
2015-03-12 18:58:13 -0300

    Added logic to get attachments from reports in uploadViews

--- views/reports/_attachments/scripts/workspaces/services/workspaces.js
+++ views/reports/_attachments/scripts/workspaces/services/workspaces.js
@@ -56,23 +56,79 @@ angular.module('faradayApp')
         };
 
         uploadViews = function(workspace) {
-            $http.get(BASEURL + 'reports/_design/reports').
+            /*
+            //curl -vX POST http://localhost:5984/aaaaaaa123123/_bulk_docs -d "{"docs":[{"_id":"0","integer":0,"string":"0"},{"_id":"1","integer":1,"string":"1"},{"_id":"2","integer":2,"string":"2"}]}"
+
+            var data = {
+                "docs": [
+                    {"_id": "0", "integer": 0, "string": "0"},
+                    {"_id": "1", "integer": 1, "string": "1"},
+                    {"_id": "2", "integer": 2, "string": "2"},
+                    {"_id": "3", "integer": 3, "string": "3"}
+                ]
+            };
+
+            $http.post('http://localhost:5984/aaaaaaa123123/_bulk_docs', JSON.stringify(data)).
+                then(function(lala) {
+                    console.log(lala);
+                });
+            */
+
+            var bulk = {"docs": []},
+            paths = {},
+            reports = BASEURL + 'reports/_design/reports',
+            tree = {};
+            $http.get(reports).
                 success(function(data) {
-                    var views = {},
-                    attachments = data._attachments;
+                    var attachments = data._attachments;
                     if(Object.keys(attachments).length > 0) {
                         for(var prop in attachments) {
                             if(attachments.hasOwnProperty(prop)) {
                                 if(prop.indexOf("views/") > -1) {
-                                    views[prop] = attachments[prop];
+                                    paths[prop] = $http.get(reports + "/" + prop);
                                 }
                             }
                         }
                     }
+                    $q.all(paths).then(function(resp) {
+                        for(var path in paths) {
+                            if(paths.hasOwnProperty(path)) {
+                                var parts = path.split("/"), 
+                                component = parts[1], 
+                                name = parts[3], 
+                                file = parts[4].split(".")[0],
+                                fileObj = Object(),
+                                nameObj = Object();
+
+                                if(tree.hasOwnProperty(component)) {
+                                    if(tree[component].hasOwnProperty(name)) {
+                                        tree[component][name][file] = resp[path]["data"]; 
+                                    } else {
+                                        fileObj[file] = resp[path]["data"];
+                                        tree[component][name] = fileObj;
+                                    }
+                                } else {
+                                    fileObj[file] = resp[path].data;
+                                    nameObj[name] = fileObj;
+                                    tree[component] = nameObj;
+                                }
+                            }
+                        }
+                    }, errorHandler);
                 }).
                 error(function(data) {
                     errorHandler;
                 });
+        };
+
+        hasDocument = function(list, name) {
+            var ret = false;
+            list.forEach(function(item) {
+                if(item._id == name) {
+                    ret = true;
+                }
+            });
+            return ret;
         };
 
         workspacesFact.update = function(workspace, onSuccess) {

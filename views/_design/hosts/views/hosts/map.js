d37124b9ef9f580f5529700fc2835fb0acb5d2d0
micabot <mranea@infobytesec.com>
2015-03-12 20:07:44 -0300

    Removing old views files

--- views/_design/hosts/views/hosts/map.js
+++
@@ -1,8 +0,0 @@
-// Faraday Penetration Test IDE
-// Copyright (C) 2013  Infobyte LLC (http://www.infobytesec.com/)
-// See the file 'doc/LICENSE' for the license information
-function(doc) {
-  if(doc.type=="Host"){
-    emit(doc._id, {"name": doc.name, "os": doc.os, "owned": doc.owned});
-  }
-}

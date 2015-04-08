f76cda4e576d3fc79da3a5dbbfd9d6437fc809fe
Franco Linares <flinares@infobytesec.com>
2015-04-08 11:33:54 -0300

    Add alert if you are using iceweasel

--- views/reports/_attachments/scripts/index/controllers/indexCtrl.js
+++ views/reports/_attachments/scripts/index/controllers/indexCtrl.js
@@ -2,6 +2,7 @@ angular.module('faradayApp')
     .controller('indexCtrl', 
         ['$scope', '$http', 'BASEURL',
         function($scope, $http, BASEURL) {
+        	$scope.isIceweasel = undefined;
         	if(navigator.userAgent.toLowerCase().indexOf('iceweasel') > -1) {
         		$scope.isIceweasel = true;
 			}

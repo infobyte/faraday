ec6dddad148c434c27226a2f9ce5654df3027792
Franco Linares <flinares@infobytesec.com>
2015-04-08 15:20:16 -0300

    Fix bug of alerts on index
    Added function on navigationCtrl. Added HTML code on leftBar.html.

--- views/reports/_attachments/scripts/index/controllers/indexCtrl.js
+++ views/reports/_attachments/scripts/index/controllers/indexCtrl.js
@@ -2,9 +2,9 @@ angular.module('faradayApp')
     .controller('indexCtrl', 
         ['$scope', '$http', 'BASEURL',
         function($scope, $http, BASEURL) {
-   //      	$scope.isIceweasel = undefined;
-   //      	if(navigator.userAgent.toLowerCase().indexOf('iceweasel') > -1) {
-   //      		$scope.isIceweasel = true;
-			// }
+
+			$http.get('/reports/_design/reports/scripts/config/config.json').then(function(conf){
+				$scope.version = conf.data.ver;
+			});
 
         }]);
--- views/reports/_attachments/scripts/navigation/controllers/navigationCtrl.js
+++ views/reports/_attachments/scripts/navigation/controllers/navigationCtrl.js
@@ -3,8 +3,8 @@
 // See the file 'doc/LICENSE' for the license information
 
 angular.module('faradayApp')
-    .controller('navigationCtrl', ['$scope', '$route', '$routeParams', '$cookies', '$location',
-        function($scope, $route, $routeParams, $cookies, $location) {
+    .controller('navigationCtrl', ['$scope', '$http','$route', '$routeParams', '$cookies', '$location',
+        function($scope, $http, $route, $routeParams, $cookies, $location) {
 
         $scope.workspace = "";
 
@@ -30,5 +30,23 @@ angular.module('faradayApp')
         };
 
         $scope.loadCurrentWorkspace();
+
+        if(navigator.userAgent.toLowerCase().indexOf('iceweasel') > -1) {
+             $scope.isIceweasel = "Your browser is not supported, please use Firefox or Chrome";
+        }
+        $http.get('/reports/_design/reports/scripts/config/config.json').then(function(conf){
+            var dt_expiration = new Date(conf.data.cdate);
+            var today = new Date(),
+                dt_days;
+
+            dt_expiration.setFullYear(dt_expiration.getFullYear()+1);
+            var timeDiff = Math.abs(dt_expiration.getTime() - today.getTime());
+            var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            if (diffDays < 30){
+                $scope.expiration = "Your current license is due to expire soon.</br>Please visit <a href='https://www.faradaysec.com/users/' target='_blank'>https://www.faradaysec.com/users/</a> to extend your license before it expires.";
+            }else if(today > dt_expiration){
+                $scope.expiration = "Your license has expired.</br>Please visit <a href='https://www.faradaysec.com/users/' target='_blank'>https://www.faradaysec.com/users/</a> to extend your license.";
+            }
+        });
 
 	}]);

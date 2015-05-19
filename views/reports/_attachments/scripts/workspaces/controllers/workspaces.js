332ffaf66fcf07f362a129eaba4f945ec0593066
Franco Linares <flinares@infobytesec.com>
2015-05-19 15:17:07 -0300

    Fix datepicker bug
    On modal new of workspace.

--- views/reports/_attachments/scripts/workspaces/controllers/workspaces.js
+++ views/reports/_attachments/scripts/workspaces/controllers/workspaces.js
@@ -291,11 +291,6 @@ angular.module('faradayApp')
             $scope.dt = null;
         };
 
-        // Disable weekend selection
-        $scope.disabled = function(date, mode) {
-            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-        };
-
         $scope.minDate = new Date();
 
         $scope.open = function($event, isStart) {

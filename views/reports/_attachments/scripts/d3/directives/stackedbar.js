a6d4b7ffab4ca7d0b5eee2112a0eeade60a4494b
micabot <mranea@infobytesec.com>
2015-06-05 19:00:03 -0300

    Refactor D3 directive for stacked bars
    This new code works as expected and is clearer than the last three.
    Horizontal stacked bars FTW!

--- views/reports/_attachments/scripts/d3/directives/stackedbar.js
+++ views/reports/_attachments/scripts/d3/directives/stackedbar.js
@@ -53,15 +53,174 @@ angular.module('faradayApp')
                                 });
 */
 
+/*
+from here horizontal stacked bars, according to the interwebz this is a nice solution
+*/
+
+    var margins = {
+        top: 12,
+        left: 48,
+        right: 24,
+        bottom: 24
+    },
+    legendPanel = {
+        width: 180
+    },
+    width = 500 - margins.left - margins.right - legendPanel.width,
+    height = 100 - margins.top - margins.bottom,
+    dataset = [{
+        data: [{
+            month: 'Oct',
+            count: 345
+        }],
+        name: 'Series #1'
+    }, {
+        data: [{
+            month: 'Oct',
+            count: 573
+        }],
+        name: 'Series #4'
+    }, {
+        data: [{
+            month: 'Oct',
+            count: 573
+        }],
+        name: 'Series #2'
+    }
+
+    ],
+    series = dataset.map(function (d) {
+        return d.name;
+    }),
+    dataset = dataset.map(function (d) {
+        return d.data.map(function (o, i) {
+            // Structure it so that your numeric
+            // axis (the stacked amount) is y
+            return {
+                y: o.count,
+                x: o.month
+            };
+        });
+    }),
+    stack = d3.layout.stack();
+
+    stack(dataset);
+
+    var dataset = dataset.map(function (group) {
+        return group.map(function (d) {
+            // Invert the x and y values, and y0 becomes x0
+            return {
+                x: d.y,
+                y: d.x,
+                x0: d.y0
+            };
+        });
+    }),
+    svg = d3.select(".stackedbars")
+        .append('svg')
+        .attr('width', width + margins.left + margins.right + legendPanel.width)
+        .attr('height', height + margins.top + margins.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
+    xMax = d3.max(dataset, function (group) {
+        return d3.max(group, function (d) {
+            return d.x + d.x0;
+        });
+    }),
+    xScale = d3.scale.linear()
+        .domain([0, xMax])
+        .range([0, width]),
+    months = dataset[0].map(function (d) {
+        return d.y;
+    }),
+    _ = console.log(months),
+    yScale = d3.scale.ordinal()
+        .domain(months)
+        .rangeRoundBands([0, height], .1),
+    xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom'),
+    yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient('left'),
+    colours = d3.scale.category10(),
+    groups = svg.selectAll('g')
+        .data(dataset)
+        .enter()
+        .append('g')
+        .style('fill', function (d, i) {
+        return colours(i);
+    }),
+    rects = groups.selectAll('rect')
+        .data(function (d) {
+        return d;
+    })
+        .enter()
+        .append('rect')
+        .attr('x', function (d) {
+        return xScale(d.x0);
+    })
+        .attr('y', function (d, i) {
+        return yScale(d.y);
+    })
+        .attr('height', function (d) {
+        return yScale.rangeBand();
+    })
+        .attr('width', function (d) {
+        return xScale(d.x);
+    })
+        .on('mouseover', function (d) {
+        var xPos = parseFloat(d3.select(this).attr('x')) / 2 + width / 2;
+        var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand() / 2;
+
+        d3.select('#tooltip')
+            .style('left', xPos + 'px')
+            .style('top', yPos + 'px')
+            .select('#value')
+            .text(d.x);
+
+        d3.select('#tooltip').classed('hidden', false);
+    })
+    .on('mouseout', function () {
+        d3.select('#tooltip').classed('hidden', true);
+    });
+
+    svg.append('rect')
+        .attr('fill', 'yellow')
+        .attr('width', 160)
+        .attr('height', 30 * dataset.length)
+        .attr('x', width + margins.left)
+        .attr('y', 0);
+
+    series.forEach(function (s, i) {
+        svg.append('text')
+            .attr('fill', 'black')
+            .attr('x', width + margins.left + 8)
+            .attr('y', i * 24 + 24)
+            .text(s);
+        svg.append('rect')
+            .attr('fill', colours(i))
+            .attr('width', 60)
+            .attr('height', 20)
+            .attr('x', width + margins.left + 90)
+            .attr('y', i * 24 + 6);
+    });
+
+/*
+up to here horizontal stacked bars, according to the interwebz this is a nice solution
+*/
+
+/*
+from here nasty horizontal stacked bars
 
 var data = [
-{"key":"FL", "pop1":3000, "pop2":4000, "pop3":5000},
-{"key":"CA", "pop1":3000, "pop2":3000, "pop3":3000},
-{"key":"NY", "pop1":12000, "pop2":5000, "pop3":13000},
-{"key":"NC", "pop1":8000, "pop2":21000, "pop3":11000},
-{"key":"SC", "pop1":30000, "pop2":12000, "pop3":8000},
-{"key":"AZ", "pop1":26614, "pop2":6944, "pop3":30778},
-{"key":"TX", "pop1":8000, "pop2":12088, "pop3":20000}
+    {"key":"FL", "pop1":3000, "pop2":4000, "pop3":5000},
+    {"key":"CA", "pop1":3000, "pop2":3000, "pop3":3000},
+    {"key":"NY", "pop1":12000, "pop2":5000, "pop3":13000},
+    {"key":"NC", "pop1":8000, "pop2":21000, "pop3":11000},
+    {"key":"SC", "pop1":30000, "pop2":12000, "pop3":8000},
+    {"key":"AZ", "pop1":26614, "pop2":6944, "pop3":30778},
+    {"key":"TX", "pop1":8000, "pop2":12088, "pop3":20000}
 ];
  
 var n = 3, // number of layers
@@ -132,8 +291,17 @@ svg.append("g")
     .attr("class", "y axis")
     .call(yAxis);
 
+up to here nasty horizontal stacked bars
+*/
+
+
+
+
 /*
-from here simple horizontal stacked bars
+from here simple vertical stacked bars
+*/
+
+/*
 var sales = [
   {
     name: "Hoodie",
@@ -223,6 +391,9 @@ layers.selectAll('rect')
         // so we subtract from `height`
         return height - y(d.y)
       });
+*/
+
+/*
 up to here, simple stacked vertical bars
 */
 

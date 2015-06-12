3eef631f01344fa77302e328a55000fb1227dcab
micabot <mranea@infobytesec.com>
2015-06-12 16:39:27 -0300

    Fix for box width in dashboard

--- views/reports/_attachments/scripts/d3/directives/stackedbar.js
+++ views/reports/_attachments/scripts/d3/directives/stackedbar.js
@@ -22,102 +22,104 @@ angular.module('faradayApp')
                         });
 
                         scope.render = function(data) {
-                            d3.select('.stackedbars').selectAll('svg').remove();
+                            scope.$on("vulnsByPriceDataReady", function(e) {
+                                d3.select('.stackedbars').selectAll('svg').remove();
 
-                            var margins = {
-                                top: 12,
-                                left: 24,
-                                right: 12,
-                                bottom: 12
-                            };
+                                var margins = {
+                                    top: 12,
+                                    left: 24,
+                                    right: 12,
+                                    bottom: 12
+                                };
 
-                            pwidth = ele.parent().width();
+                                pwidth = ele.parent().width();
 
-                            width = pwidth * 0.9,
-                            height = 80 - margins.top - margins.bottom;
-                            dataset = data.map(function(d) {
-                                    return [{
-                                        k: d.key,
-                                        c: d.color,
-                                        y: d.value,
-                                        x: 0
-                                    }];
-                            });
-                            stack = d3.layout.stack();
-
-                            stack(dataset);
-
-                            var dataset = dataset.map(function (group) {
-                                return group.map(function (d) {
-                                    // Invert the x and y values, and y0 becomes x0
-                                    return {
-                                        k: d.k,
-                                        c: d.c,
-                                        x: d.y,
-                                        y: d.x,
-                                        x0: d.y0
-                                    };
+                                width = pwidth * 0.9,
+                                height = 80 - margins.top - margins.bottom;
+                                dataset = data.map(function(d) {
+                                        return [{
+                                            k: d.key,
+                                            c: d.color,
+                                            y: d.value,
+                                            x: 0
+                                        }];
                                 });
-                            }),
-                            svg = d3.select(".stackedbars")
-                                .append('svg')
-                                .attr('width', width + margins.left + margins.right)
-                                .attr('height', height + margins.top + margins.bottom)
-                                .append('g')
-                                .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
-                            xMax = d3.max(dataset, function (group) {
-                                return d3.max(group, function (d) {
-                                    return d.x + d.x0;
-                                });
-                            }),
-                            xScale = d3.scale.linear()
-                                .domain([0, xMax])
-                                .range([0, width]),
-                            months = dataset[0].map(function (d) {
-                                return d.y;
-                            }),
-                            yScale = d3.scale.ordinal()
-                                .domain(months)
-                                .rangeRoundBands([0, height], .1),
-                            xAxis = d3.svg.axis()
-                                .scale(xScale)
-                                .orient('bottom'),
-                            yAxis = d3.svg.axis()
-                                .scale(yScale)
-                                .orient('left'),
-                            colours = d3.scale.category10(),
-                            groups = svg.selectAll('g')
-                                .data(dataset)
-                                .enter()
-                                .append('g')
-                                .style('fill', function (d, i) {
-                                return d[0].c;
-                            }),
-                            rects = groups.selectAll('rect')
-                                .data(function (d) {
-                                return d;
-                            })
-                                .enter()
-                                .append('rect')
-                                .attr('x', function (d) {
-                                return xScale(d.x0);
-                            })
-                                .attr('y', function (d, i) {
-                                return yScale(d.y);
-                            })
-                                .attr('height', function (d) {
-                                return yScale.rangeBand();
-                            })
-                                .attr('width', function (d) {
-                                return xScale(d.x);
-                            })
-                                .attr('tooltip-append-to-body', true)
-                                .attr('tooltip', function(d) {
-                                    return d.k + " sums $" + d.x;
-                            });
+                                stack = d3.layout.stack();
 
-                            ele.removeAttr("d3-horizontal-stacked-bar");
-                            $compile(ele)(scope);
+                                stack(dataset);
+
+                                var dataset = dataset.map(function (group) {
+                                    return group.map(function (d) {
+                                        // Invert the x and y values, and y0 becomes x0
+                                        return {
+                                            k: d.k,
+                                            c: d.c,
+                                            x: d.y,
+                                            y: d.x,
+                                            x0: d.y0
+                                        };
+                                    });
+                                }),
+                                svg = d3.select(".stackedbars")
+                                    .append('svg')
+                                    .attr('width', width + margins.left + margins.right)
+                                    .attr('height', height + margins.top + margins.bottom)
+                                    .append('g')
+                                    .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
+                                xMax = d3.max(dataset, function (group) {
+                                    return d3.max(group, function (d) {
+                                        return d.x + d.x0;
+                                    });
+                                }),
+                                xScale = d3.scale.linear()
+                                    .domain([0, xMax])
+                                    .range([0, width]),
+                                months = dataset[0].map(function (d) {
+                                    return d.y;
+                                }),
+                                yScale = d3.scale.ordinal()
+                                    .domain(months)
+                                    .rangeRoundBands([0, height], .1),
+                                xAxis = d3.svg.axis()
+                                    .scale(xScale)
+                                    .orient('bottom'),
+                                yAxis = d3.svg.axis()
+                                    .scale(yScale)
+                                    .orient('left'),
+                                colours = d3.scale.category10(),
+                                groups = svg.selectAll('g')
+                                    .data(dataset)
+                                    .enter()
+                                    .append('g')
+                                    .style('fill', function (d, i) {
+                                    return d[0].c;
+                                }),
+                                rects = groups.selectAll('rect')
+                                    .data(function (d) {
+                                    return d;
+                                })
+                                    .enter()
+                                    .append('rect')
+                                    .attr('x', function (d) {
+                                    return xScale(d.x0);
+                                })
+                                    .attr('y', function (d, i) {
+                                    return yScale(d.y);
+                                })
+                                    .attr('height', function (d) {
+                                    return yScale.rangeBand();
+                                })
+                                    .attr('width', function (d) {
+                                    return xScale(d.x);
+                                })
+                                    .attr('tooltip-append-to-body', true)
+                                    .attr('tooltip', function(d) {
+                                        return d.k + " sums $" + d.x;
+                                });
+
+                                ele.removeAttr("d3-horizontal-stacked-bar");
+                                $compile(ele)(scope);
+                            });
 
                         };
                     });
--- views/reports/_attachments/scripts/dashboard/controllers/vulnsbyprice.js
+++ views/reports/_attachments/scripts/dashboard/controllers/vulnsbyprice.js
@@ -4,12 +4,13 @@
 
 angular.module('faradayApp')
     .controller('vulnsbypriceCtrl', 
-        ['$scope', '$route', '$routeParams', 'dashboardSrv',
-        function($scope, $route, $routeParams, dashboardSrv) {
+        ['$scope', '$rootScope', '$route', '$routeParams', 'dashboardSrv',
+        function($scope, $rootScope, $route, $routeParams, dashboardSrv) {
             init = function() {
                 //current workspace
                 $scope.workspace = $routeParams.wsId;
 
+                /*
                 $scope.prices = {
                     "critical": 1000,
                     "high": 500,
@@ -18,11 +19,23 @@ angular.module('faradayApp')
                     "info": 0,
                     "unclassified": 0
                 };
+                */
+
+                $scope.prices = [
+                    {"key": "critical", "value": 1000},
+                    {"key": "high", "value": 500},
+                    {"key": "med", "value": 200},
+                    {"key": "low", "value": 100},
+                    {"key": "info", "value": 0},
+                    {"key": "unclassified", "value": 0}
+                ];
 
                 dashboardSrv.getVulnerabilities($scope.workspace).then(function(res) {
                     $scope.vulns = res;
                     $scope.generateData(res, $scope.prices);
+                    $rootScope.$broadcast("vulnsByPriceDataReady");
                 }); 
+
             };
 
             $scope.generateData = function(vulns, prices) {
@@ -69,17 +82,22 @@ angular.module('faradayApp')
                     }
                 ];
 
+                pricesx = {};
+                prices.forEach(function(price) {
+                    pricesx[price.key] = price.value;
+                });
+
                 vulns.forEach(function(vuln) {
                     var sev = vuln.value.severity;
 
                     if(sev == 2 || sev == "low") {
-                        dashboardSrv.accumulate(data, "low", prices[sev]);
+                        dashboardSrv.accumulate(data, "low", pricesx[sev]);
                     } else if(sev == 3 || sev == "med") {
-                        dashboardSrv.accumulate(data, "med", prices[sev]);
+                        dashboardSrv.accumulate(data, "med", pricesx[sev]);
                     } else if(sev == 4 || sev == "high") {
-                        dashboardSrv.accumulate(data, "high", prices[sev]);
+                        dashboardSrv.accumulate(data, "high", pricesx[sev]);
                     } else if(sev == 5 || sev == "critical") {
-                        dashboardSrv.accumulate(data, "critical", prices[sev]);
+                        dashboardSrv.accumulate(data, "critical", pricesx[sev]);
                     }
                 });
 

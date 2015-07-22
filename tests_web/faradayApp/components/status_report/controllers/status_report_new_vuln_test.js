77b9f87fabc9c6b43c7151cc8cf24700b42bdaef
Franco Linares <flinares@infobytesec.com>
2015-07-22 16:34:14 -0300

    Add test to modal new controller

--- tests_web/faradayApp/components/status_report/controllers/status_report_new_vuln_test.js
+++ tests_web/faradayApp/components/status_report/controllers/status_report_new_vuln_test.js
@@ -3,7 +3,8 @@
 // See the file 'doc/LICENSE' for the license information
 
 describe('modalNewVulnCtrl', function() {
-    var $controller,
+    var $controller, 
+        vm,
         $scope;
 
     var $hostsManager,
@@ -19,7 +20,7 @@ describe('modalNewVulnCtrl', function() {
     beforeEach(function () {
         module('faradayApp');
 
-        inject(function(_$rootScope_, _$controller_, _$q_, _$modalInstance_) {
+        inject(function(_$rootScope_, _$controller_, _$q_) {
             // The injector unwraps the underscores (_) from around the parameter names when matching
             $scope = _$rootScope_.$new();
             returnPromise = function(res) {
@@ -36,32 +37,39 @@ describe('modalNewVulnCtrl', function() {
 
             hostsManagerMock = {
                 getHosts: function(ws) {
-                    return [
+                    return returnPromise([
                         { _id: "1", name: "host1" },
                         { _id: "2", name: "host2" },
                         { _id: "3", name: "host3" }
-                    ]
+                    ])
                 },
                 getInterfaces: function(ws, h_id) {
-                    return [{
-                        _id: h_id + ".1",
-                        name: "int1",
-                        hostnames: ["test" + h_id + ".faradaysec.com"]
-                    }]
+                    return returnPromise([
+                        {"value":
+                            {
+                                _id: h_id + ".1",
+                                name: "int1",
+                                hostnames: ["test" + h_id + ".faradaysec.com"]
+                            }
+                        }
+                    ])
                 }
             }
 
             servicesManagerMock = {
                 getServicesByHost: function(ws, h_id) {
-                    return [
+                    return returnPromise([
                         { _id: h_id + ".1.1", name: "serv" + h_id },
                         { _id: h_id + ".1.2", name: "serv" + h_id },
                         { _id: h_id + ".1.3", name: "serv" + h_id },
-                    ]
+                    ])
                 }
             }
 
             cweFactMock = {
+                get: function() {
+                    return returnPromise([]);
+                }
             }
 
             modalInstance = {
@@ -76,19 +84,63 @@ describe('modalNewVulnCtrl', function() {
                 severities: ['low', 'medium', 'high'],
                 workspace: 'test',
                 hostsManager: hostsManagerMock,
-                cweFact: cweFactMock,
+                servicesManager: servicesManagerMock,
+                cweFact: cweFactMock
             });
+
         });
     });
 
 
     describe('Modal controller init function', function() {
+        beforeEach(function() {
+            vm = $controller;
+        });
         it('controller variables loaded', function() {
             $scope.$apply();
-            expect($scope.vulns.length).toEqual(3);
-            expect($scope.vulns).toContain(vuln1);
-            expect($scope.vulns).toContain(vuln2);
-            expect($scope.vulns).toContain(vuln3);
+            vm.targets.forEach(function(target, j) {
+                expect(target.name).toEqual("host" + (j + 1));
+                expect(target.hostnames).toEqual(["test" + (j + 1) + ".faradaysec.com"]);
+                target.services.forEach(function(services, k) {
+                    expect(services._id).toEqual((j + 1) + ".1." + (k + 1));
+                    expect(services.name).toEqual("serv" + (j + 1));
+                });
+            });
+        });
+    });
+
+    describe('Modal controller functions', function() {
+        beforeEach(function() {
+            vm = $controller;
+        });
+        it('object changed after execution of toggleImpact function', function() {
+            $scope.$apply();
+            expect(vm.data.impact['accountability']).toEqual(false);
+            vm.toggleImpact('accountability');
+            expect(vm.data.impact['accountability']).toEqual(true);
+        });
+        it('add variable to data.refs after execution of newReference function', function() {
+            $scope.$apply();
+            vm.new_ref = "This is a new Reference";
+            vm.newReference();
+            expect(vm.data.refs).toContain('This is a new Reference');
+
+            vm.new_ref = "This is another reference";
+            vm.newReference();
+            expect(vm.data.refs).toContain('This is another reference');
+        });
+        it('selected is true of data.parent object execution of setTarget function', function() {
+            $scope.$apply();
+            var target = vm.targets[0];
+            vm.setTarget(target);
+            expect(vm.data.parent).toEqual(target);
+            expect(vm.data.parent.selected).toEqual(true);
+
+            //if a service is the target
+            var service_target = target.services[0];
+            vm.setTarget(target.services[0]);
+            expect(vm.data.parent).toEqual(service_target);
+            expect(vm.data.parent.selected).toEqual(true);
         });
     });
 });

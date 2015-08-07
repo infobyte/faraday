9c506f041aa9a4f10a1d3355e8e7a21b399b0ea8
micabot <mranea@infobytesec.com>
2015-08-07 04:25:23 +0000

    Fixed all vulnsManager tests
    Injected dependencies and hostsManagerMock as needed.
    
    Test #1311

--- tests_web/faradayApp/components/vulns/providers/vulns.js
+++ tests_web/faradayApp/components/vulns/providers/vulns.js
@@ -7,15 +7,21 @@ describe('vulnsManager', function() {
     var vulnsManager,
     Vuln,
     WebVuln,
-    $filter,
+    hostsManager;
+    
+    var $filter,
     $httpBackend,
-    $provide,
     $q,
     BASEURL;
 
     // Declare data
     var vuln1,
-    hosts, interfaces, 
+    couchVuln1,
+    vuln2,
+    couchVuln2,
+    couchVulnEmpty;
+
+    var hosts, interfaces, 
     hostnames = [];
 
     // Set up the module
@@ -61,94 +67,136 @@ describe('vulnsManager', function() {
             "ws": "ws"
         };
 
-        hosts = {
+        couchVuln1 = {
             "total_rows": 1,
             "offset": 0,
-            "rows": [
+            "rows":[
                 {
-                    "_id": "1", 
-                    "value": {
-                        "name": "Host parent"
-                    }
+                    "id": "1.2.3.8b4ffaedb84dd60d5f43c58eba66a7651458c8de",
+                    "key":"4b84b15bff6ee5796152495a230e45e3d7e947d9.34ac3ea37a2854ce00f2d97c648bf3a7cc27ebca",
+                    "value": vuln1
                 }
             ]
         };
 
-        interfaces = {
-            "total_rows": 3,
-            "offset": 0,
-            "rows": [
+        vuln2 = {
+            "_id": "1.e29ba38bfa81e7f9050f6517babc14cf32cacdff",
+            "_rev": "1-abe16726389e434ca3f37384ea76128e",
+            "desc": "Hello World!",
+            "data": "",
+            "metadata": {
+               "update_time": 1429643049.395857,
+               "update_user": "",
+               "update_action": 0,
+               "creator": "UI Web",
+               "create_time": 1429643049.395857,
+               "update_controller_action": "ModelControler.newVuln",
+               "owner": ""
+            },
+            "name": "Something something dark side",
+            "owned": false,
+            "owner": "john",
+            "parent": "1",
+            "resolution": "Be careful",
+            "refs": [
+               "CVE-2002-1623",
+               "7423",
+               "OSVDB:3820, CERT:886601"
+            ],
+            "severity": "med",
+            "type": "Vulnerability",
+            "ws": "ws"
+        };
+
+        couchVuln2 = {
+            "total_rows":1,
+            "offset":0,
+            "rows":[
                 {
-                    "_id": "1.2",
-                    "value": {
-                        "parent": "1",
-                        "hostnames": [
-                            "h21",
-                            "h22",
-                            "h23"
-                        ]
-                    }
-                }, {
-                    "_id": "1.3",
-                    "value": {
-                        "parent": "1",
-                        "hostnames": [
-                            "h31",
-                            "h32",
-                            "h33"
-                        ]
-                    }
-                }, {
-                    "_id": "1.4",
-                    "value": {
-                        "parent": "1",
-                        "hostnames": [
-                            "h41",
-                            "h42",
-                            "h43"
-                        ]
-                    }
+                    "id": "1.2.3.8b4ffaedb84dd60d5f43c58eba66a7651458c8de",
+                    "key":"4b84b15bff6ee5796152495a230e45e3d7e947d9.34ac3ea37a2854ce00f2d97c648bf3a7cc27ebca",
+                    "value": vuln2
                 }
             ]
         };
 
-        interfaces.rows.forEach(function(interf) {
-            hostnames = hostnames.concat(interf.value.hostnames);
+        couchVulnEmpty = {
+            "total_rows":0,
+            "offset":0,
+            "rows":[]
+        };
+
+        hosts = [
+            {
+                "_id": "1", 
+                "name": "Host parent"
+            }
+        ];
+
+        interfaces = [
+            {
+                "parent": "1",
+                "hostnames": [
+                    "h21",
+                    "h22",
+                    "h23"
+                ]
+            }, {
+                "parent": "1",
+                "hostnames": [
+                    "h31",
+                    "h32",
+                    "h33"
+                ]
+            }, {
+                "parent": "1",
+                "hostnames": [
+                    "h41",
+                    "h42",
+                    "h43"
+                ]
+            }
+        ];
+
+        interfaces.forEach(function(interf) {
+            interf.hostnames.forEach(function(hostname) {
+                if(hostnames.indexOf(hostname) < 0) hostnames.push(hostname);
+            });
         });
     });
 
-    beforeEach(angular.mock.module(function(_$provide_) {
-        $provide = _$provide_;
-    }));
-
     // Initialize dependencies
-    beforeEach(inject(function($injector, _vulnsManager_, _Vuln_, _WebVuln_) {
-    //beforeEach(function() {
+    //beforeEach(inject(function($injector, _vulnsManager_, _Vuln_, _WebVuln_, _$filter_, _$httpBackend_, _$q_) {
+    beforeEach(function() {
         var hostsManagerMock = {
             getHosts: function(ws) {
-                var deferred = _$q_.defer();
+                var deferred = $q.defer();
                 deferred.resolve(hosts);
                 return deferred.promise;
             },
             getAllInterfaces: function() {
-                var deferred = _$q_.defer();
+                var deferred = $q.defer();
                 deferred.resolve(interfaces);
                 return deferred.promise;
             }
         };
 
-        $provide.value('hostsManager', hostsManagerMock);
+        module(function($provide) {
+            $provide.factory('hostsManager', function($q) { return hostsManagerMock; });
+        });
 
-        inject(function(_vulnsManager_, _Vuln_, _WebVuln_, _$filter_, _$httpBackend_, _$q_) {
+        inject(function(_vulnsManager_, _Vuln_, _WebVuln_, _$filter_, _$httpBackend_, _$q_, _hostsManager_) {
             $filter = _$filter_;
             $httpBackend = _$httpBackend_;
             $q = _$q_;
             vulnsManager = _vulnsManager_;
             Vuln = _Vuln_;
             WebVuln = _WebVuln_;
+            hostsManager = _hostsManager_;
             BASEURL = 'http://localhost:9876/'; 
         });
-    }));
+
+    });
 
     afterEach(function() {
         $httpBackend.verifyNoOutstandingExpectation();
@@ -157,24 +205,9 @@ describe('vulnsManager', function() {
 
     describe('Basic usage', function() {
         it('getVulns', function() {
-            var vuln = {
-                "total_rows": 1,
-                "offset": 0,
-                "rows":[
-                    {
-                        "id": "1.2.3.8b4ffaedb84dd60d5f43c58eba66a7651458c8de",
-                        "key":"4b84b15bff6ee5796152495a230e45e3d7e947d9.34ac3ea37a2854ce00f2d97c648bf3a7cc27ebca",
-                        "value": vuln1
-                    }
-                ]
-            };
-
-
             var vulns;
 
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/vulns/_view/all').respond(200, vuln);
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/hosts/_view/hosts').respond(200, hosts);
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/interfaces/_view/interfaces').respond(200, interfaces);
+            $httpBackend.expect('GET', BASEURL + 'ws/_design/vulns/_view/all').respond(200, couchVuln1);
 
             vulnsManager.getVulns("ws")
                 .then(function(vs) {
@@ -191,7 +224,7 @@ describe('vulnsManager', function() {
                 for(var prop in vuln1) {
                     expect(v[prop]).toEqual(vuln1[prop]);
                 }
-                expect(v["target"]).toEqual(hosts.rows[0].value.name);
+                expect(v["target"]).toEqual(hosts[0].name);
                 expect(v["hostnames"]).toEqual(hostnames);
             });
 
@@ -200,7 +233,7 @@ describe('vulnsManager', function() {
                 for(var prop in vuln1) {
                     expect(v[prop]).toEqual(vuln1[prop]);
                 }
-                expect(v["target"]).toEqual(hosts.rows[0].value.name);
+                expect(v["target"]).toEqual(hosts[0].name);
                 expect(v["hostnames"]).toEqual(hostnames);
             });
         });
@@ -208,35 +241,31 @@ describe('vulnsManager', function() {
         it('createVuln', function() {
             var id = vuln1._id,
             vuln = vuln1;
+
             delete vuln._id;
             delete vuln._rev;
-
-            var resp = {
-                "total_rows":1,
-                "offset":0,
-                "rows":[
-                    {
-                        "id": "1.2.3.8b4ffaedb84dd60d5f43c58eba66a7651458c8de",
-                        "key":"4b84b15bff6ee5796152495a230e45e3d7e947d9.34ac3ea37a2854ce00f2d97c648bf3a7cc27ebca",
-                        "value": vuln1
-                    }
-                ]
-            };
 
             var vulns = [];
 
             // insert new vuln in Couch
             $httpBackend.expect('PUT', BASEURL + "ws/" + id).respond(201, {"rev": "1234"});
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/hosts/_view/hosts').respond(200, hosts);
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/interfaces/_view/interfaces').respond(200, interfaces);
 
-            var lala = vulnsManager.createVuln("ws", vuln);
+            vulnsManager.createVuln("ws", vuln)
+                .then(function(vs) {
+                    vulns = vs;
+                });
 
             $httpBackend.flush();
 
-            expect(vulns.length).toEqual(1);
-            expect(vulns[0]._id).toEqual(id);
+            expect(vulnsManager.vulns.length).toEqual(1);
             expect(vulnsManager.vulns[0]._id).toEqual(id);
+            vulnsManager.vulns.forEach(function(v) {
+                for(var prop in vuln1) {
+                    if(prop !== "metadata") expect(v[prop]).toEqual(vuln1[prop]);
+                }
+                expect(v["target"]).toEqual(hosts[0].name);
+                expect(v["hostnames"]).toEqual(hostnames);
+            });
         });
 
         it('deleteVuln', function() {
@@ -245,28 +274,8 @@ describe('vulnsManager', function() {
             delete vuln._id;
             delete vuln._rev;
 
-            var respInsert = {
-                "total_rows":1,
-                "offset":0,
-                "rows":[
-                    {
-                        "id": "1.2.3.8b4ffaedb84dd60d5f43c58eba66a7651458c8de",
-                        "key":"4b84b15bff6ee5796152495a230e45e3d7e947d9.34ac3ea37a2854ce00f2d97c648bf3a7cc27ebca",
-                        "value": vuln1
-                    }
-                ]
-            };
-
-            var respDelete = {
-                "total_rows":0,
-                "offset":0,
-                "rows":[]
-            };
-
             // insert new vuln in Couch
             $httpBackend.expect('PUT', BASEURL + "ws/" + id).respond(201, {"rev": vuln1._rev});
-            // getVulns
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/vulns/_view/all').respond(200, respInsert);
 
             vulnsManager.createVuln("ws", vuln);
 
@@ -274,10 +283,8 @@ describe('vulnsManager', function() {
 
             // delete vuln
             $httpBackend.expect('DELETE', BASEURL + 'ws/' + id + "?rev=" + vuln1._rev).respond(200);
-            // getVulns
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/vulns/_view/all').respond(200, respDelete);
             
-            vulnsManager.deleteVuln("ws", vulnsManager.vulns[0]);
+            vulnsManager.deleteVuln(vulnsManager.vulns[0]);
             $httpBackend.flush();
 
             expect(vulnsManager.vulns.length).toEqual(0);
@@ -289,80 +296,23 @@ describe('vulnsManager', function() {
             delete vuln._id;
             delete vuln._rev;
 
-            var vulnMod = {
-                "_id": "1.2.3.e29ba38bfa81e7f9050f6517babc14cf32cacdff",
-                "_rev": "1-abe16726389e434ca3f37384ea76128e",
-                "desc": "Hello World!",
-                "data": "",
-                "metadata": {
-                   "update_time": 1429643049.395857,
-                   "update_user": "",
-                   "update_action": 0,
-                   "creator": "UI Web",
-                   "create_time": 1429643049.395857,
-                   "update_controller_action": "ModelControler.newVuln",
-                   "owner": ""
-                },
-                "name": "Something something dark side",
-                "owned": false,
-                "owner": "",
-                "parent": "1.2.3",
-                "resolution": "Be careful",
-                "refs": [
-                   "CVE-2002-1623",
-                   "7423",
-                   "OSVDB:3820, CERT:886601"
-                ],
-                "severity": "med",
-                "type": "Vulnerability",
-                "ws": "ws"
-            };
-
-            var respInsert = {
-                "total_rows":1,
-                "offset":0,
-                "rows":[
-                    {
-                        "id": "1.2.3.8b4ffaedb84dd60d5f43c58eba66a7651458c8de",
-                        "key":"4b84b15bff6ee5796152495a230e45e3d7e947d9.34ac3ea37a2854ce00f2d97c648bf3a7cc27ebca",
-                        "value": vuln1
-                    }
-                ]
-            };
-
-            var respUpdate = {
-                "total_rows":1,
-                "offset":0,
-                "rows":[
-                    {
-                        "id": "1.2.3.8b4ffaedb84dd60d5f43c58eba66a7651458c8de",
-                        "key":"4b84b15bff6ee5796152495a230e45e3d7e947d9.34ac3ea37a2854ce00f2d97c648bf3a7cc27ebca",
-                        "value": vulnMod
-                    }
-                ]
-            };
-
             // insert new vuln in Couch
             $httpBackend.expect('PUT', BASEURL + "ws/" + id).respond(201, {"rev": "1234"});
-            // getVulns
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/vulns/_view/all').respond(200, respInsert);
             // call to insert
             vulnsManager.createVuln("ws", vuln);
             $httpBackend.flush();
 
             // update vuln
-            $httpBackend.expect('PUT', BASEURL + 'ws/' + id).respond(200, {"rev": "2345"});
-            // getVulns
-            $httpBackend.expect('GET', BASEURL + 'ws/_design/vulns/_view/all').respond(200, respUpdate);
+            $httpBackend.expect('PUT', BASEURL + 'ws/' + id).respond(200, {"rev": "1-abe16726389e434ca3f37384ea76128e"});
             
-            var vulns = vulnsManager.updateVuln(vuln.ws, vulnsManager.vulns[0], vulnMod);
+            var vulns = vulnsManager.updateVuln(vulnsManager.vulns[0], vuln2);
             $httpBackend.flush();
 
             expect(vulnsManager.vulns.length).toEqual(1);
 
-            for(var prop in vulnMod) {
-                if(vulnMod.hasOwnProperty(prop)) {
-                    if(prop != "metadata") expect(vulnsManager.vulns[0][prop]).toEqual(vulnMod[prop]);
+            for(var prop in vuln2) {
+                if(vuln2.hasOwnProperty(prop)) {
+                    if(prop != "metadata") expect(vulnsManager.vulns[0][prop]).toEqual(vuln2[prop]);
                 }
             }
         });

0c00e4736447397bd564d395b4b446172bfb5e3f
micabot <mranea@infobytesec.com>
2015-09-22 15:22:45 -0300

    Add link to third party References
    Added function to calculate URL on click, to avoid pre-calculating all
    links, which should get pretty heavy in a big Workspace.
    It matches all references that are either:
        * URLs
        * CVE ID
        * CWE ID
        * Exploit Database ID
        * Open Source Vulnerability Database
    All other references are sent to Google as a search.
    
    Test #1123

--- views/reports/_attachments/scripts/statusReport/controllers/statusReport.js
+++ views/reports/_attachments/scripts/statusReport/controllers/statusReport.js
@@ -5,11 +5,11 @@
 angular.module('faradayApp')
     .controller('statusReportCtrl', 
                     ['$scope', '$filter', '$routeParams',
-                    '$location', '$modal', '$cookies', '$q', 'BASEURL',
+                    '$location', '$modal', '$cookies', '$q', '$window', 'BASEURL',
                     'SEVERITIES', 'EASEOFRESOLUTION', 'hostsManager',
                     'vulnsManager', 'workspacesFact', 'csvService',
                     function($scope, $filter, $routeParams,
-                        $location, $modal, $cookies, $q, BASEURL,
+                        $location, $modal, $cookies, $q, $window, BASEURL,
                         SEVERITIES, EASEOFRESOLUTION, hostsManager,
                         vulnsManager, workspacesFact, csvService) {
         $scope.baseurl;
@@ -107,6 +107,37 @@ angular.module('faradayApp')
             };
             
             $scope.vulnWebSelected = false;
+        };
+
+        $scope.processReference = function(text) {
+            var url = 'http://google.com/',
+            url_pattern = new RegExp('^(http|https):\\/\\/?');
+
+            var cve_pattern = new RegExp(/^CVE-\d{4}-\d{4,7}$/),
+            cwe_pattern = new RegExp(/^CWE-\d{4}-\d{4,7}$/),
+            edb_pattern = new RegExp(/^EDB-ID:\d{1,}$/),
+            osvdb_pattern = new RegExp(/^OSVDB:\d{1,}$/);
+
+            var cve = text.search(cve_pattern),
+            cwe = text.search(cwe_pattern),
+            edb = text.search(edb_pattern),
+            osvdb = text.search(osvdb_pattern);
+
+            if(url_pattern.test(text)) {
+                url = text;
+            } else if(cve > -1) {
+                url = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=" + text.substring(cve + 4);
+            } else if(cwe > -1) {
+                url = "https://cwe.mitre.org/data/definitions/" + text.substring(cwe + 3) + ".html";
+            } else if(osvdb > -1) {
+                url = "http://osvdb.org/show/osvdb/" + text.substring(osvdb + 6);
+            } else if(edb > -1) {
+                url = "https://www.exploit-db.com/exploits/" + text.substring(edb + 7);
+            } else {
+                url += 'search?q=' + text;
+            }
+            
+            $window.open(url, '_blank');
         };
 
         $scope.selectedVulns = function() {

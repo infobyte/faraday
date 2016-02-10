f42fe2eccf2fc86a15b36baead9c1d059ede6b93
Franco Linares <flinares@infobytesec.com>
2016-02-10 12:11:08 -0300

    Fix bug
    If there is no workspace

--- views/reports/_attachments/scripts/navigation/controllers/navigationCtrl.js
+++ views/reports/_attachments/scripts/navigation/controllers/navigationCtrl.js
@@ -31,8 +31,9 @@ angular.module('faradayApp')
         });
 
         $scope.$on('$routeChangeSuccess', function() {
-            if(componentsNeedsWS.indexOf($location.path().split("/")[1]) != -1) {
+            if(componentsNeedsWS.indexOf($location.path().split("/")[1]) != -1 && $routeParams.wsId !== undefined) {
                 workspacesFact.exists($routeParams.wsId).then(function(resp){
+                    console.log(resp);
                     if(resp === true) {
                         $scope.workspaceExists = true;
                     } else {

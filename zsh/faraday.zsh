1a66ab3a9503f18c6556a725b1d6b6ac64a12263
EzequielTBH <ezequieltbh@infobytesec.com>
2016-04-14 11:57:14 -0300

    Change zsh interaction with pluginControllerAPI

--- zsh/faraday.zsh
+++ zsh/faraday.zsh
@@ -8,6 +8,8 @@
 WORKSPACE=`cat $HOME/.faraday/config/user.xml |  grep '<last_workspace' | cut -d '>' -f 2 | cut -d '<' -f 1`
 STATUS=`curl -s $FARADAY_ZSH_HOST:$FARADAY_ZSH_RPORT/status/check |  sed "s/[^0-9]//g" | grep -v '^[[:space:]]*$'`
 PS1="%{${fg_bold[red]}%}[faraday]($WORKSPACE)%{${reset_color}%} $PS1"
+FARADAY_OUTPUT=
+FARADAY_PLUGIN=
 
 echo ">>> WELCOME TO FARADAY"
 echo "[+] Current Workspace: $WORKSPACE"
@@ -28,10 +30,30 @@ plugin_controller_client=$HOME/.faraday/zsh/plugin_controller_client.py
 old_cmd=
 
 add-output() {
-    old_cmd=$BUFFER
-    new_cmd=`python2 $plugin_controller_client send_cmd $BUFFER`
-    BUFFER=" $new_cmd"
-    zle .accept-line "$@"
+
+	old_cmd=$BUFFER
+    json_response=`$plugin_controller_client send_cmd $$ $BUFFER`
+	FARADAY_PLUGIN=
+	FARADAY_OUTPUT=
+
+	if [ "$json_response" != "" ]; then
+		new_cmd=`python -c 'import json;print(json.loads($json_response))["cmd"]'`
+		if [ "$new_cmd" != "null" ]; then
+			BUFFER=" $new_cmd"
+			FARADAY_OUTPUT = `$plugin_controller_client gen_output $$`
+			BUFFER = $BUFFER + " >&1 > $OUTPUT"
+		FARADAY_PLUGIN=`python -c 'import json;print(json.loads($json_response))["plugin"]'`
+
+	zle .accept-line "$@"
+}
+
+send-output(){
+	if [ ! -z "$FARADAY_PLUGIN" ]; then
+		`$plugin_controller_client send_output $$ $? $FARADAY_OUTPUT`
+	fi
+
+	FARADAY_OUTPUT=
+	FARADAY_PLUGIN=
 }
 
 function zshaddhistory() {
@@ -39,6 +61,15 @@ function zshaddhistory() {
     print -sr -- "$old_cmd"
     fc -p
     return 1
+}
+
+function precmd(){
+	WORKSPACE=`cat $HOME/.faraday/config/user.xml |  grep '<last_workspace' | cut -d '>' -f 2 | cut -d '<' -f 1`
+	send-output()
+}
+
+function zshexit(){
+	send-output()
 }
 
 zle -N accept-line add-output

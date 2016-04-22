128162b7f391df66b869d774b6f348167d9ef7b0
Joaquin L. Pereyra <joaquinlp@infobytesec.com>
2016-04-22 10:29:57 -0300

    added copyright info

--- gui/gtk/application.py
+++ gui/gtk/application.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
+'''
+Faraday Penetration Test IDE
+Copyright (C) 2016  Infobyte LLC (http://www.infobytesec.com/)
+See the file 'doc/LICENSE' for the license information
+
+'''
+
 import os
 import gi
 import model.guiapi
--- gui/gtk/mainwidgets.py
+++ gui/gtk/mainwidgets.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+'''
+Faraday Penetration Test IDE
+Copyright (C) 2016  Infobyte LLC (http://www.infobytesec.com/)
+See the file 'doc/LICENSE' for the license information
+
+'''
 import gi
 import os
 import sys

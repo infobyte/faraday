d46771f7dea18a6d8ce028019327d1d5c08635f6
micabot <mranea@infobytesec.com>
2016-05-27 16:15:22 -0300

    Add topServices, topHosts and vulnsPierchart files
    Adding controllers and partials.

---
+++ views/reports/_attachments/scripts/dashboard/controllers/vulnsPiechart.js
@@ -0,0 +1,40 @@
+// Faraday Penetration Test IDE
+// Copyright (C) 2013  Infobyte LLC (http://www.infobytesec.com/)
+// See the file 'doc/LICENSE' for the license information
+
+angular.module('faradayApp')
+    .controller('vulnsPiechartCtrl',
+        ['$scope', '$routeParams', 'dashboardSrv',
+        function($scope, $routeParams, dashboardSrv) {
+
+            $scope.topServices;
+            $scope.workspace;
+
+            init = function() {
+                if($routeParams.wsId != undefined) {
+                    $scope.workspace = $routeParams.wsId;
+
+                    dashboardSrv.getTopServices($scope.workspace)
+                        .then(function(services) {
+                            $scope.topServices = {"children": services};
+                        });
+                }
+            };
+
+            $scope.treemap = function(data) {
+                if(data !== undefined && data != {}) {
+                    var modal = $uibModal.open({
+                        templateUrl: 'scripts/dashboard/partials/modal-treemap.html',
+                        controller: 'treemapModalCtrl',
+                        size: 'lg',
+                        resolve: {
+                            workspace: function() {
+                                return $scope.workspace;
+                            }
+                        }
+                    });
+                }
+            };
+
+            init();
+    }]);

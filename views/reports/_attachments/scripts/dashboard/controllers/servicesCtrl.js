162179a4f44e8da2a028479b619c3efc8c0acd6e
micabot <mranea@infobytesec.com>
2016-05-31 13:16:28 -0300

    Add functionality to servicesCtrl
    Also added the controller to the view.

--- views/reports/_attachments/scripts/dashboard/controllers/servicesCtrl.js
+++ views/reports/_attachments/scripts/dashboard/controllers/servicesCtrl.js
@@ -4,13 +4,37 @@
 
 angular.module('faradayApp')
     .controller('servicesCtrl',
-        ['$scope', '$routeParams', 'dashboardSrv',
-        function($scope, $routeParams, dashboardSrv) {
+        ['$scope', '$uibModal', '$routeParams', 'dashboardSrv',
+        function($scope, $uibModal, $routeParams, dashboardSrv) {
+            $scope.servicesCount;
             $scope.workspace;
 
             init = function() {
                 if($routeParams.wsId != undefined) {
                     $scope.workspace = $routeParams.wsId;
+
+                    dashboardSrv.getServicesCount($scope.workspace)
+                        .then(function(services) {
+                            $scope.servicesCount = services;
+                        });
+                }
+            };
+
+            $scope.showHosts = function(srv_name) {
+                if($scope.workspace != undefined) {
+                    var modal = $uibModal.open({
+                        templateUrl: 'scripts/dashboard/partials/modal-hosts-by-service.html',
+                        controller: 'summarizedCtrlHostsModal',
+                        size: 'lg',
+                        resolve: {
+                            srv_name: function() {
+                                return srv_name;
+                            },
+                            workspace: function() {
+                                return $scope.workspace;
+                            }
+                        }
+                     });
                 }
             };
 

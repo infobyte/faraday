de9f83964c5d3ff7ad32df59b30378c614d8ae60
micabot <mranea@infobytesec.com>
2016-05-31 12:16:52 -0300

    Add vulnerabilities count data to controller

--- views/reports/_attachments/scripts/dashboard/controllers/vulnsPiechartCtrl.js
+++ views/reports/_attachments/scripts/dashboard/controllers/vulnsPiechartCtrl.js
@@ -4,18 +4,30 @@
 
 angular.module('faradayApp')
     .controller('vulnsPiechartCtrl',
-        ['$scope', '$routeParams', 'dashboardSrv',
-        function($scope, $routeParams, dashboardSrv) {
+        ['$scope', '$routeParams', 'dashboardSrv', 'SEVERITIES',
+        function($scope, $routeParams, dashboardSrv, SEVERITIES) {
 
-            $scope.doughnut;
-            $scope.topServices;
+            $scope.data;
+            $scope.loaded = false;
             $scope.workspace;
 
             init = function() {
                 if($routeParams.wsId != undefined) {
                     $scope.workspace = $routeParams.wsId;
 
-                    $scope.doughnut = {key: [], value: [], colors: [], options: {maintainAspectRatio: false}};
+                    $scope.data = {key: [], value: [], colors: [], options: {maintainAspectRatio: false}};
+
+                    dashboardSrv.getVulnerabilitiesCount($scope.workspace)
+                        .then(function(vulns) {
+                            $scope.loaded = true;
+                            SEVERITIES.forEach(function(severity, index) {
+                                if(severity != "unclassified" && vulns[severity] != undefined) {
+                                    $scope.data.value.push(vulns[severity]);
+                                    $scope.data.key.push(severity);
+                                    $scope.data.colors.push(dashboardSrv.vulnColors[index]);
+                                }
+                            });
+                        });
                 }
             };
 

ba816499c7081a2845b396d445cc0bb2d8066195
micabot <mranea@infobytesec.com>
2016-06-14 17:29:29 -0300

    Fix controller

--- views/reports/_attachments/scripts/dashboard/controllers/vulnsByPriceCtrl.js
+++ views/reports/_attachments/scripts/dashboard/controllers/vulnsByPriceCtrl.js
@@ -7,13 +7,48 @@ angular.module('faradayApp')
         ['$scope', '$routeParams', 'dashboardSrv', 'SEVERITIES',
         function($scope, $routeParams, dashboardSrv, SEVERITIES) {
 
-            $scope.vulns = {};
+            $scope.vulnPrices;
+            $scope.vulns;
             $scope.workspace;
+            $scope.workspaceWorth;
 
             init = function() {
                 if($routeParams.wsId != undefined) {
                     $scope.workspace = $routeParams.wsId;
+                    $scope.vulnPrices = dashboardSrv.vulnPrices;
+
+                    dashboardSrv.getVulnsWorth($scope.workspace)
+                        .then(function(vulns) {
+                            $scope.vulns = vulns;
+                            $scope.generateVulnPrices($scope.vulns, $scope.vulnPrices);
+                            $scope.workspaceWorth = $scope.sumProperty($scope.vulns, "amount");
+                        });
+
+                        $scope.$watch('vulnPrices', function(ps) {
+                            if($scope.vulns != undefined) {
+                                $scope.generateVulnPrices($scope.vulns, $scope.vulnPrices);
+                                $scope.workspaceWorth = $scope.sumProperty($scope.vulns, "amount");
+                            }
+                        }, true);
                 }
+            };
+
+            $scope.generateVulnPrices = function(vulns, prices) {
+                vulns.forEach(function(vuln) {
+                    vuln.amount = vuln.value * prices[vuln.key];
+                });
+            };
+
+            $scope.sumProperty = function(data, prop) {
+                var total = 0;
+
+                for(var d in data) {
+                    if(data.hasOwnProperty(d)) {
+                        if(data[d][prop] !== undefined) total += parseInt(data[d][prop]);
+                    }
+                }
+
+                return total;
             };
 
             init();

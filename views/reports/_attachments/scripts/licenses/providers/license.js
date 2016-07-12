b191bfec1dacabc2f4891f4f1ccd9670ca0d76c5
micabot <mranea@infobytesec.com>
2016-07-12 11:30:22 -0500

    Add License object

---
+++ views/reports/_attachments/scripts/licenses/providers/license.js
@@ -0,0 +1,121 @@
+// Faraday Penetration Test IDE
+// Copyright (C) 2013  Infobyte LLC (http://www.infobytesec.com/)
+// See the file 'doc/LICENSE' for the license information
+
+angular.module('faradayApp')
+    .factory('License', ['BASEURL', 'configSrv', '$http', '$q',
+    function(BASEURL, configSrv, $http, $q) {
+        License = function(data) {
+            var now = new Date(),
+            date = now.getTime() / 1000.0;
+
+            this._id = "";
+            this._rev = "";
+            this.end = "";
+            this.lictype = "";
+            this.metadata = {
+                update_time: date,
+                update_user: "",
+                update_action: 0,
+                creator: "UI Web",
+                create_time: date,
+                update_controller_action: "UI Web New",
+                owner: ""
+            };
+            this.notes = "";
+            this.product = "";
+            this.start = "";
+            this.type = "License";
+
+            if(data) {
+                if(data.product === undefined || data.product === "") {
+                    throw new Error("Unable to create License without a product name");
+                }
+                this.set(data);
+            }
+        };
+
+        License.prototype = {
+            public_properties: [
+                'end', 'lictype', 'notes', 'product', 'start'
+            ],
+            set: function(data) {
+                var self = this;
+
+                // new license
+                if(data._id != undefined) {
+                    self._id = data._id;
+                    if(data._rev !== undefined) self._rev = data._rev;
+                    if(data.metadata !== undefined) self.metadata = data.metadata;
+                }
+
+                self.public_properties.forEach(function(property) {
+                    if(data[property] !== undefined) self[property] = data[property];
+                });
+            },
+            remove: function() {
+                var deferred = $q.defer(),
+                self = this;
+
+                configSrv.promise
+                    .then(function() {
+                        var url = BASEURL + configSrv.license_db + "/" + self._id + "?rev=" + self._rev;
+
+                        $http.delete(url)
+                            .then(function(resp) {
+                                deferred.resolve(resp);
+                            }, function() {
+                                deferred.reject("Unable to delete License from database.");
+                            });
+                    }, function() {
+                        deferred.reject("Unable to load configuration to delete License.");
+                    });
+
+                return deferred.promise;
+            },
+            update: function(data) {
+                var deferred = $q.defer(),
+                self = this;
+
+                configSrv.promise
+                    .then(function() {
+                        var url = BASEURL + configSrv.license_db;
+
+                        $http.put(url, self)
+                            .then(function(data) {
+                                self._rev = data.rev;
+                                deferred.resolve(self);
+                            }, function(data, status, headers, config) {
+                                deferred.reject(status);
+                            });
+                    }, function() {
+                        deferred.reject("Unable to load configuration to update License.");
+                    });
+
+                return deferred.promise;
+            },
+            save: function() {
+                var deferred = $q.defer(),
+                self = this;
+
+                configSrv.promise
+                    .then(function() {
+                        var url = BASEURL + configSrv.license_db;
+
+                        $http.post(url, self)
+                            .then(function(data) {
+                                self._rev = data.rev;
+                                deferred.resolve(self);
+                            }, function(data, status, headers, config) {
+                                deferred.reject(status);
+                            });
+                    }, function() {
+                        deferred.reject("Unable to load configuration to save License.");
+                    });
+
+                return deferred.promise;
+            }
+        };
+
+        return License;
+    }]);

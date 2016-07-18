827ee0dd8113059699cc184513e98beef12d207e
micabot <mranea@infobytesec.com>
2016-07-18 13:12:19 -0500

    Add licenses editing capabilities
    Only one license can be edited at a time.

--- views/reports/_attachments/scripts/licenses/controllers/modalEdit.js
+++ views/reports/_attachments/scripts/licenses/controllers/modalEdit.js
@@ -3,50 +3,31 @@
 // See the file 'doc/LICENSE' for the license information
 
 angular.module('faradayApp')
-    .controller('hostsModalEdit',
-        ['$scope', '$modalInstance', '$routeParams', 'hostsManager', 'host', 'commonsFact',
-        function($scope, $modalInstance, $routeParams, hostsManager, host, commons) {
+    .controller('licensesModalEdit',
+        ['$scope', '$modalInstance', 'licensesManager', 'license',
+        function($scope, $modalInstance, licensesManager, license) {
 
-        var ws = $routeParams.wsId; 
-        $scope.hostdata = {};
-        hostsManager.getInterfaces(ws, host._id).then(function(resp){
-            $scope.interface = resp[0].value;
-            $scope.interface.hostnames = commons.arrayToObject($scope.interface.hostnames);
-        });
+        $scope.data = {};
 
-        $scope.host = {
-            "_id": host._id,
-            "_rev": host._rev,
-            "description": host.description,
-            "default_gateway": "None",
-            "metadata": host.metadata,
-            "name": host.name,
-            "os": host.os,
-            "owner": "",
-            "owned": host.owned,
-            "parent": host.parent,
-            "type": host.type
+        init = function() {
+            $scope.data = new License;
+            angular.copy(license, $scope.data);
         };
 
         $scope.ok = function() {
-            var date = new Date(),
-            timestamp = date.getTime()/1000.0;
-            $scope.interface.hostnames = commons.objectToArray($scope.interface.hostnames.filter(Boolean));
+            $modalInstance.close($scope.data);
+        };
 
-            $scope.hostdata = $scope.host;
-            $scope.hostdata.metadata['update_time'] = timestamp;
-            $scope.hostdata.metadata['update_user'] = "UI Web";
+        $scope.open = function($event, isStart) {
+            $event.preventDefault();
+            $event.stopPropagation();
 
-            $modalInstance.close([$scope.hostdata, $scope.interface]);
+            if(isStart) $scope.openedStart = true; else $scope.openedEnd = true;
         };
 
         $scope.cancel = function() {
             $modalInstance.dismiss('cancel');
         };
 
-        $scope.newHostnames = function($event){
-            $scope.interface.hostnames.push({key:''});
-            $event.preventDefault();
-        }
-
+        init();
     }]);

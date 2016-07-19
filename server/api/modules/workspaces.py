98ae64855b4c3e37c3341e0674b4e0576684e41c
Martin Rocha <martinr@infobytesec.com>
2016-07-19 20:02:03 -0300

    add workspace summary statistics api

---
+++ server/api/modules/workspaces.py
@@ -0,0 +1,40 @@
+# Faraday Penetration Test IDE
+# Copyright (C) 2016  Infobyte LLC (http://www.infobytesec.com/)
+# See the file 'doc/LICENSE' for the license information
+
+import flask
+
+from server.app import app
+from server.dao.host import HostDAO
+from server.dao.vuln import VulnerabilityDAO
+from server.dao.service import ServiceDAO
+from server.dao.interface import InterfaceDAO
+from server.dao.note import NoteDAO
+from server.utils.web import gzipped, validate_workspace
+
+
+@app.route('/ws/<workspace>/summary', methods=['GET'])
+@gzipped
+def workspace_summary(workspace=None):
+    validate_workspace(workspace)
+
+    services_count = ServiceDAO(workspace).count()
+    vuln_count = VulnerabilityDAO(workspace).count()
+    host_count = HostDAO(workspace).count()
+    iface_count = InterfaceDAO(workspace).count()
+    note_count = NoteDAO(workspace).count()
+
+    response = {
+        'stats': {
+            'services':    services_count.get('total_count', 0),
+            'total_vulns': vuln_count.get('total_count', 0),
+            'web_vulns':   vuln_count.get('web_vuln_count', 0),
+            'std_vulns':   vuln_count.get('vuln_count', 0),
+            'hosts':       host_count.get('total_count', 0),
+            'interfaces':  iface_count.get('total_count', 0),
+            'notes':       note_count.get('total_count', 0),
+        }
+    }
+
+    return flask.jsonify(response)
+

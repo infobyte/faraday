beabbd6dd298a32432b2e9c3b030f198ea74cf89
umpacu <rgerman@infobytesec.com>
2016-07-23 20:59:10 -0300

    Fix spining icon in status report

--- server/www/scripts/statusReport/controllers/statusReport.js
+++ server/www/scripts/statusReport/controllers/statusReport.js
@@ -804,6 +804,7 @@ angular.module('faradayApp')
 
         loadVulns = function() {
             delete searchFilter.confirmed;
+            $scope.gridOptions.totalItems = null;
             if ($scope.confirmed)
                 searchFilter.confirmed = true;
             // load all vulnerabilities

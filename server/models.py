f90d9f9f36524142810b00b0426aaee7bfc4c36b
EzequielTBH <ezequieltbh@infobytesec.com>
2016-09-14 17:18:35 -0300

    Fixex version...

--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SCHEMA_VERSION = 'W.0.5'
+SCHEMA_VERSION = 'W.2.1.0'
 
 Base = declarative_base()
 

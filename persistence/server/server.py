e67b337187e3623bc910a11baf19538561450108
umpacu <rgerman@infobytesec.com>
2016-11-01 13:45:37 -0300

    Fix typo

--- persistence/server/server.py
+++ persistence/server/server.py
@@ -21,7 +21,7 @@ from persistence.server.changes_stream import CouchChangesStream
 # NOTE: Change is you want to use this module by itself.
 # If FARADAY_UP is False, SERVER_URL must be a valid faraday server url
 FARADAY_UP = True
-SERVER_URL = "http://127.0.1:5984"
+SERVER_URL = "http://127.0.0.1:5984"
 
 
 def _get_base_server_url():

b35847c1c6695bdd166608ff51c4b52d3c91eeab
EzequielTBH <ezequieltbh@infobytesec.com>
2016-12-06 18:55:29 -0300

    Added count of hosts, vulns, services in Activity Feed

--- server/www/scripts/dashboard/controllers/activityFeedCtrl.js
+++ server/www/scripts/dashboard/controllers/activityFeedCtrl.js
@@ -1,6 +1,7 @@
 // Faraday Penetration Test IDE
 // Copyright (C) 2016  Infobyte LLC (http://www.infobytesec.com/)
 // See the file 'doc/LICENSE' for the license information
+
 'use strict';
 
 angular.module('faradayApp')
@@ -8,8 +9,41 @@ angular.module('faradayApp')
         ['$scope', '$routeParams', 'dashboardSrv',
         function($scope, $routeParams, dashboardSrv) {
             
-            var self = this;
-            self.commands = [];
+            var vm = this;
+            vm.commands = [];
+            
+            // Get a count of all hosts created by this command.
+            vm.getHostCount = function(command){
+                dashboardSrv.getHostsCountByCommandId($scope.workspace, command._id)
+                        .then(function(hosts) {
+
+                           if( !isNaN(hosts['total_rows']) ){
+                               command['hosts_count'] = hosts['total_rows'];
+                           }
+                        });
+            };
+
+            // Get a count of all services created by this command.
+            vm.getServiceCount = function(command){
+                dashboardSrv.getServicesByCommandId($scope.workspace, command._id)
+                        .then(function(services) {
+                            
+                            if( services['services'].length != 0 ){
+                                command['services_count'] = services['services'].length;
+                            }
+                        });
+            };
+
+            // Get a count of all vulns created by this command.
+            vm.getVulnsCount = function(command){
+                dashboardSrv.getVulnsByCommandId($scope.workspace, command._id)
+                        .then(function(vulnerabilities) {
+                            
+                            if( !isNaN(vulnerabilities['count']) ){
+                                command['vulnerabilities_count'] = vulnerabilities['count'];
+                            }
+                        });
+            };
             
             // Get last 5 commands
             var init = function() {
@@ -18,13 +52,17 @@ angular.module('faradayApp')
 
                     dashboardSrv.getCommands($scope.workspace, 0, 5)
                         .then(function(commands) {
-                            self.commands = commands;
-                            console.log(commands);
+
+                            vm.commands = commands;
+                            vm.commands.forEach(function(command){
+                              vm.getHostCount(command);
+                              vm.getServiceCount(command);
+                              vm.getVulnsCount(command);
+                            });
                         });
                 }
             };
 
             dashboardSrv.registerCallback(init);
             init();
-            
     }]);
--- server/www/scripts/dashboard/providers/dashboard.js
+++ server/www/scripts/dashboard/providers/dashboard.js
@@ -105,6 +105,19 @@ angular.module('faradayApp')
             return deferred.promise;
         };
 
+        dashboardSrv.getServicesByCommandId = function(ws, command_id) {
+            var deferred = $q.defer();
+            ServerAPI.getServices(ws, {"command_id": command_id})
+                .then(function(res) {
+                    deferred.resolve(res.data);
+                }, function() {
+                    deferred.reject("Unable to get Services");
+                });
+
+            return deferred.promise;
+        };
+
+
         dashboardSrv.getTopServices = function(ws, colors) {
             var deferred = $q.defer();
 
@@ -125,6 +138,18 @@ angular.module('faradayApp')
                     }
                 }, function() {
                     deferred.reject("Unable to get Top Services");
+                });
+
+            return deferred.promise;
+        };
+
+        dashboardSrv.getVulnsByCommandId = function(ws, command_id) {
+            var deferred = $q.defer();
+            ServerAPI.getVulns(ws, {"command_id": command_id})
+                .then(function(res) {
+                    deferred.resolve(res.data);
+                }, function() {
+                    deferred.reject("Unable to get Vulnerabilities");
                 });
 
             return deferred.promise;
@@ -219,7 +244,7 @@ angular.module('faradayApp')
                         } else if(_cmd.duration != undefined) {
                             _cmd.duration = _cmd.duration.toFixed(2) + "s";
                         }
-                        _cmd.date = _cmd.itime * 1000;
+                        _cmd.date = _cmd.itime * 1000;                        
                         tmp.push(_cmd);
                     });
 
@@ -242,6 +267,19 @@ angular.module('faradayApp')
                         tmp.push(_host);
                     });
                     deferred.resolve(tmp);
+                }, function() {
+                    deferred.reject();
+                });
+            return deferred.promise;
+        };
+
+        dashboardSrv.getHostsCountByCommandId = function(ws, command_id) {
+
+            var deferred = $q.defer();
+          
+            ServerAPI.getHosts(ws, {"command_id": command_id })
+                .then(function(res) {
+                    deferred.resolve(res.data);
                 }, function() {
                     deferred.reject();
                 });

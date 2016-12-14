94d934cb08d10c8ff2efc288f7c429333febf1a9
robertfiz <robertfiz@users.noreply.github.com>
2016-12-14 11:39:05 -0300

    Update plugin.py

--- plugins/repo/hping3/plugin.py
+++ plugins/repo/hping3/plugin.py
@@ -33,7 +33,6 @@ class hping3 (core.PluginBase):
         ip_address=reg.group(1)
         hostname=output.split(" ")[1]
         host_id=self.createAndAddHost(hostname)
-        verita=False
         if self._isIPV4(ip_address):
             
                 i_id=self.createAndAddInterface(host_id, ip_address, ipv4_address=ip_address, hostname_resolution=hostname)
@@ -42,9 +41,8 @@ class hping3 (core.PluginBase):
                 i_id=self.createAndAddInterface(
                     host_id, ip_address, ipv6_addres=ip_address, hostname_resolution=hostname)
 
-                
         '''
-        Las siguientes lineas fueron testeadas con el parametro --scan -S
+        las siguientes lineas procesan la salida de --scan -S
         '''
         lineas=output.split("\n")
         for linea in lineas:
@@ -52,8 +50,9 @@ class hping3 (core.PluginBase):
             if (re.match(" ",linea)):
                 lista_recibida=re.findall("\w+",linea)
                 servicio=lista_recibida[1]
-                puerto=lista_recibida[0]
-                s_id=self.createAndAddServiceToInterface(host_id, i_id,servicio,protocol="tcp",ports=puerto,status="open") 
+                puerto=[lista_recibida[0]]
+                if lista_recibida[2]=="S" and lista_recibida[3]=="A":
+                    s_id=self.createAndAddServiceToInterface(host_id, i_id,servicio,protocol="tcp",ports=puerto,status="open") 
                 
     def _isIPV4(self, ip):
         if len(ip.split(".")) == 4:

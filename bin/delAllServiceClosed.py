11b62a1a89375d603e59f5715cfc55c6ea2a63c9
Sebastian Kulesz <sebikul@infobytesec.com>
2017-03-13 15:49:38 -0300

    fplugin: Fixed an exception when performing a bulk deletion

--- bin/delAllServiceClosed.py
+++ bin/delAllServiceClosed.py
@@ -8,6 +8,7 @@ See the file 'doc/LICENSE' for the license information
 '''
 
 from persistence.server import models
+from utils.user_input import query_yes_no
 
 __description__ = 'Deletes all services with a non open port'
 __prettyname__ = 'Delete All Service Closed'
@@ -17,11 +18,11 @@ def main(workspace='', args=None, parser=None):
     parser.add_argument('-y', '--yes', action="store_true")
     parsed_args = parser.parse_args(args)
     if not parsed_args.yes:
-        msg = ("Are you sure you want to delete all closed services in the "
-               "workspace {}? This action can't be undone [y/n] ".format(
-                   workspace))
-        if raw_input(msg) not in ('y', 'yes'):
+
+        if not query_yes_no("Are you sure you want to delete all closed services in the "
+                            "workspace %s" % workspace, default='no'):
             return 1, None
+
     for service in models.get_services(workspace):
         if service.status != 'open' and service.status != 'opened':
             print('Deleted service: ' + service.name)

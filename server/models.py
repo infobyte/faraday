e394fa2827e90ccfe6f3610fd7113cebba8349f7
micabot <mranea@infobytesec.com>
2017-06-16 16:19:12 -0300

    Update DB version number

--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SCHEMA_VERSION = 'W.2.5.0'
+SCHEMA_VERSION = 'W.2.6.0'
 
 Base = declarative_base()
 

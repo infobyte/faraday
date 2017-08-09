a486f6f96189a9a7215bca53c68bb336907310e8
micabot <mranea@infobytesec.com>
2017-08-09 16:11:59 -0300

    Update version to 2.6.2

--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SCHEMA_VERSION = 'W.2.6.1'
+SCHEMA_VERSION = 'W.2.6.2'
 
 Base = declarative_base()
 

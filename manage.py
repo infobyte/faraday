59879a52d3a9cd21483503a2e93e2f65860ae9b1
Leonardo Lazzaro <leonardol@infobytesec.com>
2017-10-10 18:11:20 -0300

    Add show urls command

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ from server.web import app
 from server.importer import ImportCouchDB
 from server.commands.initdb import InitDB
 from server.commands.faraday_schema_display import DatabaseSchema
+from server.commands.app_urls import AppUrls
 
 manager = Manager(app)
 
@@ -16,4 +17,5 @@ if __name__ == "__main__":
     manager.add_command('generate_database_schemas', DatabaseSchema())
     manager.add_command('initdb', InitDB())
     manager.add_command('faraday_schema_display', DatabaseSchema())
+    manager.add_command('show_urls', AppUrls())
     manager.run()

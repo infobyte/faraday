873f8e6282ae10e3823b4a0584b1b27ab3b33e81
Leonardo Lazzaro <leonardol@infobytesec.com>
2017-10-23 18:01:37 -0300

    Delete programming

--- server/api/modules/hosts.py
+++ server/api/modules/hosts.py
@@ -127,29 +127,3 @@ class HostsView(PaginatedMixin,
         }
 
 HostsView.register(host_api)
-
-
-@gzipped
-@host_api.route('/ws/<workspace>/hosts', methods=['GET'])
-def list_hosts(workspace=None):
-    validate_workspace(workspace)
-    get_logger(__name__).debug("Request parameters: {!r}"\
-        .format(flask.request.args))
-
-    page = get_integer_parameter('page', default=0)
-    page_size = get_integer_parameter('page_size', default=0)
-    search = flask.request.args.get('search')
-    order_by = flask.request.args.get('sort')
-    order_dir = flask.request.args.get('sort_dir')
-
-    host_filter = filter_request_args('page', 'page_size', 'search', 'sort', 'sort_dir')
-
-    dao = HostDAO(workspace)
-    result = dao.list(search=search,
-                      page=page,
-                      page_size=page_size,
-                      order_by=order_by,
-                      order_dir=order_dir,
-                      host_filter=host_filter)
-
-    return flask.jsonify(result)

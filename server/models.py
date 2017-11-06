2b60d72121b993ca61b619b2ef53fbb53cb2be22
Ezequiel Tavella <ezequieltbh@infobytesec.com>
2017-11-06 15:04:44 -0300

    Update VERSION for publish, now is ok!

--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SCHEMA_VERSION = 'W.2.6.4'
+SCHEMA_VERSION = 'W.2.7'
 
 Base = declarative_base()
 

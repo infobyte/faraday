8caa9a4e09c996caeca78c635a52ffd76fd3348f
Leonardo Lazzaro <leonardol@infobytesec.com>
2017-11-17 18:43:24 -0300

    Goodbye ok, undefined, you win message!

--- server/www/scripts/commons/providers/server.js
+++ server/www/scripts/commons/providers/server.js
@@ -87,14 +87,6 @@ angular.module("faradayApp")
             var send_data = function(url, data, is_update, method) {
                 // undefined is just evil...
                 if (typeof is_update === "undefined") {var is_update = false;}
-                if (is_update && !data._rev) {
-                    // ok, undefined, you win
-                    console.log('ok, undefined, you win');
-                    return get(url).then(function s(r) {
-                        data._rev = r.data._rev;
-                        return serverComm(method, url, data);
-                    }).catch(function e(r) {$q.reject(r)});
-                }
                 return serverComm(method, url, data);
             };
 

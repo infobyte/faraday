d7e2be6f1855a4d3929774e110e48c31c48f7854
Leonardo Lazzaro <leonardol@infobytesec.com>
2018-02-19 12:49:02 -0300

    Remove interfaces from old command

--- bin/getAllIps.py
+++ bin/getAllIps.py
@@ -6,7 +6,7 @@ Faraday Penetration Test IDE
 Copyright (C) 2016  Infobyte LLC (http://www.infobytesec.com/)
 See the file 'doc/LICENSE' for the license information
 '''
-
+import re
 from persistence.server import models
 
 __description__ = "Get all scanned interfaces"
@@ -14,7 +14,14 @@ __prettyname__ = "Get All IPs Interfaces"
 
 
 def main(workspace='', args=None, parser=None):
-    for interface in models.get_interfaces(workspace):
-        print(interface.ipv4['address'])
+    ip_regex = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+    not_matching_count = 0
+    for host in models.get_hosts(workspace):
+        if re.match(ip_regex, host.ip):
+            print(host.ip)
+        else:
+            not_matching_count += 1
+    if not_matching_count:
+        print('Hosts that has invalid ip addresses {0}'.format(not_matching_count))
 
     return 0, None

2cf5865188ec7e04419fcde95124c8b4c7cd386e
Ezequiel Tavella <ezequieltbh@infobytesec.com>
2018-02-21 20:14:57 -0300

    Delete print of Websocket changes

--- persistence/server/changes_stream.py
+++ persistence/server/changes_stream.py
@@ -104,9 +104,7 @@ class WebsocketsChangesStream(ChangesStream):
         print error
 
     def on_close(selg, ws):
-        print "### closed ###"
-
-        print 'open'
+        pass
 
     def __enter__(self):
         return self

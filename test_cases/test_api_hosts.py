d6d0cb91681f896f4be8a92ed43a24254376299e
Matías Lang <matiasl@infobytesec.com>
2018-03-09 12:27:34 -0300

    Don't run hypothesis tests by default

--- test_cases/test_api_hosts.py
+++ test_cases/test_api_hosts.py
@@ -662,7 +662,9 @@ def host_json():
 
 
 @pytest.mark.usefixtures('logged_user')
+@pytest.mark.hypothesis
 def test_hypothesis(host_with_hostnames, test_client, session):
+    assert 0, "hypothesis test run"
     session.commit()
     HostData = host_json()
 

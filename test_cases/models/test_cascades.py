8fac6a00db8417be13c8484fc6957b4a2cb452e6
Matías Lang <matiasl@infobytesec.com>
2018-04-17 12:11:11 -0300

    Add delete user test
    It shouldn't cascade delete

--- test_cases/models/test_cascades.py
+++ test_cases/models/test_cascades.py
@@ -19,7 +19,7 @@ def test_delete_user(workspace, session):
 
 class TestCascadeDelete:
     @pytest.fixture(autouse=True)
-    def populate(self, workspace, service, session,
+    def populate(self, workspace, service, session, user,
                  vulnerability_factory, credential_factory,
                  empty_command_factory):
         session.commit()
@@ -27,6 +27,7 @@ class TestCascadeDelete:
         assert service.workspace_id == workspace.id
 
         workspace.set_scope(['*.infobytesec.com', '192.168.1.0/24'])
+        self.user = user
         self.workspace = workspace
         self.host = service.host
         self.host.set_hostnames(['a.com', 'b.com'])
@@ -35,25 +36,29 @@ class TestCascadeDelete:
         self.host_cred = credential_factory.create(
             host=self.host,
             service=None,
-            workspace=workspace
+            workspace=workspace,
+            creator=user,
         )
 
         self.service_cred = credential_factory.create(
             host=None,
             service=service,
-            workspace=workspace
+            workspace=workspace,
+            creator=user,
         )
 
         self.host_vuln = vulnerability_factory.create(
             host=self.host,
             service=None,
             workspace=workspace,
+            creator=user,
         )
 
         self.service_vuln = vulnerability_factory.create(
             host=None,
             service=service,
             workspace=workspace,
+            creator=user,
         )
 
         session.flush()
@@ -67,6 +72,7 @@ class TestCascadeDelete:
             content='test',
             object_type='vulnerability',
             object_id=self.service_vuln.id,
+            creator=user,
         )
         self.session.add(self.attachment)
 
@@ -76,6 +82,7 @@ class TestCascadeDelete:
             content='test',
             object_type='host',
             object_id=self.host.id,
+            creator=user,
         )
         self.session.add(self.host_attachment)
 
@@ -83,7 +90,8 @@ class TestCascadeDelete:
             text="test",
             object_type='host',
             object_id=self.host.id,
-            workspace=self.workspace
+            workspace=self.workspace,
+            creator=user,
         )
         self.session.add(self.comment)
 
@@ -93,23 +101,34 @@ class TestCascadeDelete:
             object_id=self.host.id,
             workspace=self.workspace,
             reply_to=self.comment,
+            creator=user,
         )
 
-        self.command = empty_command_factory.create(workspace=workspace)
+        self.command = empty_command_factory.create(
+            workspace=workspace,
+            creator=user)
         CommandObject.create(self.host_vuln, self.command)
         CommandObject.create(self.service_vuln, self.command)
 
         session.commit()
 
     @contextmanager
-    def assert_deletes(self, *objs):
+    # def assert_deletes(self, *objs, should_delete=True):
+    def assert_deletes(self, *objs, **kwargs):
+        # this could be better with python3 (like in the comment before the function
+        # definition)
+        should_delete = kwargs.get('should_delete', True)
         assert all(obj.id is not None for obj in objs)
         ids = [(obj.__table__, obj.id) for obj in objs]
         yield
         self.session.commit()
         for (table, id_) in ids:
+            if should_delete:
+                expected_count = 0
+            else:
+                expected_count = 1
             assert self.session.query(table).filter(
-                table.columns['id'] == id_).count() == 0
+                table.columns['id'] == id_).count() == expected_count
 
     def test_delete_host_cascade(self):
         with self.assert_deletes(self.host, self.service,
@@ -139,3 +158,17 @@ class TestCascadeDelete:
     def test_delete_host_attachments(self):
         with self.assert_deletes(self.host_attachment):
             self.session.delete(self.host)
+
+    def test_delete_user_does_not_delete_childs(self):
+        objs = [self.workspace, self.host, self.service,
+                self.host_cred, self.service_cred,
+                self.host_vuln, self.service_vuln,
+                self.attachment, self.host_attachment,
+                self.comment, self.reply_comment,
+                self.command]
+        for obj in objs:
+            assert obj.creator is not None
+        with self.assert_deletes(*objs, should_delete=False):
+            self.session.delete(self.user)
+        for obj in objs:
+            assert obj.creator is None

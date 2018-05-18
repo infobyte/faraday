53c5aa7e4930abe1e056ef12c8e155e7f4216228
Ezequiel Tavella <ezequieltbh@infobytesec.com>
2018-05-18 18:52:12 -0300

    Deleted views from couchdb

--- views/reports/_attachments/views/hosts/views/vulns/map.js
+++
@@ -1,9 +0,0 @@
-// Faraday Penetration Test IDE
-// Copyright (C) 2013  Infobyte LLC (http://www.infobytesec.com/)
-// See the file 'doc/LICENSE' for the license information
-
-function(doc) {
-	if(doc.type=="Vulnerability" || doc.type=="VulnerabilityWeb") {
-	  emit(doc.severity, 1);
-	}
-}

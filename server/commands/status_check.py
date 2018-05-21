6e4c10c8a8e7d5f9e9e24286b620079f1d351f89
Winna_Z <zoem@infobytesec.com>
2018-05-21 18:22:05 -0300

    check_server_status() done

--- server/commands/status_check.py
+++ server/commands/status_check.py
@@ -1,18 +1,25 @@
 from colorama import init
 from colorama import Fore, Back, Style
-
+import socket
+from server.utils.daemonize import is_server_running
 init()
 
 
-def check_server_running():
-    pass
 
+def check_server_running():
+    pid = is_server_running()
+    if pid is not None:
+        print('Faraday Server is Running. PID:{PID} {green} Running. \
+        '.format(green=Fore.GREEN, PID=pid))
+        return True
+    else:
+        print('Faraday Server is not running {red} Not Running. \
+        '.format(red=Fore.RED))
+        return True
 
 def check_open_ports():
     pass
 
-
 def full_status_check():
-    print('{red} Incomplete Check {white}.'.format(red=Fore.RED, white=Fore.WHITE))
     check_server_running()
-    check_open_ports()
+    

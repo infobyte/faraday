582f4d0eb8c7ecba6e33849b2f8b8989fe71eb60
Matías Lang <matiasl@infobytesec.com>
2018-06-08 15:42:18 -0300

    Add metadata to alembic env to allow migration autogeneration

--- migrations/env.py
+++ migrations/env.py
@@ -22,8 +22,8 @@ fileConfig(config.config_file_name)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = db.metadata
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

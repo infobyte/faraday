1c84700a9eb6d2b8df1f40d5160e8ee9481be853
Matías Lang <matiasl@infobytesec.com>
2018-06-12 16:42:11 -0300

    Add header to help page

--- server/www/scripts/commons/controllers/headerCtrl.js
+++ server/www/scripts/commons/controllers/headerCtrl.js
@@ -12,7 +12,7 @@ angular.module('faradayApp')
                 var noSwitcher = [
                     "", "home", "login", "index", "workspaces", "users", "licenses",
                     "taskgroup", "executive", // Only for white versions!!
-                    "vulndb", "comparison", "webshell"
+                    "vulndb", "comparison", "webshell", "help"
                 ];
                 return noSwitcher.indexOf($scope.component) < 0;
             };

b7edad130bbd7cafcee94365d4f55d2127c5a383
Leonardo Lazzaro <leonardol@infobytesec.com>
2018-06-14 20:06:02 +0000

    [FIX] PEP8 and a fix to params value for new api

--- plugins/repo/brutexss/plugin.py
+++ plugins/repo/brutexss/plugin.py
@@ -3,13 +3,15 @@
 
 '''
 Faraday Penetration Test IDE
-Copyright (C) 2017  Infobyte LLC (http://www.infobytesec.com/)
+Copyright (C) 2018  Infobyte LLC (http://www.infobytesec.com/)
 See the file 'doc/LICENSE' for the license information
 '''
 
-from plugins import core
 import re
 import socket
+from urlparse import urlparse
+
+from plugins import core
 
 __author__ = "Roberto Focke"
 __copyright__ = "Copyright (c) 2017, Infobyte LLC"
@@ -19,42 +21,44 @@ __version__ = "1.0.0"
 
 class brutexss (core.PluginBase):
 
-	def __init__(self):
+        def __init__(self):
+                core.PluginBase.__init__(self)
+                self.id = "brutexss"
+                self.name = "brutexss"
+                self.plugin_version = "0.0.2"
+                self.version = "1.0.0"
+                self.protocol='tcp'
 
-		core.PluginBase.__init__(self)
-        	self.id = "brutexss"
-        	self.name = "brutexss"
-        	self.plugin_version = "0.0.1"
-        	self.version = "1.0.0"
-        	self.protocol='tcp'
+                self._command_regex = re.compile(r'^(sudo brutexss|brutexss|sudo brutexss\.py|brutexss\.py|python brutexss\.py|\.\/brutexss\.py).*?')
 
-        	self._command_regex = re.compile(r'^(sudo brutexss|brutexss|sudo brutexss\.py|brutexss\.py|python brutexss\.py|\.\/brutexss\.py).*?')
-        
+        def parseOutputString(self, output, debug=False):
+                lineas = output.split("\n")
+                parametro=[]
+                found_vuln = False
+                for linea in lineas:
+                    if (linea.find("is available! Good!")>0):
+                        print(linea)
+                        url = re.findall('(?:[-\w.]|(?:%[\da-fA-F]{2}))+', linea)[0]
+                        port = 80
+                        if urlparse(url).scheme == 'https':
+                            port = 443
+                        netloc_splitted = urlparse(url).netloc.split(':')
+                        if len(netloc_splitted) > 1:
+                            port = netloc_splitted[1]
+                    if ((linea.find("Vulnerable")>0) and (linea.find("No")<0)):
+                        vuln_list = re.findall("\w+", linea)
+                        if vuln_list[2]=="Vulnerable":
+                                parametro.append(vuln_list[1])
+                                found_vuln=len(parametro) > 0
+                                host_id = self.createAndAddHost(url)
+                                address=socket.gethostbyname(url)
+                                interface_id = self.createAndAddInterface(host_id,address,ipv4_address=address,hostname_resolution=url)
+                                service_id = self.createAndAddServiceToInterface(host_id,interface_id,self.protocol,'tcp',ports=[port],status='Open',version="",description="")
+                if found_vuln:
+                    self.createAndAddVulnWebToService(host_id,service_id,name="xss",desc="XSS",ref='',severity='med',website=url,path='',method='',pname='',params=''.join(parametro),request='',response='')
 
-        
-
-	def parseOutputString(self, output, debug=False):
-		lineas = output.split("\n")
-    		parametro=[]
-		for linea in lineas:
-        		if (linea.find("is available! Good!")>0):
-            			url = re.findall('(?:[-\w.]|(?:%[\da-fA-F]{2}))+', linea)
-            			print url
-        		if ((linea.find("Vulnerable")>0) and (linea.find("No")<0)):
-            			list = re.findall("\w+", linea)
-            			if list[2]=="Vulnerable":
-                			parametro.append(list[1])
-                			j=len(parametro)
-                			host_id = self.createAndAddHost(url[0])
-                			address=socket.gethostbyname(url[0])
-                			interface_id = self.createAndAddInterface(host_id,address,ipv4_address=address,hostname_resolution=url[0])
-                			service_id = self.createAndAddServiceToInterface(host_id,interface_id,self.protocol,'tcp',ports=['80'],status='Open',version="",description="")
-                if j:
-		
-			self.createAndAddVulnWebToService(host_id,service_id,name="xss",desc="XSS",ref='',severity='med',website=url[0],path='',method='',pname='',params=parametro,request='',response='')
-                               
-	def processCommandString(self, username, current_path, command_string):
-        	return None
+        def processCommandString(self, username, current_path, command_string):
+                return None
 
 
 def createPlugin():

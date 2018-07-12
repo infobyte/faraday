c588c38f10c565168eea75aeee95ec99bf0b39f9
Eric Horvat <erich@infobytesec.com>
2018-07-12 16:33:22 -0300

    [ADD] VERSION file test check

--- test_cases/test_server_config.py
+++ test_cases/test_server_config.py
@@ -7,6 +7,8 @@ See the file 'doc/LICENSE' for the license information
 import random
 import string
 import mock
+import os
+import re
 
 from server.config import (
     copy_default_config_to_local,
@@ -32,3 +34,49 @@ def test_copy_default_config_to_local_does_not_exist(copyfile, makedirs):
     assert copy_default_config_to_local() is None
     assert not makedirs.called
     assert not copyfile.called
+
+VERSION_PATTERN = r"""
+    v?
+    (?:
+        (?:(?P<epoch>[0-9]+)!)?                           # epoch
+        (?P<release>[0-9]+(?:\.[0-9]+)*)                  # release segment
+        (?P<pre>                                          # pre-release
+            [-_\.]?
+            (?P<pre_l>(a|b|c|rc|alpha|beta|pre|preview))
+            [-_\.]?
+            (?P<pre_n>[0-9]+)?
+        )?
+        (?P<post>                                         # post release
+            (?:-(?P<post_n1>[0-9]+))
+            |
+            (?:
+                [-_\.]?
+                (?P<post_l>post|rev|r)
+                [-_\.]?
+                (?P<post_n2>[0-9]+)?
+            )
+        )?
+        (?P<dev>                                          # dev release
+            [-_\.]?
+            (?P<dev_l>dev)
+            [-_\.]?
+            (?P<dev_n>[0-9]+)?
+        )?
+    )
+    (?:\+(?P<local>[a-z0-9]+(?:[-_\.][a-z0-9]+)*))?       # local version
+"""
+
+_regex = re.compile(
+    r"^\s*" + VERSION_PATTERN + r"\s*$",
+    re.VERBOSE | re.IGNORECASE,
+)
+
+def isPEP440(arg):
+    return not _regex.match(arg) is None
+
+
+def test_exists_and_content():
+    f = open(os.path.join(os.getcwd(),"..","VERSION"),"r")
+    line1 = f.readline().rstrip()
+    assert f.read() == ''
+    assert isPEP440(line1)

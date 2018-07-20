34a96fc1587fbd92a93b656a95b8aba3987c5cdb
Leonardo Lazzaro <leonardol@infobytesec.com>
2018-07-20 14:16:55 -0300

    add postgresql to install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ then
     apt-get update
 
     #Install community dependencies
-    for pkg in build-essential python-setuptools python-pip python-dev libpq-dev libffi-dev gir1.2-gtk-3.0 gir1.2-vte-2.91 python-gobject zsh curl python-psycopg2 ; do
+    for pkg in build-essential python-setuptools python-pip python-dev libpq-dev libffi-dev gir1.2-gtk-3.0 gir1.2-vte-2.91 python-gobject zsh curl python-psycopg2 postgresql; do
         apt-get install -y $pkg
     done
 fi

a41dbaf04ea261333892cbc99023d35db1182f38
Matías Lang <matiasl@infobytesec.com>
2018-08-07 18:45:40 -0300

    Don't use pypi2nix with shell.nix
    It was causing me many problems, so I prefer to keep using pip and
    virtualenv while developing. Now the shell.nix file uses an existing or
    creates a new virtualenv with the faraday requirements and sources it

--- shell.nix
+++ shell.nix
@@ -2,6 +2,25 @@ with (import <nixpkgs> {});
 let
 in
   mkShell {
-    buildInputs = with (import ./default.nix);
-      [server python.interpreter];
+    buildInputs = with python27Packages;
+      [virtualenv pyopenssl psycopg2 pillow];
+    shellHook = ''
+      unset SOURCE_DATE_EPOCH  # Required to make pip work
+
+      makeVirtualEnv(){
+        rm -rf venv
+        virtualenv --system-site-packages venv
+        source venv/bin/activate
+        pip install -r requirements_server.txt
+        pip install -r requirements_dev.txt
+      }
+
+      if [[ -d venv ]]; then
+        echo Creating new virtualenv
+        source venv/bin/activate
+      else
+        makeVirtualEnv
+      fi
+
+    '';
   }

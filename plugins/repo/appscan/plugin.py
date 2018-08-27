f99ac79f7edba1e9e12cc59efcd3e8ac0027bbed
montive <jmontilva@infobytesec.com>
2018-08-27 18:55:14 -0300

    Appscan plugin updated

--- plugins/repo/appscan/plugin.py
+++ plugins/repo/appscan/plugin.py
@@ -8,6 +8,7 @@ See the file 'doc/LICENSE' for the license information
 '''
 
 import pprint
+import socket
 from plugins import core
 from lxml import objectify
 from urlparse import urlparse
@@ -26,7 +27,7 @@ def get_ip(domain):
         data = socket.gethostbyname_ex(domain)
         ip = repr(data[2])
         return ip
-    except Exception:
+    except Exception as e:
         return domain
 
 
@@ -43,53 +44,67 @@ class AppscanParser():
         self.issue_list = []
         self.obj_xml = objectify.fromstring(output)
 
+    def parse_issue_type(self):
+        res = {}
+
+        for issue_type in self.obj_xml["issue-type-group"]["item"]:
+            res[issue_type.attrib['id']] = {
+                'name': issue_type.name.text, 
+                'advisory': issue_type["advisory"]["ref"].text,
+                'fix-recommendation': issue_type["fix-recommendation"]["ref"].text
+                } 
+
+            if "cve" in issue_type:
+                res[issue_type.attrib['id']] = {'cve': issue_type["cve"].text} 
+        
+        return res
+
+    
+    def parse_advisory_group(self, advisory):
+        '''
+        Function that parse advisory-group in order to get the item's description
+        '''
+        for item in self.obj_xml["advisory-group"]["item"]:
+            if item.attrib['id'] == advisory:
+                return item['advisory']['testTechnicalDescription']['text'].text
+
+
+    def get_url(self, ref):
+        for item in self.obj_xml['url-group']['item']:
+            if item.attrib['id'] == ref:
+                return item['name'].text
+
+
     def parse_issues(self):
-        for issue in self.obj_xml["issue-type-group"]["item"]:
-            url_list = []
+        issue_type = self.parse_issue_type()
+        for issue in self.obj_xml["issue-group"]["item"]:
+            issue_data = issue_type[issue['issue-type']['ref']]
             obj_issue = {}
 
-            obj_issue["name"] = issue["name"].text
-            obj_issue['advisory'] = issue["advisory"]["ref"].text
+            obj_issue["name"] = issue_data["name"]
+            obj_issue['advisory'] = issue_data["advisory"]
 
-            if("cve" in issue):
-                obj_issue['cve'] = issue["cve"].text
+            if("cve" in issue_data):
+                obj_issue['cve'] = issue_data["cve"].text
 
-            for threat in self.obj_xml["url-group"]["item"]:
-                if threat["issue-type"] == issue["fix-recommendation"]["ref"]:
-
-                    url_list.append(threat['name'].text)
-
-                    obj_issue['urls'] = url_list
-
-                    for item in self.obj_xml["issue-group"]["item"]:
-
-                        if int(item["url"]["ref"]) == int(threat.get('id')):
-                            if "test-http-traffic" in item["variant-group"]["item"] and item["issue-type"]["ref"] == threat['issue-type']:
-
-                                http_traffic = item["variant-group"]["item"]["test-http-traffic"].text.split("\n\n")
-
-                                obj_issue["request"] = http_traffic[0]
-                                obj_issue["response"] = http_traffic[1]
-
-                        if(issue["threat-class"]["ref"] == item["threat-class"]["ref"]):
-
-                            obj_issue["severity"] = item["severity"].text
-                            obj_issue["cvss_score"] = item["cvss-score"].text
-                            if ("issue-tip" in item["variant-group"]["item"]["issue-information"]):
-                                obj_issue["issue_description"] = item["variant-group"]["item"]["issue-information"]["issue-tip"].text
-                            break
+            obj_issue['url'] = self.get_url(issue['url']['ref'].text)
+            obj_issue['cvss_score'] = issue["cvss-score"].text
+            obj_issue['response'] = issue['variant-group']['item']['issue-information']["testResponseChunk"].text
+            obj_issue['request'] = issue['variant-group']['item']["test-http-traffic"].text
+            obj_issue['severity'] = issue['severity'].text
+            obj_issue['issue-description'] = self.parse_advisory_group(issue_data['advisory'])
 
             for recomendation in self.obj_xml["fix-recommendation-group"]["item"]:
                 full_data = ""
-                if(recomendation.attrib['id'] == issue["fix-recommendation"]["ref"]):
+                if(recomendation.attrib['id'] == issue_data["fix-recommendation"]):
                     for data in recomendation['general']['fixRecommendation']["text"]:
                         full_data += '' + data
                     obj_issue["recomendation"] = full_data
                     if(hasattr(recomendation['general']['fixRecommendation'], 'link')):
                         obj_issue["ref_link"] = recomendation['general']['fixRecommendation']['link'].text
-
+            
             self.issue_list.append(obj_issue)
-
+        
         return self.issue_list
 
     def get_scan_information(self):
@@ -118,58 +133,39 @@ class AppscanPlugin(core.PluginBase):
         parser = AppscanParser(output)
         issues = parser.parse_issues()
         for issue in issues:
+            url_parsed = urlparse(str(issue['url']))
 
-            if "urls" not in issue:
-                continue
+            host_id = self.createAndAddHost(url_parsed.hostname)
 
-            for url in issue["urls"]:
+            service_id = self.createAndAddServiceToHost(
+                host_id,
+                url_parsed.scheme,
+                ports=[url_parsed.port],
+                protocol="tcp?HTTP")
 
-                url_parsed = urlparse(url)
+            refs = []
+            if "ref_link" in issue:
+                refs.append("Fix link: " + issue["ref_link"])
+            if "cvss_score" in issue:
+                refs.append("CVSS Score: " + issue["cvss_score"])
+            if "cve" in issue:
+                refs.append("CVE: " + issue["cve"])
+            if "advisory" in issue:
+                refs.append("Advisory: " + issue["advisory"])
 
-                # Get domain of URL.
-                if url_parsed.netloc:
-                    hostname = url_parsed.netloc
-                    ip = get_ip(url_parsed.netloc)
-                elif url_parsed.path:
-                    hostname = url_parsed.path
-                    ip = get_ip(url_parsed.path)
-
-                host_id = self.createAndAddHost(ip)
-                interface_id = self.createAndAddInterface(
-                    host_id,
-                    ip,
-                    ipv4_address=ip,
-                    hostname_resolution=[hostname])
-
-                service_id = self.createAndAddServiceToInterface(
-                    host_id,
-                    interface_id,
-                    "HTTP Server",
-                    protocol="tcp?HTTP")
-
-                refs = []
-                if "ref_link" in issue:
-                    refs.append("Fix link: " + issue["ref_link"])
-                if "cvss_score" in issue:
-                    refs.append("CVSS Score: " + issue["cvss_score"])
-                if "cve" in issue:
-                    refs.append("CVE: " + issue["cve"])
-                if "advisory" in issue:
-                    refs.append("Advisory: " + issue["advisory"])
-
-                self.createAndAddVulnWebToService(
-                    host_id,
-                    service_id,
-                    cleaner_unicode(issue["name"]),
-                    cleaner_unicode(issue["issue_description"]) if "issue_description" in issue else "",
-                    ref=refs,
-                    severity=issue["severity"],
-                    resolution=cleaner_unicode(issue["recomendation"]),
-                    website=hostname,
-                    path=url_parsed.path,
-                    request=cleaner_unicode(issue["request"]) if "request" in issue else "",
-                    response=cleaner_unicode(issue["response"]) if "response" in issue else "",
-                    method=issue["request"][0:3] if "request" in issue else "")
+            self.createAndAddVulnWebToService(
+                host_id,
+                service_id,
+                cleaner_unicode(issue["name"]),
+                cleaner_unicode(issue["issue_description"]) if "issue_description" in issue else "",
+                ref=refs,
+                severity=issue["severity"],
+                resolution=cleaner_unicode(issue["recomendation"]),
+                website=url_parsed.netloc,
+                path=url_parsed.path,
+                request=cleaner_unicode(issue["request"]) if "request" in issue else "",
+                response=cleaner_unicode(issue["response"]) if "response" in issue else "",
+                method=issue["request"][0:3] if "request" in issue else "")
 
         return
 
@@ -179,3 +175,12 @@ class AppscanPlugin(core.PluginBase):
 
 def createPlugin():
     return AppscanPlugin()
+
+
+if __name__ == '__main__':
+    parser = AppscanPlugin()
+    with open('/home/javier/Reports_Testing/MCMD.XML', 'r') as report:
+        parser.parseOutputString(report.read())
+        for item in parser.items:
+            if item.status == 'up':
+                print item

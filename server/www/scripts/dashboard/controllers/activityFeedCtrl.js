fd84f226178aeef1ea98be26abf390b3bc07a4dc
Jorge Luis González Iznaga <jliznaga@gmail.com>
2018-09-06 16:13:28 -0400

    Fix activity_feed slide up

--- server/www/scripts/dashboard/controllers/activityFeedCtrl.js
+++ server/www/scripts/dashboard/controllers/activityFeedCtrl.js
@@ -7,23 +7,41 @@
 angular.module('faradayApp')
     .controller('activityFeedCtrl',
         ['$scope', '$routeParams', 'dashboardSrv',
-        function($scope, $routeParams, dashboardSrv) {
-            
-            var vm = this;
-            vm.commands = [];
+            function ($scope, $routeParams, dashboardSrv) {
 
-            // Get last 15 commands
-            var init = function() {
-                if($routeParams.wsId != undefined) {
-                    $scope.workspace = $routeParams.wsId;
+                var vm = this;
+                vm.commands = [];
 
-                    dashboardSrv.getActivityFeed($scope.workspace)
-                        .then(function(response) {
-                            vm.commands = response.activities;
-                        });
-                }
-            };
+                // Get last 15 commands
+                var init = function () {
+                    if ($routeParams.wsId != undefined) {
+                        $scope.workspace = $routeParams.wsId;
+                        $scope.cmdLimit = 5;
+                        $scope.isExpanded = false;
 
-            dashboardSrv.registerCallback(init);
-            init();
-    }]);
+                        dashboardSrv.getActivityFeed($scope.workspace)
+                            .then(function (response) {
+                                vm.commands = response.activities;
+                            });
+                    }
+                };
+
+                $scope.toggleExpanded = function () {
+                    var lastVulnPanel = angular.element('#last-vuln-panel');
+                    var vulnsByPrice = angular.element('#vulns-by-price');
+                    if ($scope.isExpanded) {
+                        $scope.cmdLimit = 5;
+                        $scope.isExpanded = false;
+                        lastVulnPanel.removeClass('slide-up');
+                        vulnsByPrice.removeClass('slide-up');
+                    }else{
+                        $scope.cmdLimit = 15; // Should be a constant
+                        $scope.isExpanded = true;
+                        lastVulnPanel.addClass('slide-up');
+                        vulnsByPrice.addClass('slide-up');
+                    }
+                };
+
+                dashboardSrv.registerCallback(init);
+                init();
+            }]);

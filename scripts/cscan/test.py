54b7329c1bb013490e65c5276bb17f0cf9361e6d
winnaz <zoem@infobytesec.com>
2018-10-10 20:20:32 -0300

    deleted dummy test scripts

--- scripts/cscan/test.py
+++
@@ -1,7 +0,0 @@
-import os
-import shutil
-
-for section in config.sections():
-    dictionary[section] = {}
-    for option in config.options(section):
-        dictionary[section][option] = config.get(section, option)

d3079feabb2b6d739ce5df9d08536ff12fc06ba8
Leonardo Lazzaro <leonardol@infobytesec.com>
2018-10-29 16:53:16 -0300

    Add cusotm fields schema api endpoint

--- server/app.py
+++ server/app.py
@@ -76,6 +76,7 @@ def register_blueprints(app):
     from server.api.modules.upload_reports import upload_api
     from server.api.modules.websocket_auth import websocket_auth_api
     from server.api.modules.get_exploits import exploits_api
+    from server.api.modules.custom_fields import custom_fields_schema_api
     app.register_blueprint(commandsrun_api)
     app.register_blueprint(activityfeed_api)
     app.register_blueprint(credentials_api)
@@ -92,6 +93,7 @@ def register_blueprints(app):
     app.register_blueprint(upload_api)
     app.register_blueprint(websocket_auth_api)
     app.register_blueprint(exploits_api)
+    app.register_blueprint(custom_fields_schema_api)
 
 
 def check_testing_configuration(testing, app):

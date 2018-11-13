3feebaa98b544ce60862fecca35a02669fa48cdd
Leonardo Lazzaro <leonardol@infobytesec.com>
2018-11-13 15:54:33 -0300

    Lower maximum value since postgres uses signed ints

--- test_cases/factories.py
+++ test_cases/factories.py
@@ -150,7 +150,7 @@ class ReferenceTemplateFactory(FaradayFactory):
 class ServiceFactory(WorkspaceObjectFactory):
     name = FuzzyText()
     description = FuzzyText()
-    port = FuzzyInteger(1, 2**32)  # Using 2**16 it generates many collisions
+    port = FuzzyInteger(1, 2**31)  # Using 2**16 it generates many collisions
     protocol = FuzzyChoice(['TCP', 'UDP'])
     host = factory.SubFactory(HostFactory, workspace=factory.SelfAttribute('..workspace'))
     status = FuzzyChoice(Service.STATUSES)

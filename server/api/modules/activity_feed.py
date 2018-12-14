6d7e387e01196d883d5fc22aeb9ca3e8cc5198b4
montive <jmontilva@infobytesec.com>
2018-12-14 18:43:27 -0300

    [DEL] datetime is already been imported in "from date import datetime"

--- server/api/modules/activity_feed.py
+++ server/api/modules/activity_feed.py
@@ -1,7 +1,6 @@
 # Faraday Penetration Test IDE
 # Copyright (C) 2018  Infobyte LLC (http://www.infobytesec.com/)
 # See the file 'doc/LICENSE' for the license information
-import datetime
 import time
 from datetime import datetime
 

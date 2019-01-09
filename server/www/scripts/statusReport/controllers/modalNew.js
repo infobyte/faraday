e4c7c8bc164fd5205782faa34cbdea4cee07c3e6
Jorge Luis González Iznaga <jliznaga@gmail.com>
2019-01-09 17:35:30 -0300

    [ADD] Ability to apply new fields to vulns from templates

--- server/www/scripts/statusReport/controllers/modalNew.js
+++ server/www/scripts/statusReport/controllers/modalNew.js
@@ -78,6 +78,7 @@ angular.module('faradayApp')
                 path: "",
                 pname: "",
                 policyviolations: [],
+                easeofresolution: "",
                 query: "",
                 refs: [],
                 request: "",

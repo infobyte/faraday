3ea8151fcf6159520a718bbb148be6b79c755ed9
Jorge Luis González Iznaga <jliznaga@gmail.com>
2019-02-13 18:57:23 -0300

    [FIX] Side bar selected custom_field

--- server/www/scripts/admin/customFields/controllers/customFields.js
+++ server/www/scripts/admin/customFields/controllers/customFields.js
@@ -4,59 +4,73 @@
 
 angular.module('faradayApp')
     .controller('customFieldsCtrl',
-        ['$scope', 'ServerAPI', function($scope, ServerAPI) {
+        ['$scope', 'ServerAPI', function ($scope, ServerAPI) {
 
-        $scope.customFields = [];
-        $scope.selected_cf = {};
-        var init  = function () {
-            loadCustomFields();
-        };
+            $scope.customFields = [];
+            $scope.selected_cf = {
+                field_display_name: "",
+                field_name: "",
+                field_order: null,
+                field_type: null
+            };
+            $scope.isEditable = false;
+
+            var init = function () {
+                loadCustomFields();
+            };
 
 
-        var loadCustomFields = function () {
-            ServerAPI.getCustomFields().then(
-                function(response){
-                    $scope.customFields = response.data;
-                });
-        };
+            var loadCustomFields = function () {
+                ServerAPI.getCustomFields().then(
+                    function (response) {
+                        $scope.customFields = response.data;
+                    });
+            };
 
 
-        $scope.setCustomField = function (cf) {
-            $scope.selected_cf = angular.copy(cf);
-            $scope.changeType(cf.field_type);
-        };
+            $scope.setCustomField = function (cf) {
+                $scope.selected_cf = angular.copy(cf);
+                $scope.isEditable = true;
+                $scope.changeType(cf.field_type);
+            };
 
-        $scope.updateBtnTypeColor = function (type) {
-            var color = undefined;
-            switch (type) {
-                case "str":
-                    color = '#2e97bd';
-                    break;
-                case "list":
-                    color = '#a1ce31';
-                    break;
-                case "int":
-                    color = '#932ebe';
-                    break;
-                default:
-                    color = '#AAAAAA';
-                    break;
-            }
+            $scope.updateBtnTypeColor = function (type) {
+                var color = undefined;
+                switch (type) {
+                    case "str":
+                        color = '#2e97bd';
+                        break;
+                    case "list":
+                        color = '#a1ce31';
+                        break;
+                    case "int":
+                        color = '#932ebe';
+                        break;
+                    default:
+                        color = '#AAAAAA';
+                        break;
+                }
 
-            angular.element('#btn-chg-type').css('background-color', color);
-            angular.element('#caret-chg-type').css('background-color', color);
-        };
+                angular.element('#btn-chg-type').css('background-color', color);
+                angular.element('#caret-chg-type').css('background-color', color);
+            };
 
-        $scope.changeType = function (type) {
-            $scope.selected_cf.field_type = type;
-            $scope.updateBtnTypeColor(type);
-        };
+            $scope.changeType = function (type) {
+                $scope.selected_cf.field_type = type;
+                $scope.updateBtnTypeColor(type);
+            };
 
-        $scope.clearSelection = function () {
-            $scope.selected_cf = undefined;
-            $scope.updateBtnTypeColor(null);
+            $scope.clearSelection = function () {
+                $scope.selected_cf = {
+                    field_display_name: "",
+                    field_name: "",
+                    field_order: null,
+                    field_type: null
+                };
+                $scope.updateBtnTypeColor(null);
+                $scope.isEditable = false;
 
-        };
+            };
 
-	    init();
-    }]);
+            init();
+        }]);

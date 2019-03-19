10af0884330670770ab254e3fc2aef14bf3b55f3
Matías Lang <matiasl@infobytesec.com>
2019-03-19 12:57:15 -0300

    Include server/www in the build
    So the path is not hardcoded anymore. I had to use a MANIFEST.in
    instead of package_data to recursively include directories. I had
    to spend a lot of time debugging why it wasn't working, and it turned
    out to be that I had to prepend a ./ in the path. This is an ugly
    setuptools bug.

--- setup.py
+++ setup.py
@@ -179,9 +179,10 @@ setup(
     #
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
-    package_data={  # Optional
-        'faraday': ['VERSION', 'requirements_server.txt'],
-    },
+    # package_data={  # Optional
+    #     'faraday': ['VERSION', 'requirements_server.txt'],
+    # },
+    include_package_data=True,
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:

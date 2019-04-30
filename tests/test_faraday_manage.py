401e8afe6b0fbc87ff0d9189ee342261edf1dec5
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-04-30 17:00:00 -0300

    [ADD] Create tables in migrate test

--- tests/test_faraday_manage.py
+++ tests/test_faraday_manage.py
@@ -15,6 +15,29 @@ def test_manage_migrate():
     """
     if 'POSTGRES_DB' in os.environ:
         # I'm on gitlab ci runner
+        # I will overwrite server.ini
+        connection_string = 'postgresql+psycopg2://{username}:{password}@postgres/{database}'.format(
+            username=os.environ['POSTGRES_USER'],
+            password=os.environ['POSTGRES_PASSWORD'],
+            database=os.environ['POSTGRES_DB'],
+        )
+        faraday_config = ConfigParser.SafeConfigParser()
+        config_path = os.path.expanduser('~/.faraday/config/server.ini')
+        faraday_config.read(config_path)
+        try:
+            faraday_config.add_section('database')
+        except ConfigParser.DuplicateSectionError:
+            pass
+        faraday_config.set('database', 'connection_string', connection_string)
+        with open(config_path, 'w') as faraday_config_file:
+            faraday_config.write(faraday_config_file)
+
+        command = ['faraday-manage', 'create-tables']
+        subproc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                   cwd=FARADAY_BASE)  # TODO THIS SHOULD FIXED AND THEN REMOVED ISSUE 5570
+        subproc.wait()
+        std, err = subproc.communicate()
+        assert subproc.returncode == 0, ('Create tables failed!', std, err)
 
         command = ['faraday-manage', 'migrate']
         subproc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)

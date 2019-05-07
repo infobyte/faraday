ed3f6d9839e0444712866ade603b6f48f04b1b7d
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-05-07 18:11:24 -0300

    Reorder prepare_test_env

--- prepare_test_env.sh
+++ prepare_test_env.sh
@@ -2,9 +2,9 @@
 pip install virtualenv
 virtualenv -p python2 faraday_venv
 source faraday_venv/bin/activate
+python setup.py install
 pip install --upgrade responses pytest-xdist pytest-cov
 pip install --upgrade -r requirements_dev.txt
-python setup.py install
 mkdir -p ~/.faraday/config
-cp test_cases/data/server.ini ~/.faraday/config
+cp tests/data/server.ini ~/.faraday/config
 sed -i 's/mapped_table/persist_selectable/' faraday_venv/lib/python2.7/site-packages/flask_sqlalchemy/__init__.py  # TODO remove when flask_sqlalchemy fixes the issue

3746eb3bd4bb02e6994b03dfdbddeea858e436ab
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-05-07 18:20:19 -0300

    [REM] sed in gitlab.ci

--- prepare_test_env.sh
+++ prepare_test_env.sh
@@ -7,4 +7,4 @@ pip install --upgrade responses pytest-xdist pytest-cov
 pip install --upgrade -r requirements_dev.txt
 mkdir -p ~/.faraday/config
 cp tests/data/server.ini ~/.faraday/config
-sed -i 's/mapped_table/persist_selectable/' faraday_venv/lib/python2.7/site-packages/flask_sqlalchemy/__init__.py  # TODO remove when flask_sqlalchemy fixes the issue
+#sed -i 's/mapped_table/persist_selectable/' faraday_venv/lib/python2.7/site-packages/flask_sqlalchemy/__init__.py  # TODO remove when flask_sqlalchemy fixes the issue

b6f027d015371b0afa75545941ea629ae63c48bc
Mariano Marchetta <mmarchetta@infobytesec.com>
2019-05-09 11:18:45 -0300

    Fix populate of custom fields

--- faraday/server/www/scripts/statusReport/controllers/modalNew.js
+++ faraday/server/www/scripts/statusReport/controllers/modalNew.js
@@ -246,6 +246,12 @@ angular.module('faradayApp')
                 policyviolations.push({value: policyviolation});
             });
             vm.data.policyviolations = policyviolations;
+
+            if(item.customfields){
+                for(var cf in item.customfields){
+                    vm.data.custom_fields[cf] = item.customfields[cf];
+                }
+            }
         }
 
         vm.updateBtnSeverityColor = function (severity) {

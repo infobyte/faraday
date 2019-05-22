12cd140df0f9f298ed401bfc06cd4b8965313c0f
Jorge Luis González Iznaga <jliznaga@gmail.com>
2019-05-22 17:41:25 -0400

    [FIX] migration file

--- faraday/migrations/versions/9c4091d1a09b_create_agent_table.py
+++ faraday/migrations/versions/9c4091d1a09b_create_agent_table.py
@@ -8,7 +8,6 @@ Create Date: 2019-05-22 19:17:31.444968+00:00
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 from sqlalchemy.dialects import postgresql
 
@@ -28,6 +27,9 @@ def upgrade():
         sa.Column('projects', sa.Integer),
         sa.Column('jobs', sa.Integer),
         sa.Column('create_date', sa.DateTime),
+        sa.Column('update_date', sa.DateTime),
+        sa.Column('creator_id', sa.Integer),
+        sa.Column('update_user_id', sa.Integer),
     )
     # There is a bug with alembic and postgresql with enum types
     # alembic tries to create the enum type when creating a new table.
@@ -43,12 +45,24 @@ def upgrade():
     op.add_column('agent', sa.Column('type', sa.Enum(('shared', 'specific'), name='agent_types'), nullable=False))
     op.add_column('agent', sa.Column('status', sa.Enum(('locked', 'pause', 'offline'), name='agent_status'), nullable=True))
 
+    op.create_foreign_key(
+        'agent_creator_id_fkey',
+        'agent',
+        'faraday_user', ['creator_id'], ['id']
+    )
+
+    op.create_foreign_key(
+        'agent_update_user_id_fkey',
+        'agent',
+        'faraday_user', ['update_user_id'], ['id']
+    )
+
 
 def downgrade():
+    op.drop_table('agent')
+
     agent_types = postgresql.ENUM('shared', 'specific', name='agent_types')
     agent_types.drop(op.get_bind())
 
     agent_status = postgresql.ENUM('locked', 'pause', 'offline', name='agent_status')
     agent_status.drop(op.get_bind())
-
-    op.drop_table('agent')

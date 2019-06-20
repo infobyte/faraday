905556002da955e24cbfc6c271610590f83166f8
Pablo Marcelli <marcellipablo@gmail.com>
2019-06-20 13:16:47 -0300

    remove the check of faradayversion

--- faraday/client/start_client.py
+++ faraday/client/start_client.py
@@ -434,7 +434,6 @@ def check_faraday_version():
     except RuntimeError:
         getLogger("launcher").error(
             "The server is running a different Faraday version than the client you are running. Version numbers must match!")
-        sys.exit(2)
 
 
 def try_login_user(server_uri, api_username, api_password):

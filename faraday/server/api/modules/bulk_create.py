1c03cb635787be011471b6ef2cdf49e761e4bb8b
Matías Lang <matiasl@infobytesec.com>
2019-07-02 16:05:02 -0300

    Allow host vuln creation in bulk create

--- faraday/server/api/modules/bulk_create.py
+++ faraday/server/api/modules/bulk_create.py
@@ -1,21 +1,25 @@
 import sqlalchemy
-from marshmallow import fields
+from marshmallow import fields, ValidationError
 from marshmallow.validate import Range
 from faraday.server.models import (
     db,
     Host,
     Hostname,
     Service,
+    Vulnerability,
 )
 from faraday.server.utils.database import (
     get_conflict_object,
     is_unique_constraint_violation
     )
-from faraday.server.api.modules.hosts import HostSchema as OriginalHostSchema
-from faraday.server.api.modules.services import ServiceSchema as OriginalServiceSchema
+from faraday.server.api.modules import (
+    hosts,
+    services,
+    vulns,
+)
 
 
-class ServiceSchema(OriginalServiceSchema):
+class ServiceSchema(services.ServiceSchema):
     """It's like the original service schema, but now it only uses port
     instead of ports (a single integer array). That field was only used
     to keep backwards compatibility with the Web UI"""
@@ -26,22 +30,35 @@ class ServiceSchema(OriginalServiceSchema):
         # Don't require the parent field
         return
 
-    class Meta(OriginalServiceSchema.Meta):
+    class Meta(services.ServiceSchema.Meta):
         fields = tuple(
-            field_name for field_name in OriginalServiceSchema.Meta.fields
+            field_name for field_name in services.ServiceSchema.Meta.fields
             if field_name not in ('parent', 'ports')
         )
 
 
-class HostSchema(OriginalHostSchema):
+class VulnerabilitySchema(vulns.VulnerabilitySchema):
+    class Meta(vulns.VulnerabilitySchema.Meta):
+        fields = tuple(
+            field_name for field_name in vulns.VulnerabilitySchema.Meta.fields
+            if field_name not in ('parent', 'parent_type')
+        )
+
+
+class HostSchema(hosts.HostSchema):
     services = fields.Nested(
         ServiceSchema(many=True, context={'updating': False}),
         many=True,
         missing=[],
     )
+    vulnerabilities = fields.Nested(
+        VulnerabilitySchema(many=True),
+        many=True,
+        missing=[],
+    )
 
-    class Meta(OriginalHostSchema.Meta):
-        fields = OriginalHostSchema.Meta.fields + ('services',)
+    class Meta(hosts.HostSchema.Meta):
+        fields = hosts.HostSchema.Meta.fields + ('services', 'vulnerabilities')
 
 
 def get_or_create(ws, model_class, data):
@@ -79,6 +96,7 @@ def create_host(ws, raw_data):
     host_data = schema.load(raw_data).data
     hostnames = host_data.pop('hostnames', [])
     services = host_data.pop('services')
+    vulns = host_data.pop('vulnerabilities')
     (created, host) = get_or_create(ws, Host, host_data)
     if created:
         for name in hostnames:
@@ -88,6 +106,9 @@ def create_host(ws, raw_data):
     for service_data in services:
         create_service(ws, host, service_data)
 
+    for vuln_data in vulns:
+        create_hostvuln(ws, host, vuln_data, False)
+
 
 def create_service(ws, host, raw_data):
     schema = ServiceSchema(strict=True, context={'updating': False})
@@ -95,3 +116,26 @@ def create_service(ws, host, raw_data):
     service_data['host'] = host
     (_, service) = get_or_create(ws, Service, service_data)
     db.session.commit()
+
+
+def create_hostvuln(ws, host, vuln_data, reload_data=True):
+    if reload_data:
+        schema = VulnerabilitySchema(strict=True)
+        vuln_data = schema.load(vuln_data).data
+
+    attachments = vuln_data.pop('_attachments', {})
+    references = vuln_data.pop('references', [])
+    policyviolations = vuln_data.pop('policy_violations', [])
+
+    vuln_data['host'] = host
+    if vuln_data['type'] != 'vulnerability':
+        raise ValidationError('Type must be "Vulnerability"')
+    (created, vuln) = get_or_create(ws, Vulnerability, vuln_data)
+    db.session.commit()
+
+    if created:
+        vuln.references = references
+        vuln.policyviolations = policyviolations
+        # TODO attachments
+        db.session.add(vuln)
+        db.session.commit()
--- tests/test_api_bulk_create.py
+++ tests/test_api_bulk_create.py
@@ -1,7 +1,11 @@
+import pytest
+from marshmallow import ValidationError
 from faraday.server.models import (
     db,
     Host,
     Service,
+    Vulnerability,
+    VulnerabilityGeneric,
 )
 from faraday.server.api.modules import bulk_create as bc
 
@@ -15,6 +19,18 @@ service_data = {
     "name": "http",
     "port": 80,
     "protocol": "tcp",
+}
+
+vuln_data = {
+    'name': 'sql injection',
+    'desc': 'test',
+    'severity': 'high',
+    'type': 'Vulnerability',
+    'impact': {
+        'accountability': True,
+        'availability': False,
+    },
+    'refs': ['CVE-1234']
 }
 
 
@@ -80,3 +96,58 @@ def test_create_existing_service(session, service):
     }
     bc.create_service(service.workspace, service.host, data)
     assert count(Service, service.host.workspace) == 1
+
+def test_create_host_vuln(session, host):
+    bc.create_hostvuln(host.workspace, host, vuln_data)
+    assert count(VulnerabilityGeneric, host.workspace) == 1
+    assert count(Vulnerability, host.workspace) == 1
+    vuln = host.workspace.vulnerabilities[0]
+    assert vuln.name == 'sql injection'
+    assert vuln.description == 'test'
+    assert vuln.severity == 'high'
+    assert vuln.impact_accountability
+    assert not vuln.impact_availability
+    assert not vuln.impact_confidentiality
+    assert vuln.references == {u'CVE-1234'}
+
+
+def test_cannot_create_host_vulnweb(session, host):
+    data = vuln_data.copy()
+    data['type'] = 'VulnerabilityWeb'
+    with pytest.raises(ValidationError):
+        bc.create_hostvuln(host.workspace, host, data)
+    assert count(VulnerabilityGeneric, host.workspace) == 0
+
+
+def test_create_existing_host_vuln(session, host, vulnerability_factory):
+    vuln = vulnerability_factory.create(
+        workspace=host.workspace, host=host, service=None)
+    session.add(vuln)
+    session.commit()
+    vuln.references = ['old']
+    session.add(vuln)
+    session.commit()
+    data = {
+        'name': vuln.name,
+        'desc': vuln.description,
+        'severity': vuln.severity,
+        'type': 'Vulnerability',
+        'refs': ['new']
+    }
+    bc.create_hostvuln(host.workspace, host, data)
+    session.commit()
+    assert count(Vulnerability, host.workspace) == 1
+    vuln = Vulnerability.query.get(vuln.id)  # just in case it isn't refreshed
+    assert 'old' in vuln.references  # it must preserve the old references
+
+
+def test_create_host_with_vuln(session, workspace):
+    host_data_ = host_data.copy()
+    host_data_['vulnerabilities'] = [vuln_data]
+    bc.bulk_create(workspace, dict(hosts=[host_data_]))
+    assert count(Host, workspace) == 1
+    host = workspace.hosts[0]
+    assert count(Vulnerability, workspace) == 1
+    vuln = Vulnerability.query.filter(Vulnerability.workspace == workspace).one()
+    assert vuln.name == 'sql injection'
+    assert vuln.host == host

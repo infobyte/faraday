8ef25e0d48444bbcd9374f2d89f6fa84b7c43cf0
Leonardo Lazzaro <leonardol@infobytesec.com>
2019-07-12 17:12:12 -0300

    add agents ui

---
+++ faraday/server/www/scripts/admin/agents/controllers/agents.js
@@ -0,0 +1,239 @@
+// Faraday Penetration Test IDE
+// Copyright (C) 2019  Infobyte LLC (http://www.infobytesec.com/)
+// See the file 'doc/LICENSE' for the license information
+
+angular.module('faradayApp')
+    .controller('agentsCtrl', [
+        '$scope',
+        'uuid',
+        'agentFact',
+        'workspacesFact',
+        'Notification',
+        '$routeParams',
+        '$uibModal',
+        'commonsFact',
+        function ($scope,
+                  uuid,
+                  agentFact,
+                  workspacesFact,
+                  Notification,
+                  $routeParams,
+                  $uibModal,
+                  commonsFact) {
+            $scope.agentToken = {id: null, token: null};
+            $scope.workspace = null;
+            $scope.agents = [];
+            $scope.selectAll = false;
+            $scope.options = [];
+
+            $scope.init = function () {
+                getWorkspaces();
+            };
+
+
+            var getWorkspaces = function () {
+                workspacesFact.getWorkspaces().then(function (wss) {
+                    $scope.workspaces = [];
+
+                    wss.forEach(function (ws) {
+                        $scope.workspaces.push(ws);
+                    });
+
+                    $scope.workspace = $scope.workspaces[0].name;
+                    $scope.workspaceData = $scope.workspaces[0];
+
+                    getToken();
+                    getAgents();
+                });
+            };
+
+
+            $scope.switchWorkspace = function (workspace) {
+                $scope.workspace = workspace;
+
+                workspacesFact.get(workspace).then(function (ws) {
+                    $scope.workspaceData = ws
+                });
+
+                getToken();
+                getAgents();
+            };
+
+
+            var getToken = function () {
+                agentFact.getAgentToken($scope.workspace).then(
+                    function (response) {
+                        $scope.agentToken = response.data;
+                    }, function (error) {
+                        console.log(error);
+                    });
+            };
+
+            var createToken = function () {
+                var agentToken = {'token': uuid.v4()};
+                agentFact.createAgentToken(agentToken).then(
+                    function (response) {
+                        $scope.agentToken = response.data;
+                    }, function (error) {
+                        console.log(error);
+                    });
+            };
+
+            var getAgents = function () {
+                agentFact.getAgents($scope.workspace).then(
+                    function (response) {
+                        $scope.agents = response.data
+                    }, function (error) {
+                        console.log(error);
+                    });
+            };
+
+            var removeAgentFromScope = function (agentId) {
+                for (var i = 0; i < $scope.agents.length; i++) {
+                    if ($scope.agents[i].id === agentId) {
+                        $scope.agents.splice(i, 1);
+                        break;
+                    }
+                }
+            };
+
+            $scope.refreshToken = function () {
+                var agentToken = {id: $scope.agentToken.id, token: uuid.v4()};
+                agentFact.updateAgentToken(agentToken).then(
+                    function (response) {
+                        $scope.agentToken = response.data;
+                    }, function (error) {
+                        console.log(error);
+                    });
+            };
+
+            $scope.copyToClipboard = function () {
+                var copyElement = document.createElement("textarea");
+                copyElement.style.position = 'fixed';
+                copyElement.style.opacity = '0';
+                copyElement.textContent = decodeURI($scope.agentToken.token);
+                var body = document.getElementsByTagName('body')[0];
+                body.appendChild(copyElement);
+                copyElement.select();
+                document.execCommand('copy');
+                body.removeChild(copyElement);
+                Notification.success("Token copied to clipboard");
+            };
+
+            var _delete = function (agentId) {
+                var modal = $uibModal.open({
+                    templateUrl: 'scripts/commons/partials/modalDelete.html',
+                    controller: 'commonsModalDelete',
+                    size: 'md',
+                    resolve: {
+                        msg: function () {
+                            return "A agent will be deleted. This action cannot be undone. " +
+                                "Are you sure you want to proceed?";
+                        }
+                    }
+                });
+
+                modal.result.then(function () {
+                    agentFact.deleteAgent($scope.workspace, agentId).then(
+                        function (response) {
+                            removeAgentFromScope(agentId);
+                            Notification.success("The Agent has been removed");
+                        }, function (error) {
+                            console.log(error);
+                        });
+                });
+            };
+
+            $scope.removeAgent = function (agentId) {
+                _delete(agentId);
+            };
+
+            $scope.changeStatusAgent = function (agent) {
+                var oldStatus = agent.status;
+                if (agent.status === 'paused')
+                    agent.status = 'running';
+                else
+                    agent.status = 'paused';
+
+                var agentData = {
+                    id: agent.id,
+                    status: agent.status
+                };
+
+                agentFact.updateAgent($scope.workspace, agentData).then(
+                    function (response) {
+                        if (response.data.status === 'paused')
+                            Notification.success("The Agent has been paused");
+                        else
+                            Notification.success("The Agent is running");
+                    }, function (error) {
+                        agent.status = oldStatus;
+                        console.log(error);
+                    });
+            };
+
+
+            $scope.toggleAgent = function (index) {
+                $scope.agents[index].checked = !$scope.agents[index].checked;
+                if (!$scope.agents[index].checked) {
+                    $scope.selectAll = false;
+                } else {
+                    $scope.selectAll = $scope.agents.filter(function (agent) {
+                            return agent.checked === true
+                        }).length === $scope.agents.length;
+                }
+            };
+
+            $scope.toggleAll = function () {
+                var checked = $scope.selectAll;
+                for (var i = 0; i < $scope.agents.length; i++) {
+                    $scope.options[i] = checked;
+                    $scope.agents[i].checked = checked;
+                    $scope.agents[i].selected = checked;
+                }
+            };
+
+            var getCurrentSelection = function () {
+                return $scope.agents.filter(function (agent) {
+                    return agent.selected === true
+                });
+            };
+
+
+            var putTag = function (data) {
+                getCurrentSelection().forEach(function (agent) {
+                    agent.tags = data.tags;
+                    agentFact.updateAgent($scope.workspace, agent).then(
+                        function (response) {
+                        }, function (error) {
+                            console.log(error);
+                        });
+                });
+            };
+
+            $scope.agentsTags = function () {
+                if (getCurrentSelection().length > 0) {
+                    var modal = $uibModal.open({
+                        templateUrl: 'scripts/tags/partials/modalTags.html',
+                        controller: 'modalTagsCtrl',
+                        size: 'lg',
+                        resolve: {
+                            vulns: function () {  // TODO: CHANGE VAR NAME, SHOULD BE 'items' or 'objects' IN 'modalTagsCtrl'
+                                return getCurrentSelection();
+                            },
+                            workspace: function () {
+                                return $scope.workspace;
+                            }
+                        }
+                    });
+
+                    modal.result.then(function (data) {
+                        putTag(data);
+                    });
+                } else {
+                    commonsFact.showMessage('No agents were selected to tag');
+                }
+            };
+
+            $scope.init();
+        }]);
---
+++ faraday/server/www/scripts/admin/agents/providers/agents.js
@@ -0,0 +1,71 @@
+// Faraday Penetration Test IDE
+// Copyright (C) 2013  Infobyte LLC (http://www.infobytesec.com/)
+// See the file 'doc/LICENSE' for the license information
+
+angular.module('faradayApp')
+    .factory('agentFact', ['BASEURL', 'ServerAPI', '$http', '$q', function(BASEURL, ServerAPI, $http, $q) {
+        var agentFact = {};
+
+        agentFact.createAgentToken = function(agentToken) {
+            var deferred = $q.defer();
+            ServerAPI.createAgentToken(agentToken).then(function(response) {
+                    deferred.resolve(response);
+                }, function (error) {
+                deferred.reject(error)
+            });
+            return deferred.promise;
+        };
+
+
+        agentFact.getAgentToken = function() {
+            var deferred = $q.defer();
+            ServerAPI.getAgentToken().then(function(response) {
+                    deferred.resolve(response);
+                }, function (error) {
+                deferred.reject(error)
+            });
+            return deferred.promise;
+        };
+
+        agentFact.updateAgentToken = function(agentToken) {
+            var deferred = $q.defer();
+            ServerAPI.updateAgentToken(agentToken).then(function(response) {
+                    deferred.resolve(response);
+                }, function (error) {
+                deferred.reject(error)
+            });
+            return deferred.promise;
+        };
+
+        agentFact.getAgents = function(wsName) {
+            var deferred = $q.defer();
+            ServerAPI.getAgents(wsName).then(function(response) {
+                    deferred.resolve(response);
+                }, function (error) {
+                deferred.reject(error)
+            });
+            return deferred.promise;
+        };
+
+        agentFact.deleteAgent = function(wsName, agentId) {
+            var deferred = $q.defer();
+            ServerAPI.deleteAgent(wsName, agentId).then(function(response) {
+                    deferred.resolve(response);
+                }, function (error) {
+                deferred.reject(error)
+            });
+            return deferred.promise;
+        };
+
+        agentFact.updateAgent = function(wsName, agent) {
+            var deferred = $q.defer();
+            ServerAPI.updateAgent(wsName, agent).then(function(response) {
+                    deferred.resolve(response);
+                }, function (error) {
+                deferred.reject(error)
+            });
+            return deferred.promise;
+        };
+
+        return agentFact;
+    }]);

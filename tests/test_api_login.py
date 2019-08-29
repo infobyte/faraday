47fb4c3a0b4d08d1987067253a962c8968c5cb8d
Pablo Marcelli <marcellipablo@gmail.com>
2019-08-29 17:56:34 -0300

    remove Bit space to avoid the database give error

--- tests/test_api_login.py
+++ tests/test_api_login.py
@@ -125,3 +125,34 @@ class TestLogin():
         test_client.cookie_jar.clear()
         res = test_client.get('/v2/ws/', headers=headers)
         assert res.status_code == 401
+
+    def test_null_caracters(self, test_client, session):
+        """
+            Use of a valid auth token
+        """
+
+        alice = factories.UserFactory.create(
+                active=True,
+                username='asdasd',
+                password='asdasd',
+                role='pentester')
+        session.add(alice)
+        session.commit()
+
+        ws = factories.WorkspaceFactory.create(name='wonderland')
+        session.add(ws)
+        session.commit()
+
+        login_payload = {
+            'email': "\x00asd\00asd\0",
+            'password': "\x00asd\00asd\0",
+        }
+        res = test_client.post('/login', data=login_payload)
+        # import ipdb; ipdb.set_trace()
+        assert res.status_code == 200
+        assert 'authentication_token' in res.json['response']['user']
+
+        headers = {'Authentication-Token': res.json['response']['user']['authentication_token']}
+
+        ws = test_client.get('/v2/ws/wonderland/', headers=headers)
+        assert ws.status_code == 200

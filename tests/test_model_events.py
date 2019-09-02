e475d26bb3c209fc885893e4d53b579e7281d675
Mariano Ramon <mramon@infobytesec.com>
2019-09-02 20:07:29 -0300

    [ADD] more test to different workspace verification in parent-child relationship

--- tests/test_model_events.py
+++ tests/test_model_events.py
@@ -6,6 +6,7 @@ See the file 'doc/LICENSE' for the license information
 '''
 import pytest
 from tests.factories import HostFactory, ServiceFactory
+from faraday.server.models import Host, Workspace
 
 
 def test_child_parent_verification_event_fails(session, workspace,
@@ -15,6 +16,12 @@ def test_child_parent_verification_event_fails(session, workspace,
     with pytest.raises(AssertionError):
         session.commit()
 
+    session.rollback()
+
+    assert session.query(Host).filter(
+            Workspace.id == workspace.id
+        ).first() == None
+
 
 def test_child_parent_verification_event_succeeds(session, workspace):
     """
@@ -23,3 +30,42 @@ def test_child_parent_verification_event_succeeds(session, workspace):
     host = HostFactory.build(workspace=workspace)
     ServiceFactory.build(host=host, workspace=workspace)
     session.commit()
+
+
+def test_child_parent_verification_event_fails_update(session, workspace,
+                                                      second_workspace):
+    host = HostFactory.build(workspace=workspace)
+    service = ServiceFactory.build(host=host, workspace=workspace)
+    session.commit()
+    service.workspace = second_workspace
+    session.add(service)
+    with pytest.raises(AssertionError):
+        session.commit()
+
+
+def test_child_parent_verification_event_succeds_update(session, workspace):
+    host = HostFactory.build(workspace=workspace)
+    service = ServiceFactory.build(host=host, workspace=workspace)
+    session.commit()
+    service.workspace = workspace
+    session.add(service)
+    session.commit()
+
+
+def test_child_parent_verification_event_changing_id_fails(session, workspace,
+                                                           second_workspace):
+    
+    session.add(workspace)
+    session.add(second_workspace)
+    session.commit()
+    host = HostFactory.build(workspace=workspace)
+    session.add(host)
+    session.commit()
+    service = ServiceFactory.build(host=host, workspace_id=second_workspace.id)
+
+    session.add(service)
+   
+    with pytest.raises(AssertionError):
+        session.commit()
+
+

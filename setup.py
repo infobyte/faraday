f06d89d0883dd34e399b8cdb6d8569df0a70ad42
Sebastian Brachi <sbrachi@infobytesec.com>
2019-10-29 12:46:29 -0300

    Fix wrong licence and python version in setup.py.

--- setup.py
+++ setup.py
@@ -138,19 +138,15 @@ setup(
         'Topic :: System :: Networking :: Monitoring',
 
         # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: tkt_white_6063-mejorar-el-readme',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         # These classifiers are *not* checked by 'pip install'. See instead
         # 'python_requires' below.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.4',
-        #'Programming Language :: Python :: 3.5',
-        #'Programming Language :: Python :: 3.6',
-        #'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # This field adds keywords for your project which will appear on the

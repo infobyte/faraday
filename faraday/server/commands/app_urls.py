8537f5886bc6de917033137303c3f7780b721d87
Leonardo Lazzaro <leonardol@infobytesec.com>
2019-12-20 19:41:26 -0300

    draft idea

--- faraday/server/commands/app_urls.py
+++ faraday/server/commands/app_urls.py
@@ -33,7 +33,7 @@ def openapi_format(format="yaml"):
         title="Faraday API",
         version="2",
         openapi_version="3.0.2",
-        plugins=[FlaskPlugin(), MarshmallowPlugin()],
+        plugins=[FaradayAPIPlugin(), MarshmallowPlugin()],
         info={'description': 'The Faraday server API'},
         tags=TAGS,
     )

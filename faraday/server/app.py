8c1052d8da8d38ebdbba02b59c12c8da4c7ea7ce
Matías Lang <matiasl@infobytesec.com>
2020-01-23 19:37:13 -0300

    Fix CSRF vulnerability in all JSON endpoints
    To fix the vuln, we need to use SameSite cookies. The upstream version
    of Flask-KVSession doesn't support that, so we forked it, merged the
    changes in https://github.com/mbr/flask-kvsession/pull/52 and uploaded
    the new version to PyPI (with the name `flask-kvsession-fork`).
    
    I also changed the session cookie name to discard old session cookies
    that don't have the SameSite attribute unset.

--- faraday/server/app.py
+++ faraday/server/app.py
@@ -304,7 +304,8 @@ def create_app(db_connection_string=None, testing=None):
             'plaintext',  # TODO: remove it
         ],
         'PERMANENT_SESSION_LIFETIME': datetime.timedelta(hours=12),
-        'SESSION_COOKIE_NAME': 'faraday_session',
+        'SESSION_COOKIE_NAME': 'faraday_session_2',
+        'SESSION_COOKIE_SAMESITE': 'Lax',
     })
 
     store = FilesystemStore(app.config['SESSION_FILE_DIR'])

22768bb052d2beaa3ec86ef8b20af5ce0f9cba97
dnadares <dnadares@infobytesec.com>
2020-02-05 12:19:46 -0300

    Add preferences api tests

---
+++ tests/test_api_preferences.py
@@ -0,0 +1,19 @@
+import pytest
+
+from tests.test_api_non_workspaced_base import GenericAPITest
+from tests.factories import UserFactory
+from faraday.server.models import User
+from faraday.server.api.modules.preferences import PreferencesView
+
+pytest.fixture('logged_user')
+class TestPreferences(GenericAPITest):
+    model = User
+    factory = UserFactory
+    api_endpoint = 'preferences'
+    view_class = PreferencesView
+
+    def test_add_preference(self, test_client):
+        data = {'id': 1}
+        response = test_client.post(self.url(), data=data)
+
+        assert response.status_code == 201

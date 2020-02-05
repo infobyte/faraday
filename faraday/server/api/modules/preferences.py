5c1bae8db1279e4c9132e719bc1af6550a357cb6
dnadares <dnadares@infobytesec.com>
2020-02-05 17:22:23 -0300

    Get user from flask.g

--- faraday/server/api/modules/preferences.py
+++ faraday/server/api/modules/preferences.py
@@ -1,6 +1,6 @@
 from faraday.server.api.base import GenericView
 from faraday.server.models import User
-from flask import Blueprint, request, session, abort, jsonify
+from flask import Blueprint, request, session, abort, jsonify, g
 
 preferences_api = Blueprint('preferences_api', __name__)
 
@@ -10,21 +10,13 @@ class PreferencesView(GenericView):
     route_base = 'preferences'
 
     def post(self):
-        # TODO: validate user id
-        user = User.query.get(session['user_id'])
-        if not user:
-            abort(404)
+        user = g.user
         preferences = request.json.get('preferences', {})
         user.preferences = preferences
+
         return jsonify(''), 201
 
-        session.add(user)
-        session.commit()
-
     def get(self):
-        user = User.query.get(session['user_id'])
-        if not user:
-            abort(404)
-        return jsonify({'preferences': user.preferences}), 200
+        return jsonify({'preferences': g.user.preferences}), 200
 
 PreferencesView.register(preferences_api)

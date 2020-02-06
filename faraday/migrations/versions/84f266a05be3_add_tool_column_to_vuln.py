bac7dbda6d084c0fc58df1881284289b2d25feeb
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2020-02-06 20:12:38 -0300

    modify migration

--- faraday/migrations/versions/84f266a05be3_add_tool_column_to_vuln.py
+++ faraday/migrations/versions/84f266a05be3_add_tool_column_to_vuln.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '84f266a05be3'
-down_revision = '904a517a2f0c'
+down_revision = 'a39a3a6e3f99'
 branch_labels = None
 depends_on = None
 

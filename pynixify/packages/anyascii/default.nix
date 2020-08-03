85782c4efd7211aa0799324a66f298b931b0d45c
Leonardo Lazzaro <leonardol@infobytesec.com>
2020-08-03 16:57:56 -0300

    add missing pynixify files
     Run pynixify
    
            * Re execution of the pynixify after a merge
    
            Command run: pynixify --nixpkgs https://github.com/infobyte/nixpkgs/archive/6edef530cb277f73587bfc7cb8ef17175abd306d.tar.gz --local faradaysec --tests faradaysec

---
+++ pynixify/packages/anyascii/default.nix
@@ -0,0 +1,22 @@
+# WARNING: This file was automatically generated. You should avoid editing it.
+# If you run pynixify again, the file will be either overwritten or
+# deleted, and you will lose the changes you made to it.
+
+{ buildPythonPackage, fetchPypi, lib }:
+buildPythonPackage rec {
+  pname = "anyascii";
+  version = "0.1.6";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "112z1jlqngcqdnpb7amb1r2yvd4n0h1748jjsfsy35qx3y32ij6r";
+  };
+
+  # TODO FIXME
+  doCheck = false;
+
+  meta = {
+    description = "Unicode to ASCII transliteration";
+    homepage = "https://github.com/hunterwb/any-ascii";
+  };
+}

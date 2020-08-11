aa1c98888e61566b044fef6b2ca14d06556bcbf9
Nicolas Rebagliati <nrebagliati@infobytesec.com>
2020-08-11 19:02:52 -0300

    remove 404 from session api when token is used

--- faraday/server/api/modules/session.py
+++ faraday/server/api/modules/session.py
@@ -7,19 +7,16 @@ See the file 'doc/LICENSE' for the license information
 from flask import jsonify, session, Blueprint, current_app, abort
 from flask_wtf.csrf import generate_csrf
 from faraday.server.api.base import get_user_permissions
+import flask
 
 session_api = Blueprint('session_api', __name__)
 
 @session_api.route('/session')
 def session_info():
-    user_id = session.get('_user_id')
-    if user_id:
-        user = current_app.user_datastore.get_user(user_id)  # TODO use public flask_login functions
-        data = user.get_security_payload()
-        data['csrf_token'] = generate_csrf()
-        data['preferences'] = user.preferences
-        data['permissions'] = get_user_permissions(user)
-        return jsonify(data)
-    else:
-        abort(404)
+    user = flask.g.user
+    data = user.get_security_payload()
+    data['csrf_token'] = generate_csrf()
+    data['preferences'] = user.preferences
+    data['permissions'] = get_user_permissions(user)
+    return jsonify(data)
 # I'm Py3

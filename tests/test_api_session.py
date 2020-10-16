a25714fb68b548e444213784bffcaedf1a1fcc7a
Nicolas Rebagliati <nrebagliati@infobytesec.com>
2020-10-16 12:00:38 -0300

    add session role test

--- tests/test_api_session.py
+++ tests/test_api_session.py
@@ -6,12 +6,22 @@ See the file 'doc/LICENSE' for the license information
 '''
 
 import pytest
+from tests.conftest import login_as
 
 @pytest.mark.usefixtures('logged_user')
 class TestSessionLogged():
     def test_session_when_user_is_logged(self, test_client):
         res = test_client.get('/session')
         assert res.status_code == 200
+
+    @pytest.mark.parametrize('role', ['admin', 'pentester', 'client'])
+    def test_session_when_user_is_logged_with_different_roles(self, test_client, session, user, role):
+        user.role = role
+        session.commit()
+        login_as(test_client, user)
+        res = test_client.get('/session')
+        assert res.json['role'] == role
+
 
 class TestSessionNotLogged():
     def test_session_when_user_is_not_logged(self, test_client):

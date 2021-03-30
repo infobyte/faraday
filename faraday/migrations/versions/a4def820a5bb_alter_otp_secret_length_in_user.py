78ee8a7caa0db44774b7e7d522ea70d3631a5c99
Eric Horvat <eric.nahuel.horvat@gmail.com>
2021-03-30 12:41:22 -0300

    [MOD] Update migration's down_revision + 2FA will be reset if len > old length.

--- faraday/migrations/versions/a4def820a5bb_alter_otp_secret_length_in_user.py
+++ faraday/migrations/versions/a4def820a5bb_alter_otp_secret_length_in_user.py
@@ -11,19 +11,12 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a4def820a5bb'
-down_revision = '077b7c925ded'
+down_revision = 'fa12b8322112'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    secrets = sa.table('faraday_user',
-                       sa.column('otp_secret', sa.String),
-                       sa.column('id', sa.Integer)
-                       )
-
-    conn = op.get_bind()
-    res = conn.execute('SELECT otp_secret, id FROM faraday_user').fetchall()
 
     op.alter_column('faraday_user',
                     'otp_secret',
@@ -31,14 +24,30 @@ def upgrade():
                     existing_type=sa.String(16),
                     existing_nullable=True)
 
-    for user in res:
-        if user[0]:
-            op.execute(
-                secrets.update().where(secrets.c.id == user[1]).values({'otp_secret': user[0]})
-            )
+
+OTP_STATES = ["disabled", "requested", "confirmed"]
 
 
 def downgrade():
+    print("The otp secret is going down from 32-char to 16-char length. If its actual values can't fix, "
+          "we are deactivating 2FA in that user!")
+
+    users = sa.table(
+        'faraday_user',
+        sa.column('otp_secret', sa.String),
+        sa.column('id', sa.Integer),
+        sa.Column('state_otp', sa.Enum(*OTP_STATES, 'user_otp_states')),
+    )
+
+    conn = op.get_bind()
+    res = conn.execute('SELECT otp_secret, id FROM faraday_user').fetchall()
+
+    for user in res:
+        if user[0] and len(user[0]) > 16:
+            op.execute(
+                users.update().where(users.c.id == user[1]).values({'otp_secret': None, 'state_otp': "disabled"})
+            )
+
     op.alter_column('faraday_user',
                     'otp_secret',
                     type_=sa.String(16),

d204f74cec19996a6afb3a7cbf431e49d41483d9
Matias Rodriguez <matias.rodriguez@synapticlinks.com.ar>
2021-04-04 23:57:29 -0300

    [FIX] fix case sensitive username

--- tests/test_api_login.py
+++ tests/test_api_login.py
@@ -30,7 +30,7 @@ class TestLogin:
         session.commit()
         # we use lower case username, but in db is Capitalized
         login_payload = {
-            'email': 'susan',
+            'email': 'Susan',
             'password': 'pepito',
         }
         res = test_client.post('/login', data=login_payload)

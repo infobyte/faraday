89a1bc2080361b47cf65f795d47581e6b25c3dc4
Nahuel Alonso <nalonso@infobytesec.com>
2021-05-17 11:04:09 -0300

    Added pylint exception

--- faraday/server/app.py
+++ faraday/server/app.py
@@ -99,7 +99,7 @@ def register_blueprints(app):
     from faraday.server.api.modules.export_data import export_data_api  # pylint:disable=import-outside-toplevel
     # Custom reset password
     from faraday.server.api.modules.auth import auth  # pylint:disable=import-outside-toplevel
-    from faraday.server.api.modules.param_type import param_type_validate
+    from faraday.server.api.modules.param_type import param_type_validate  # pylint:disable=import-outside-toplevel
 
     app.register_blueprint(commandsrun_api)
     app.register_blueprint(activityfeed_api)

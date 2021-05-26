33b5dcddbcee4310752cbf557f5e4634eb25f945
Eric Horvat <eric.nahuel.horvat@gmail.com>
2021-05-26 14:42:48 -0300

    [MOD] run pynixify

--- pynixify/packages/faraday-agent-parameters-types/default.nix
+++ pynixify/packages/faraday-agent-parameters-types/default.nix
@@ -5,15 +5,13 @@
 { buildPythonPackage
 , fetchPypi
 , lib
-, marshmallow
-, pytestrunner
 }:
 
 buildPythonPackage rec {
   pname =
     "faraday-agent-parameters-types";
   version =
-    "0.1.1";
+    "0.1.8";
 
   src =
     fetchPypi {
@@ -22,30 +20,14 @@ buildPythonPackage rec {
       pname =
         "faraday_agent_parameters_types";
       sha256 =
-        "1m9qx0byanr7ppaqmzrvnlplr2kfag1mpng855bq9b6zjpvksfrc";
+        "0jp0z4l3kxppbwaak7bx5lfr5ih2kldyp96mbwvxjhjhr825a2ba";
     };
-
-  buildInputs =
-    [
-      pytestrunner
-    ];
-  propagatedBuildInputs =
-    [
-      marshmallow
-    ];
 
   # TODO FIXME
   doCheck =
     false;
 
   meta =
-    with lib; {
-      description =
-        ''
-          The faraday agents run code remotely to ensure your domains. This info is triggered and published
-              to a faraday server instance, which had set the parameters of the code. This repository sets the models to be used
-              by both sides.'';
-      homepage =
-        "https://github.com/infobyte/faraday_agent_parameters_types";
-    };
+    with lib;
+    { };
 }

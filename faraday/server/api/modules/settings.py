4b792cefc9e0f2c0c8b0c68a034353761c498831
Nicolas Rebagliati <nrebagliati@faradaysec.com>
2021-06-16 12:26:30 -0300

    add test of settings api to white

--- faraday/server/api/modules/settings.py
+++ faraday/server/api/modules/settings.py
@@ -4,7 +4,6 @@
 from __future__ import absolute_import
 
 import flask
-import flask_login
 import logging
 from flask import abort, make_response
 from marshmallow import Schema, ValidationError
@@ -42,16 +41,13 @@ class SettingsAPIView(GenericView):
             403:
               description: Admin user required
         """
-        if flask_login.current_user.role != 'admin':
-            abort(403, "Admin only action")
-
         settings = get_settings(self.route_base)
         return self._dump(settings.value, kwargs)
 
-    def post(self, **kwargs):
+    def patch(self, **kwargs):
         """
         ---
-        post:
+        patch:
           tags: ["settings"]
           summary: Creates/Updates settings
           requestBody:
@@ -60,7 +56,7 @@ class SettingsAPIView(GenericView):
               application/json:
                 schema: {schema_class}
           responses:
-            201:
+            200:
               description: Created
               content:
                 application/json:
@@ -68,9 +64,6 @@ class SettingsAPIView(GenericView):
             403:
               description: Admin user required
         """
-        if flask_login.current_user.role != 'admin':
-            abort(403, "Admin only action")
-
         context = {'updating': False}
 
         data = self._parse_data(self._get_schema_instance(kwargs, context=context),
@@ -82,4 +75,4 @@ class SettingsAPIView(GenericView):
         except (ValidationError, InvalidConfigurationError) as e:
             logger.error(f'Invalid setting for {data}: {e}.')
             abort(make_response({'messages': {'json': {'error': f'{e}.'}}}, 400))
-        return self._dump(settings.value, kwargs), 201
+        return self._dump(settings.value, kwargs), 200

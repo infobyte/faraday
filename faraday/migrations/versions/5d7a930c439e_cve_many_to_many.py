09ecab77d37d277eaff7f1e5e844e04e8bf88724
dnadares <dnadares@infobytesec.com>
2021-07-08 16:32:38 -0300

    Fix migration

--- faraday/migrations/versions/5d7a930c439e_cve_many_to_many.py
+++ faraday/migrations/versions/5d7a930c439e_cve_many_to_many.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '5d7a930c439e'
-down_revision = 'f0439bf6688a'
+down_revision = '97a9348d0406'
 branch_labels = None
 depends_on = None
 

733913227aaff8890c7e7e53ee278a283e093927
Ignacio Feijoo <ifeijoo@infobytesec.com>
2021-11-08 18:06:25 -0300

    merge migrations

--- faraday/migrations/versions/38bb251889e6_bulks.py
+++ faraday/migrations/versions/38bb251889e6_bulks.py
@@ -1,7 +1,7 @@
 """empty message
 
 Revision ID: 38bb251889e6
-Revises: 1574fbcf72f5
+Revises: d8f0b32a5c0e
 Create Date: 2021-07-30 02:12:00.706416+00:00
 
 """
@@ -9,7 +9,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '38bb251889e6'
-down_revision = '1574fbcf72f5'
+down_revision = 'd8f0b32a5c0e'
 branch_labels = None
 depends_on = None
 

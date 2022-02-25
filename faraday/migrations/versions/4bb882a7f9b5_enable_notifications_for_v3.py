de0e63feb1b21094e3887bb210746731cdd227b5
Diego Nadares <dnadares@faradaysec.com>
2022-02-25 11:57:01 -0300

    Add migration

---
+++ faraday/migrations/versions/4bb882a7f9b5_enable_notifications_for_v3.py
@@ -0,0 +1,22 @@
+"""enable notifications for v3
+
+Revision ID: 4bb882a7f9b5
+Revises: 5cf9660bba80
+Create Date: 2022-02-25 14:24:20.027512+00:00
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '4bb882a7f9b5'
+down_revision = '5cf9660bba80'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute('UPDATE  NOTIFICATION_SUBSCRIPTION_CONFIG_BASE SET active = true WHERE id IN (1, 11, 14, 21, 13)')
+
+
+def downgrade():
+    op.execute('UPDATE  NOTIFICATION_SUBSCRIPTION_CONFIG_BASE SET active = false WHERE id IN (1, 11, 14, 21, 13)')

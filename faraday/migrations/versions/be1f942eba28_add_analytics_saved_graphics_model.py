95d0fa31ba6e5cbb5f16d13c0cfe6b8aa2119cb0
Diego Nadares <dnadares@faradaysec.com>
2022-03-29 11:00:23 -0300

    Add workspaces and host field in order to verify if exists at refresh

--- faraday/migrations/versions/be1f942eba28_add_analytics_saved_graphics_model.py
+++ faraday/migrations/versions/be1f942eba28_add_analytics_saved_graphics_model.py
@@ -26,6 +26,8 @@ def upgrade():
     sa.Column('description', sa.Text(), nullable=True),
     sa.Column('type', sa.Enum('vulnerabilities_per_host', 'vulnerabilities_per_status', 'vulnerabilities_per_severity', 'top_ten_most_affected_hosts', 'top_ten_most_repeated_vulns', 'month_evolution_by_status', 'month_evolution_by_severity', name='analytics_types'), nullable=False),
     sa.Column('filters', sa.JSON(), nullable=False),
+    sa.Column('workspaces', sa.JSON(), nullable=False),
+    sa.Column('hosts', sa.JSON(), nullable=False),
     sa.Column('data', sa.JSON(), nullable=False),
     sa.Column('creator_id', sa.Integer(), nullable=True),
     sa.Column('update_user_id', sa.Integer(), nullable=True),

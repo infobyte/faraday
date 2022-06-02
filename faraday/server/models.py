169b1504151ff4b0ba63960faa8cdd1590dfe147
Nahuel Alonso <nalonso@infobytesec.com>
2022-06-02 13:38:48 -0300

    Fixed query

--- faraday/server/models.py
+++ faraday/server/models.py
@@ -1984,7 +1984,7 @@ class Workspace(Metadata):
                 p_5.count_14 as vulnerability_unclassified_count,
                 p_5.count_15 as vulnerability_open_count,
                 p_5.count_16 as vulnerability_confirmed_count,
-                p_5.count_17 as vulnerability_open_count,
+                p_5.count_17 as vulnerability_closed_count,
                 workspace.create_date AS workspace_create_date,
                 workspace.update_date AS workspace_update_date,
                 workspace.id AS workspace_id,
@@ -2020,7 +2020,7 @@ class Workspace(Metadata):
              COUNT(case when vulnerability.severity = 'unclassified' then 1 else null end) as count_14,
              COUNT(case when vulnerability.status = 'open' OR vulnerability.status='re-opened' then 1 else null end) as count_15,
              COUNT(case when vulnerability.confirmed is True then 1 else null end) as count_16,
-                    COUNT(case when vulnerability.status = 'closed' then 1 else null end) as count_17,
+             COUNT(case when vulnerability.status = 'closed' then 1 else null end) as count_17,
                     FROM vulnerability
                     RIGHT JOIN workspace w ON vulnerability.workspace_id = w.id
                     WHERE 1=1 {0}

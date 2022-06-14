583b1ccfafd4b4eff126c4a2c5eff30b2eb45c44
Diego Nadares <dnadares@faradaysec.com>
2022-06-14 13:33:16 -0300

    Fix old migration

--- faraday/migrations/versions/89115e133f0a_add_hosts_notifications.py
+++ faraday/migrations/versions/89115e133f0a_add_hosts_notifications.py
@@ -9,8 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-from faraday.server.models import NotificationSubscription, NotificationSubscriptionWebSocketConfig, User, EventType, \
-    Role
+from faraday.server.models import NotificationSubscription, NotificationSubscriptionWebSocketConfig, User
 
 revision = '89115e133f0a'
 down_revision = '5d7a930c439e'
@@ -33,21 +32,34 @@ def upgrade():
     bind = op.get_bind()
     session = sa.orm.Session(bind=bind)
 
+    allowed_roles = sa.table(
+        'notification_allowed_roles',
+        sa.column('notification_subscription_id', sa.Integer),
+        sa.column('allowed_role_id', sa.Integer)
+    )
+    op.execute("INSERT INTO event_type (name, async_event) VALUES ('new_host', False)")
+    op.execute("INSERT INTO event_type (name, async_event) VALUES ('update_host', False)")
+    op.execute("INSERT INTO event_type (name, async_event) VALUES ('delete_host', False)")
+
+    res = bind.execute('SELECT name, id FROM event_type').fetchall()
+    event_type_ids = dict(res)
+
+    res = bind.execute('SELECT name, id FROM faraday_role').fetchall()
+    role_ids = dict(res)
+
     for config in notifications_config:
         for event_type in config['event_types']:
-            roles = session.query(Role).filter(Role.name.in_(config['roles'])).all()
-            if not roles:
-                raise ValueError(f"Roles {config['roles']} not exist.")
-
-            session.execute(f"INSERT INTO event_type (name) VALUES ('{event_type}')")  # nosec
-            event_type_id = session.query(EventType.id).filter(EventType.name == event_type).one()
-            n = NotificationSubscription(event_type_id=event_type_id, allowed_roles=roles)
-
-            ns = NotificationSubscriptionWebSocketConfig(subscription=n,
-                                                         active=True,
-                                                         role_level=True)
+            n = NotificationSubscription(event_type_id=event_type_ids[event_type])
+            session.add(n)
+            session.commit()
+            ns = NotificationSubscriptionWebSocketConfig(subscription=n, active=True, role_level=True)
             session.add(ns)
             session.commit()
+            for role_name in config['roles']:
+                op.execute(
+                    allowed_roles.insert().values({'notification_subscription_id': n.id,
+                                                   'allowed_role_id': role_ids[role_name]})
+                )
 
 
 def downgrade():
@@ -56,11 +68,35 @@ def downgrade():
 
     for config in notifications_config:
         for event_type in config['event_types']:
-            event_type_id = session.query(EventType.id).filter(EventType.name == event_type).one()
-            subscription = session.query(NotificationSubscription).filter(NotificationSubscription.event_type_id == event_type_id).one()
-            ns = session.query(NotificationSubscriptionWebSocketConfig).filter(NotificationSubscriptionWebSocketConfig.subscription == subscription).one()
-            session.delete(ns)
-            session.delete(subscription)
-            session.commit()
+            event_type_name = session.execute(f"SELECT id "
+                                              f"FROM event_type e "
+                                              f"WHERE e.name = '{event_type}'")
+            for event_type_id in event_type_name:
+                subscriptions = session.execute(
+                    f"SELECT id "
+                    f"FROM notification_subscription_config_base "
+                    f"WHERE subscription_id = '{event_type_id[0]}'")
+                for subscription_id in subscriptions:
+                    base_config = session.execute(
+                        f"SELECT id "
+                        f"FROM notification_subscription_config_base "
+                        f"WHERE subscription_id = '{subscription_id[0]}'")
+                    for base_config_id in base_config:
+                        session.execute(f"DELETE "
+                                        f"FROM notification_subscription_websocket_config "
+                                        f"WHERE id = '{base_config_id[0]}'")
+
+                        session.execute(f"DELETE "
+                                        f"FROM notification_allowed_roles na "
+                                        f"WHERE na.notification_subscription_id = '{subscription_id[0]}'")
+
+                        session.execute(f"DELETE "
+                                        f"FROM notification_subscription_config_base "
+                                        f"WHERE id = '{base_config_id[0]}'")
+
+                    session.execute(f"DELETE "
+                                    f"FROM notification_subscription ns "
+                                    f"WHERE ns.id = '{subscription_id[0]}'")
+
         name_list = ",".join([f"'{elem}'" for elem in config["event_types"]])
         session.execute(f'DELETE FROM event_type WHERE name IN ({name_list})')  # nosec

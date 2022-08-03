818c0f849419acc5084cccde75891a7aed570e44
Nicolas Rebagliati <nrebagliati@faradaysec.com>
2022-08-03 16:02:25 -0300

    fix migrations head

--- faraday/migrations/versions/4d7a8fdd94e4_rename_not_active_users.py
+++ faraday/migrations/versions/4d7a8fdd94e4_rename_not_active_users.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '4d7a8fdd94e4'
-down_revision = '384784872dc1'
+down_revision = '99a740945c44'
 branch_labels = None
 depends_on = None
 

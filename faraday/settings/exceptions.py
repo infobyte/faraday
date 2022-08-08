1dd7ac716afb0ee5f35973fed6672cdf6798a270
David Kraus <dkraus@infobytesec.com>
2022-08-08 12:13:21 -0300

    Cleanup settings/exceptions file

--- faraday/settings/exceptions.py
+++ faraday/settings/exceptions.py
@@ -1,3 +1,10 @@
+"""
+Faraday Penetration Test IDE
+Copyright (C) 2021  Infobyte LLC (https://faradaysec.com/)
+See the file 'doc/LICENSE' for the license information
+"""
+
+
 class MissingConfigurationError(Exception):
     """Raised when setting configuration is missing"""
     pass

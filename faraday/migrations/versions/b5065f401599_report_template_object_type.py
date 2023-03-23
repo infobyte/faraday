a0c29b7c89617c6e0d13de98a161d78f7fc8500e
David Kraus <dkraus@infobytesec.com>
2023-03-23 10:01:32 -0300

    Add report_template as an object_type

---
+++ faraday/migrations/versions/b5065f401599_report_template_object_type.py
@@ -0,0 +1,27 @@
+"""report_template object type
+
+Revision ID: b5065f401599
+Revises: 1e95dde5b9c8
+Create Date: 2023-03-23 12:47:14.125405+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b5065f401599'
+down_revision = '1e95dde5b9c8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    if not op.get_context().as_sql:
+        connection = op.get_bind()
+        connection.execution_options(isolation_level='AUTOCOMMIT')
+    op.execute("ALTER TYPE object_types ADD VALUE IF NOT EXISTS 'report_template'")
+
+
+def downgrade():
+    op.execute("DELETE FROM file WHERE object_type = 'report_template'")

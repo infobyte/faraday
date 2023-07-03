46efecb2728b7d69ca88704ccb92ca04d2d1ef5c
Diego Nadares <dnadares@faradaysec.com>
2023-07-03 07:16:11 -0300

    Add more joinedloads and undefers

--- faraday/server/api/base.py
+++ faraday/server/api/base.py
@@ -743,6 +743,13 @@ class FilterWorkspacedMixin(ListMixin):
                 undefer(self.model_class.vulnerability_low_generic_count),
                 undefer(self.model_class.vulnerability_info_generic_count),
                 undefer(self.model_class.vulnerability_unclassified_generic_count),
+                undefer(self.model_class.credentials_count),
+                undefer(self.model_class.open_service_count),
+                undefer(self.model_class.vulnerability_count),
+                joinedload(self.model_class.hostnames),
+                joinedload(self.model_class.services),
+                joinedload(self.model_class.update_user),
+                joinedload(getattr(self.model_class, 'creator')).load_only('username'),
             )
         return filter_query
 

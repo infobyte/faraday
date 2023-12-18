2b140e37ed8ec0e9e634d5fc15278062a9844734
Nahuel Alonso <nalonso@infobytesec.com>
2023-12-18 18:00:11 -0300

    fixed migration

--- faraday/migrations/versions/d0a6105fdef1_modify_analytics_type_enum.py
+++ faraday/migrations/versions/d0a6105fdef1_modify_analytics_type_enum.py
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'd0a6105fdef1'
-down_revision = '901344f297fb'
+down_revision = 'b87b1de2f348'
 branch_labels = None
 depends_on = None
 

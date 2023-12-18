82d3f5f3b553b8b319e634e24c3703b86fc5b39b
Nahuel Alonso <nalonso@infobytesec.com>
2023-12-18 18:02:25 -0300

    fixed migration

--- faraday/migrations/versions/257f6d0ad43f_add_fields_to_kb.py
+++ faraday/migrations/versions/257f6d0ad43f_add_fields_to_kb.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '257f6d0ad43f'
-down_revision = '901344f297fb'
+down_revision = 'd0a6105fdef1'
 branch_labels = None
 depends_on = None
 

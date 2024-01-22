305fb70809b338ac5530d584bc3627fddbb699b5
Nahuel Alonso <nalonso@infobytesec.com>
2024-01-22 15:44:59 -0300

    fix migration

--- faraday/migrations/versions/b87b1de2f348_add_delete_workspace_missing_cascades.py
+++ faraday/migrations/versions/b87b1de2f348_add_delete_workspace_missing_cascades.py
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'b87b1de2f348'
-down_revision = '443a136bb5f2'
+down_revision = 'd0a6105fdef1'
 branch_labels = None
 depends_on = None
 

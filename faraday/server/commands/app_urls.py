4aa3d177427935ca050aa0ef498265c358beb2ac
Dante Acosta <dacosta@faradaysec.com>
2024-03-14 13:25:31 -0300

    modified parameters for interactive swagger. also changed flake8 precommit repo.

--- faraday/server/commands/app_urls.py
+++ faraday/server/commands/app_urls.py
@@ -9,15 +9,45 @@ from pathlib import Path
 from flask import current_app
 
 import yaml
+import re
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec_webframeworks.flask import FlaskPlugin
 from faraday import __version__ as f_version
 import json
-from urllib.parse import urljoin
 from faraday.server.config import LOCAL_OPENAPI_FILE
 
 from faraday.utils.faraday_openapi_plugin import FaradayAPIPlugin
+
+
+def make_apispec_args(endpoint_route):
+
+    parameters = []
+
+    endpoint_params = [x[1:-1] for x in endpoint_route.split("/") if re.search("<(.*)>", x)]
+
+    for param in endpoint_params:
+
+        param_name = param_type = ""
+
+        if ":" not in param or re.search("(path|string|uuid):(.+)", param):
+            param_name = param.split(":")[-1]
+            param_type = "string"
+        elif re.search("int:(.+)", param):
+            param_name = param.split(":")[-1]
+            param_type = "integer"
+        elif re.search("float:(.+)", param):
+            param_name = param.split(":")[-1]
+            param_type = "number"
+
+        parameters.append({
+            "name": param_name,
+            "in": "path",
+            "schema": {"type": param_type},
+            "required": True
+        })
+
+    return parameters
 
 
 def openapi_format(server, modify_default=False, return_tags=False):
@@ -32,8 +62,6 @@ def openapi_format(server, modify_default=False, return_tags=False):
 
     if not server.startswith('http'):
         raise ValueError('Server must be an http url')
-
-    server = urljoin(server, "/_api")
 
     extra_specs['servers'] = [{'url': server}]
 
@@ -58,12 +86,17 @@ def openapi_format(server, modify_default=False, return_tags=False):
 
     tags = set()
 
+    endpoint_parameters = {}
+
+    for rule in current_app.url_map.iter_rules():
+        endpoint_parameters[rule.endpoint] = make_apispec_args(rule.rule)
+
     with current_app.test_request_context():
         for name, endpoint in current_app.view_functions.items():
             # TODO: check why this endpoint is breaking spec.path
             if name in ('static', 'index'):
                 continue
-            spec.path(view=endpoint, app=current_app)
+            spec.path(view=endpoint, app=current_app, parameters=endpoint_parameters[name])
 
         # Set up global tags
         spec_yaml = yaml.load(spec.to_yaml(), Loader=yaml.SafeLoader)

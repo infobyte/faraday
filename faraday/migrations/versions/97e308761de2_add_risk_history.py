46fa82953402fea4dfe4e57a1da14de746e2166a
Nahuel Alonso <nalonso@infobytesec.com>
2024-06-13 17:48:56 -0300

    fixed migration

--- faraday/migrations/versions/97e308761de2_add_risk_history.py
+++ faraday/migrations/versions/97e308761de2_add_risk_history.py
@@ -1,7 +1,7 @@
 """add risk history
 
 Revision ID: 97e308761de2
-Revises: 9f826327658a
+Revises: 27d7eff884e7
 Create Date: 2023-10-23 18:56:22.503518+00:00
 
 """
@@ -12,7 +12,7 @@ import json
 
 # revision identifiers, used by Alembic.
 revision = '97e308761de2'
-down_revision = '9f826327658a'
+down_revision = '27d7eff884e7'
 branch_labels = None
 depends_on = None
 

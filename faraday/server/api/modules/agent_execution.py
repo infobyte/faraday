2f537d790272dd4f850728dded0c75283e76524f
Adriano Canavero <acanavero@faradaysec.com>
2025-02-17 12:26:00 -0300

    select distinct rows by run id in agent execution view

--- faraday/server/api/modules/agent_execution.py
+++ faraday/server/api/modules/agent_execution.py
@@ -2,8 +2,10 @@ import logging
 
 from flask import Blueprint
 from marshmallow import fields
+from sqlalchemy import func
+
 from faraday.server.api.base import ReadOnlyView, PaginatedMixin, AutoSchema
-from faraday.server.models import AgentExecution
+from faraday.server.models import AgentExecution, db
 from faraday.server.schemas import PrimaryKeyRelatedField
 
 agent_execution_api = Blueprint('agent_execution_api', __name__)
@@ -11,7 +13,6 @@ logger = logging.getLogger(__name__)
 
 
 class AgentExecutionSchema(AutoSchema):
-    id = fields.Integer(dump_only=True, attribute='id')
     running = fields.Boolean()
     successful = fields.Boolean()
     message = fields.String()
@@ -19,12 +20,13 @@ class AgentExecutionSchema(AutoSchema):
     triggered_by = fields.String()
     executor = PrimaryKeyRelatedField('id', dump_only=True)
     command = PrimaryKeyRelatedField('id', dump_only=True, allow_none=True)
+    run_id = fields.Integer()
 
     class Meta:
         model = AgentExecution
         fields = (
-            'id', 'running', 'successful', 'message', 'parameters_data',
-            'triggered_by', 'executor', 'command',
+            'running', 'successful', 'message', 'parameters_data',
+            'triggered_by', 'executor', 'command', 'run_id'
         )
 
 
@@ -33,6 +35,18 @@ class AgentExecutionView(PaginatedMixin, ReadOnlyView):
     model_class = AgentExecution
     schema_class = AgentExecutionSchema
     order_field = AgentExecution.id.desc()
+
+    def _paginate(self, query, hard_limit=0):
+        # Select only distinct run_id rows
+        subquery = (
+            db.session.query(AgentExecution.run_id, func.min(AgentExecution.id).label("min_id"))
+            .group_by(AgentExecution.run_id)
+            .subquery()
+        )
+
+        query = query.join(subquery, AgentExecution.id == subquery.c.min_id)
+
+        return super()._paginate(query, hard_limit)
 
 
 AgentExecutionView.register(agent_execution_api)

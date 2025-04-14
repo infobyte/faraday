ab5e6cc861f507f7b1d85b634e1564a89e0a887f
Dante Acosta <dacosta@faradaysec.com>
2025-04-14 11:59:03 -0300

    Fix revert config changes

--- faraday/server/config.py
+++ faraday/server/config.py
@@ -149,8 +149,8 @@ class FaradayServerConfigObject(ConfigSection):
         self.debug = False
         self.delete_report_after_process = True
         self.celery_enabled = True
-        self.celery_broker_url = "127.0.0.1"
-        self.celery_backend_url = "127.0.0.1"
+        self.celery_broker_url = "redis://127.0.0.1:6379/0"
+        self.celery_backend_url = "redis://127.0.0.1:6379/0"
         self.socketio_ping_interval = 60
         self.socketio_ping_timeout = 220
         self.socketio_logger = False

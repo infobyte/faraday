2955ff75aed92ee7e393bd63403bd1cd334525b2
dkraus <dkraus@faradaysec.com>
2025-05-16 10:13:30 -0300

    Fix migration head

--- faraday/migrations/versions/a29d52685b58_custom_roles.py
+++ faraday/migrations/versions/a29d52685b58_custom_roles.py
@@ -1,7 +1,7 @@
 """custom roles
 
 Revision ID: a29d52685b58
-Revises: 39ddd3ca3a20
+Revises: 75c38da2da8a
 Create Date: 2025-02-24 19:29:44.754248+00:00
 
 """
@@ -87,7 +87,7 @@ RUN = PermissionsUnitAction.RUN_ACTION
 
 # revision identifiers, used by Alembic.
 revision = 'a29d52685b58'
-down_revision = '39ddd3ca3a20'
+down_revision = '75c38da2da8a'
 branch_labels = None
 depends_on = None
 

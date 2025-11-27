506616b467f045b880c066ebc3ec518c8ac2064b
Diego Nadares <dnadares@faradaysec.com>
2025-11-27 15:54:13 -0300

    Fix alembic heads
    Took 20 minutes

--- faraday/migrations/versions/8201db92d9e7_add_celery_tasks_within_commands.py
+++ faraday/migrations/versions/8201db92d9e7_add_celery_tasks_within_commands.py
@@ -1,7 +1,7 @@
 """add_celery_tasks_within_commands
 
 Revision ID: 8201db92d9e7
-Revises: c49e0cb49483
+Revises: 76b8ca65b9e8
 Create Date: 2025-11-10 14:29:08.391842+00:00
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '8201db92d9e7'
-down_revision = 'c49e0cb49483'
+down_revision = '76b8ca65b9e8'
 branch_labels = None
 depends_on = None
 
